099d89521451e4b35a409aab3a49524719f24841
Duane Wessels <wessels@packet-pushers.com>
1996-03-26 05:20:27 +0000

    adding

---
+++ src/errorpage.h
@@ -0,0 +1,20 @@
+typedef enum {
+    ERR_NONE,
+    ERR_READ_TIMEOUT,
+    ERR_LIFETIME_EXP,
+    ERR_NO_CLIENTS_BIG_OBJ,
+    ERR_READ_ERROR,
+    ERR_CLIENT_ABORT,
+    ERR_CONNECT_FAIL,
+    ERR_INVALID_URL,
+    ERR_NO_FDS,
+    ERR_DNS_FAIL,
+    ERR_NOT_IMPLEMENTED,
+    ERR_CANNOT_FETCH,
+    ERR_NO_RELAY,
+    ERR_DISK_IO,
+    ERR_MAX
+} error_t;
+
+void cached_error_entry _PARAMS((StoreEntry *, error_t, char *));
+char *cached_error_url _PARAMS((char *, error_t, char *));
