48680549f5a3bf57c5fa0ad71c78b0ce64b614f0
Duane Wessels <wessels@packet-pushers.com>
1996-03-27 20:21:11 +0000

    create setMaxFD() in tools.c

--- src/tools.c
+++ src/tools.c
@@ -349,3 +349,35 @@ void writePidFile()
     fprintf(pid_fp, "%d\n", (int) getpid());
     fclose(pid_fp);
 }
+
+
+void setMaxFD()
+{
+#if defined(HAVE_SETRLIMIT)
+    /* try to use as many file descriptors as possible */
+    /* System V uses RLIMIT_NOFILE and BSD uses RLIMIT_OFILE */
+    struct rlimit rl;
+#if defined(RLIMIT_NOFILE)
+    if (getrlimit(RLIMIT_NOFILE, &rl) < 0) {
+	perror("getrlimit: RLIMIT_NOFILE");
+    } else {
+	rl.rlim_cur = rl.rlim_max;	/* set it to the max */
+	if (setrlimit(RLIMIT_NOFILE, &rl) < 0) {
+	    perror("setrlimit: RLIMIT_NOFILE");
+	}
+    }
+#elif defined(RLIMIT_OFILE)
+    if (getrlimit(RLIMIT_OFILE, &rl) < 0) {
+	perror("getrlimit: RLIMIT_OFILE");
+    } else {
+	rl.rlim_cur = rl.rlim_max;	/* set it to the max */
+	if (setrlimit(RLIMIT_OFILE, &rl) < 0) {
+	    perror("setrlimit: RLIMIT_OFILE");
+	}
+    }
+#endif
+    debug(0,1,"setMaxFD: Using %d file descriptors\n", rl.rlim_max);
+#else /* HAVE_SETRLIMIT */
+    debug(0,1,"setMaxFD: Cannot increase: setrlimit() not supported on this system");
+#endif
+}
--- src/tools.h
+++ src/tools.h
@@ -13,5 +13,6 @@ extern void sig_child _PARAMS((int sig));
 extern void check_suid _PARAMS((void));
 extern int daemonize _PARAMS((void));
 extern void writePidFile _PARAMS((void));
+extern void setMaxFD _PARAMS((void));
 
 extern int do_mallinfo;
