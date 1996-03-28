9229cd642ba222f1c5b6c4bc9b87d3fbab37729b
Duane Wessels <wessels@packet-pushers.com>
1996-03-28 23:11:20 +0000

    init pointers to NULL

--- src/http.c
+++ src/http.c
@@ -58,7 +58,7 @@ int httpCachable(url, type, mime_hdr)
      char *type;
      char *mime_hdr;
 {
-    stoplist *p;
+    stoplist *p = NULL;
 
     /* GET and HEAD are cachable. Others are not. */
     if (((strncasecmp(type, "GET", 3) != 0)) &&
