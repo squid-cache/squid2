95817e882b1d05fd7c367a096fd223f5a1ac6041
Duane Wessels <wessels@packet-pushers.com>
1996-04-01 18:23:42 +0000

    add prototype for ipcache_unregister

--- src/ipcache.h
+++ src/ipcache.h
@@ -6,12 +6,6 @@
 /*  typedef  int(*IPH) _PARAMS((int, ipcache_entry *, caddr_t)); */
 typedef int (*IPH) _PARAMS((int, struct hostent *, caddr_t));
 
-extern int ipcache_nbgethostbyname _PARAMS((char *, int, IPH, caddr_t));
-extern struct hostent *ipcache_gethostbyname _PARAMS((char *));
-extern void ipcache_flush _PARAMS((void));
-extern void ipcache_init _PARAMS((void));
-extern void stat_ipcache_get _PARAMS((StoreEntry *, cacheinfo *));
-
 
 typedef struct _ipcache_entry {
     /* first two items must be equivalent to hash_link in hash.h */
@@ -29,5 +23,12 @@ typedef struct _ipcache_entry {
     struct _ip_pending *pending_head;
     struct _ip_pending *pending_tail;
 } ipcache_entry;
+
+extern int ipcache_nbgethostbyname _PARAMS((char *, int, IPH, caddr_t));
+extern int ipcache_unregister _PARAMS((char *, int));
+extern struct hostent *ipcache_gethostbyname _PARAMS((char *));
+extern void ipcache_flush _PARAMS((void));
+extern void ipcache_init _PARAMS((void));
+extern void stat_ipcache_get _PARAMS((StoreEntry *, cacheinfo *));
 
 #endif
