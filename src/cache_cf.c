d11a63c4db91f8187e257f0a5ec4e5c1d19ed7bd
Duane Wessels <wessels@packet-pushers.com>
1996-04-04 05:10:16 +0000

    use debug() rather than stderr

--- src/cache_cf.c
+++ src/cache_cf.c
@@ -261,8 +261,7 @@ void addToIPACL(list, ip_str, access)
 
 	case 5:
 	    if (m1 < 0 || m1 > 32) {
-		fprintf(stderr,
-		    "cached.conf error in IP acl line %s (ignored)\n",
+		debug(3, 0, "addToIPACL: Ignoring invalid IP acl line '%s'\n",
 		    ip_str);
 		return;
 	    }
@@ -274,8 +273,8 @@ void addToIPACL(list, ip_str, access)
 	    break;
 
 	default:
-	    fprintf(stderr,
-		"cached.conf error in IP acl line %s (ignored)\n", ip_str);
+	    debug(3, 0, "addToIPACL: Ignoring invalid IP acl line '%s'\n",
+		ip_str);
 	    return;
 	}
     }
@@ -460,7 +459,7 @@ static void parseHostDomainLine(line_in)
 static void parseMailTraceLine(line_in)
      char *line_in;
 {
-    fprintf(stderr, "'mail_trace' not supported in this version; ignored.\n");
+    debug(3, 0, "'mail_trace' not supported in this version; ignored.\n");
 }
 
 
