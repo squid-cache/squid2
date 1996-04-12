6684bdaa1166f681f036dea1936023143b692b08
Duane Wessels <wessels@packet-pushers.com>
1996-04-12 00:11:02 +0000

    remove unneeded blocklist stuff

--- src/cache_cf.c
+++ src/cache_cf.c
@@ -956,6 +956,7 @@ static void parseBindAddressLine()
     wordlistAdd(&Config.bind_addr_list, token);
 }
 
+#ifdef OLD_CODE
 static void parseBlockListLine()
 {
     char *token;
@@ -964,6 +965,7 @@ static void parseBlockListLine()
 	return;
     blockAddToList(token);
 }
+#endif
 
 static void parseLocalDomainLine()
 {
@@ -1280,9 +1282,11 @@ int parseConfigFile(file_name)
 	else if (!strcmp(token, "ttl_pattern"))
 	    parseTTLPattern();
 
+#ifdef OLD_CODE
 	/* Parse a blocklist line */
 	else if (!strcmp(token, "blocklist"))
 	    parseBlockListLine();
+#endif
 
 	/* Parse a negative_ttl line */
 	else if (!strcmp(token, "negative_ttl"))
