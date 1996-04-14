be361c5fea493f63297e67ca520c86fc15d0a276
Duane Wessels <wessels@packet-pushers.com>
1996-04-14 03:05:03 +0000

    deprecate has_mem_obj()

--- src/icp.c
+++ src/icp.c
@@ -447,7 +447,7 @@ int icpSendMoreData(fd, state)
 
     debug(12, 5, "icpSendMoreData: <URL:%s> sz %d: len %d: off %d.\n",
 	entry->url, entry->object_len,
-	has_mem_obj(entry) ? entry->mem_obj->e_current_len : 0, state->offset);
+	entry->mem_obj ? entry->mem_obj->e_current_len : 0, state->offset);
 
     p = state->ptr_to_4k_page = buf = get_free_4k_page();
     state->buf = NULL;
