8f3bc8eef716529973d119d695979871e2382c0b
Duane Wessels <wessels@packet-pushers.com>
1996-04-16 23:05:43 +0000

    fix config_file (now ConfigFile) mem problems

--- src/cache_cf.h
+++ src/cache_cf.h
@@ -32,6 +32,8 @@ typedef struct _ip_acl {
     struct _ip_acl *next;
 } ip_acl;
 
+
+/* Global Variables */
 extern int httpd_accel_mode;
 extern int emulate_httpd_log;
 extern int zap_disk_store;
@@ -46,29 +48,37 @@ extern ip_acl *manager_ip_acl;
 extern char config_input_line[];
 extern int config_lineno;
 extern int DnsPositiveTtl;
+extern char w_space[];
+extern intlist *connect_port_list;
+extern char *ConfigFile;	/* the whole thing */
+extern char *cfg_filename;	/* Only the tail component of the path */
+extern char *DefaultSwapDir;	/* argh */
 
 
-/* cache_cf.c */
+/* Global Functions */
 extern char *getAccelPrefix _PARAMS((void));
 extern char *getAccessLogFile _PARAMS((void));
 extern char *getAdminEmail _PARAMS((void));
+extern char *getAnnounceFile _PARAMS((void));
+extern char *getAnnounceHost _PARAMS((void));
 extern char *getAppendDomain _PARAMS((void));
 extern char *getCacheLogFile _PARAMS((void));
 extern char *getDebugOptions _PARAMS((void));
 extern char *getDnsProgram _PARAMS((void));
-extern int getDnsChildren _PARAMS((void));
 extern char *getEffectiveGroup _PARAMS((void));
 extern char *getEffectiveUser _PARAMS((void));
 extern char *getFtpOptions _PARAMS((void));
 extern char *getFtpProgram _PARAMS((void));
-extern char *getHierarchyLogFile _PARAMS((void));
-extern char *getStoreLogFile _PARAMS((void));
-extern char *getWaisRelayHost _PARAMS((void));
-extern char *getPidFilename _PARAMS((void));
-extern char *getVisibleHostname _PARAMS((void));
 extern char *getFtpUser _PARAMS((void));
+extern char *getHierarchyLogFile _PARAMS((void));
+extern char *getPidFilename _PARAMS((void));
+extern char *getStoreLogFile _PARAMS((void));
+extern char *getVisibleHostname _PARAMS((void));
+extern char *getWaisRelayHost _PARAMS((void));
 extern double getCacheHotVmFactor _PARAMS((void));
 extern int getAccelWithProxy _PARAMS((void));
+extern int getAnnouncePort _PARAMS((void));
+extern int getAnnounceRate _PARAMS((void));
 extern int getAsciiPortNum _PARAMS((void));
 extern int getBehindFirewall _PARAMS((void));
 extern int getCacheMemHighWaterMark _PARAMS((void));
@@ -80,8 +90,9 @@ extern int getCacheSwapLowWaterMark _PARAMS((void));
 extern int getCacheSwapMax _PARAMS((void));
 extern int getCleanRate _PARAMS((void));
 extern int getClientLifetime _PARAMS((void));
+extern int getConnectTimeout _PARAMS((void));
 extern int getDnSChildren _PARAMS((void));
-extern int getMaxRequestSize _PARAMS((void));
+extern int getDnsChildren _PARAMS((void));
 extern int getFtpMax _PARAMS((void));
 extern int getFtpTTL _PARAMS((void));
 extern int getGopherMax _PARAMS((void));
@@ -89,42 +100,30 @@ extern int getGopherTTL _PARAMS((void));
 extern int getHttpMax _PARAMS((void));
 extern int getHttpTTL _PARAMS((void));
 extern int getLogfileRotateNumber _PARAMS((void));
-extern int getNegativeTTL _PARAMS((void));
+extern int getMaxRequestSize _PARAMS((void));
 extern int getNegativeDNSTTL _PARAMS((void));
+extern int getNegativeTTL _PARAMS((void));
 extern int getQuickAbort _PARAMS((void));
 extern int getReadTimeout _PARAMS((void));
 extern int getSourcePing _PARAMS((void));
 extern int getStallDelay _PARAMS((void));
 extern int getUdpPortNum _PARAMS((void));
+extern int getWAISMax _PARAMS((void));
 extern int getWaisRelayPort _PARAMS((void));
 extern int ip_acl_match _PARAMS((struct in_addr, ip_acl *));
-
 extern int parseConfigFile _PARAMS((char *file_name));
 extern int setAsciiPortNum _PARAMS((int));
 extern int setCacheSwapMax _PARAMS((int size));
 extern int setUdpPortNum _PARAMS((int));
 extern ip_access_type ip_access_check _PARAMS((struct in_addr, ip_acl *));
-extern int getWAISMax _PARAMS((void));
-extern int getConnectTimeout _PARAMS((void));
-extern char *getAnnounceHost _PARAMS((void));
-extern int getAnnouncePort _PARAMS((void));
-extern char *getAnnounceFile _PARAMS((void));
-extern int getAnnounceRate _PARAMS((void));
-wordlist *getHttpStoplist _PARAMS((void));
+extern void intlistDestroy _PARAMS((intlist **));
+extern void wordlistDestroy _PARAMS((wordlist **));
+wordlist *getBindAddrList _PARAMS((void));
+wordlist *getCacheDirs _PARAMS((void));
 wordlist *getFtpStoplist _PARAMS((void));
 wordlist *getGopherStoplist _PARAMS((void));
-wordlist *getLocalDomainList _PARAMS((void));
-wordlist *getCacheDirs _PARAMS((void));
+wordlist *getHttpStoplist _PARAMS((void));
 wordlist *getInsideFirewallList _PARAMS((void));
-wordlist *getBindAddrList _PARAMS((void));
-extern void wordlistDestroy _PARAMS((wordlist **));
-extern void intlistDestroy _PARAMS((intlist **));
-
-
-extern char w_space[];
-extern intlist *connect_port_list;
-extern char *config_file;	/* the whole thing */
-extern char *cfg_filename;	/* Only the tail component of the path */
-extern char *DefaultSwapDir;	/* argh */
+wordlist *getLocalDomainList _PARAMS((void));
 
 #endif /* ndef  _CACHE_CONFIG_H_ */
--- src/stat.c
+++ src/stat.c
@@ -312,12 +312,11 @@ void squid_get_start(obj, sentry)
      StoreEntry *sentry;
 {
     squid_read_data_t *data;
-    extern char *config_file;
 
     data = (squid_read_data_t *) xmalloc(sizeof(squid_read_data_t));
     memset(data, '\0', sizeof(squid_read_data_t));
     data->sentry = sentry;
-    data->fd = file_open((char *) config_file, NULL, O_RDONLY);
+    data->fd = file_open((char *) ConfigFile, NULL, O_RDONLY);
     storeAppend(sentry, open_bracket, (int) strlen(open_bracket));
     file_walk(data->fd, (FILE_WALK_HD) squidReadEndHandler, (void *) data,
 	(FILE_WALK_LHD) squidReadHandler, (void *) data);
