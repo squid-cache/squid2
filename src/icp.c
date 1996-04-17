528cabb732563a6e882dc645a35e7cd514d459dd
Duane Wessels <wessels@packet-pushers.com>
1996-04-17 15:01:26 +0000

    use negative TTL value as UDP_HIT threshold so we never return HIT on neg-cache objects

--- src/icp.c
+++ src/icp.c
@@ -67,10 +67,6 @@ static icpUdpData *UdpQueueTail = NULL;
 #define ICP_MAX_UDP_SIZE 4096
 #define ICP_SENDMOREDATA_BUF SM_PAGE_SIZE
 
-#if !defined(UDP_HIT_THRESH)
-#define UDP_HIT_THRESH 300
-#endif
-
 typedef void (*complete_handler) _PARAMS((int fd, char *buf, int size, int errflag, void *data));
 typedef struct ireadd {
     int fd;
@@ -938,7 +934,7 @@ int icpHandleUdp(sock, not_used)
 	debug(12, 5, "icpHandleUdp: OPCODE %s\n", IcpOpcodeStr[header.opcode]);
 	if (entry &&
 	    (entry->status == STORE_OK) &&
-	    ((entry->expires - UDP_HIT_THRESH) > squid_curtime)) {
+	    (entry->expires > (squid_curtime + getNegativeTTL()))) {
 	    /* Send "HIT" message. */
 	    CacheInfo->log_append(CacheInfo,	/* UDP_HIT */
 		entry->url,
