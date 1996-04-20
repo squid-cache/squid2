27efe95c3abaed6c8571640adfcdc85b1c5c644e
Duane Wessels <wessels@packet-pushers.com>
1996-04-20 19:42:20 +0000

    Fixed chdir(swappath(0)) bug; do chdir() after storeInit()

--- src/main.c
+++ src/main.c
@@ -233,6 +233,14 @@ static void mainInitialize()
 	stmemInit();
 	writePidFile();
 
+	if (getEffectiveUser()) {
+	    /* we were probably started as root, so cd to a swap
+	     * directory in case we dump core */
+	    if (chdir(swappath(0)) < 0) {
+		debug(1, 0, "%s: %s\n", swappath(0), xstrerror());
+		fatal_dump("Cannot cd to swap directory?");
+	    }
+	}
 	/* after this point we want to see the mallinfo() output */
 	do_mallinfo = 1;
     }
--- src/tools.c
+++ src/tools.c
@@ -341,11 +341,6 @@ void check_suid()
 	return;
     if ((pwd = getpwnam(getEffectiveUser())) == NULL)
 	return;
-    /* change current directory to swap space so we can get core */
-    if (chdir(swappath(0)) < 0) {
-	debug(21, 0, "%s: %s\n", swappath(0), xstrerror());
-	fatal_dump("Cannot cd to swap directory?");
-    }
     if (getEffectiveGroup() && (grp = getgrnam(getEffectiveGroup()))) {
 	setgid(grp->gr_gid);
     } else {
