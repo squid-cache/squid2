9539f75ee4edbb981b6dfa91a10c2aa0d80d1a54
Duane Wessels <wessels@packet-pushers.com>
1996-04-24 15:05:07 +0000

    grammar

--- src/main.c
+++ src/main.c
@@ -336,7 +336,7 @@ int main(argc, argv)
 	    break;
 	case COMM_ERROR:
 	    errcount++;
-	    debug(1, 0, "Select loop Error. Retry. %d\n", errcount);
+	    debug(1, 0, "Select loop Error. Retry %d\n", errcount);
 	    if (errcount == 10)
 		fatal_dump("Select Loop failed!");
 	    break;
