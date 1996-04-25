a274b97bbb3815172e0c757d45a1da14795aeb01
Duane Wessels <wessels@packet-pushers.com>
1996-04-25 16:34:49 +0000

    call xstrerror() if we can't open config file

--- src/cache_cf.c
+++ src/cache_cf.c
@@ -1086,7 +1086,8 @@ int parseConfigFile(file_name)
     aclDestroyAccessList(&ICPAccessList);
 
     if ((fp = fopen(file_name, "r")) == NULL) {
-	sprintf(fatal_str, "Unable to open configuration file: %s", file_name);
+	sprintf(fatal_str, "Unable to open configuration file: %s: %s",
+		file_name, xstrerror());
 	fatal(fatal_str);
     }
     cfg_filename = file_name;
