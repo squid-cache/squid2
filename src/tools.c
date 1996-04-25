176cba1d348c523379f9482a38975eed2cc5960f
Duane Wessels <wessels@packet-pushers.com>
1996-04-25 16:36:30 +0000

    make setrlimit() set max FDs to FD_SETSIZE; be consistent

--- src/tools.c
+++ src/tools.c
@@ -246,6 +246,7 @@ void sig_child(sig)
 #endif
 }
 
+#ifdef OLD_CODE
 /*
  *  getMaxFD - returns the file descriptor table size
  */
@@ -271,6 +272,12 @@ int getMaxFD()
     }
     return (i);
 }
+#else
+int getMaxFD()
+{
+    return FD_SETSIZE;
+}
+#endif
 
 char *getMyHostname()
 {
@@ -405,24 +412,25 @@ void setMaxFD()
     struct rlimit rl;
 #if defined(RLIMIT_NOFILE)
     if (getrlimit(RLIMIT_NOFILE, &rl) < 0) {
-	perror("getrlimit: RLIMIT_NOFILE");
+	debug(21, 0, "setrlimit: RLIMIT_NOFILE: %s", xstrerror());
     } else {
-	rl.rlim_cur = rl.rlim_max;	/* set it to the max */
+	rl.rlim_cur = FD_SETSIZE;
 	if (setrlimit(RLIMIT_NOFILE, &rl) < 0) {
-	    perror("setrlimit: RLIMIT_NOFILE");
+	    sprintf(tmp_error_buf, "setrlimit: RLIMIT_NOFILE: %s", xstrerror());
+	    fatal_dump(tmp_error_buf);
 	}
     }
 #elif defined(RLIMIT_OFILE)
     if (getrlimit(RLIMIT_OFILE, &rl) < 0) {
-	perror("getrlimit: RLIMIT_OFILE");
+	debug(21, 0, "setrlimit: RLIMIT_NOFILE: %s", xstrerror());
     } else {
-	rl.rlim_cur = rl.rlim_max;	/* set it to the max */
+	rl.rlim_cur = FD_SETSIZE;
 	if (setrlimit(RLIMIT_OFILE, &rl) < 0) {
-	    perror("setrlimit: RLIMIT_OFILE");
+	    sprintf(tmp_error_buf, "setrlimit: RLIMIT_OFILE: %s", xstrerror());
+	    fatal_dump(tmp_error_buf);
 	}
     }
 #endif
-    debug(21, 1, "setMaxFD: Using %d file descriptors\n", rl.rlim_max);
 #else /* HAVE_SETRLIMIT */
     debug(21, 1, "setMaxFD: Cannot increase: setrlimit() not supported on this system");
 #endif
