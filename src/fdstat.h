585ec9e7e6a7aa0213b9db12d2652bf609af42fb
Duane Wessels <wessels@packet-pushers.com>
1996-04-25 03:43:55 +0000

    rearrange enum defn

--- src/fdstat.h
+++ src/fdstat.h
@@ -1,104 +1,27 @@
-/* 
- * $Id$
- *
- *  File:         fdstat.h
- *  Description:  Header of file descriptor stat module
- *  Author:       Anawat Chankhunthod, USC
- *  Created:      Fri Jul  1 00:09:05 PDT 1994
- *  Language:     C
- **********************************************************************
- *  Copyright (c) 1994, 1995.  All rights reserved.
- *  
- *    The Harvest software was developed by the Internet Research Task
- *    Force Research Group on Resource Discovery (IRTF-RD):
- *  
- *          Mic Bowman of Transarc Corporation.
- *          Peter Danzig of the University of Southern California.
- *          Darren R. Hardy of the University of Colorado at Boulder.
- *          Udi Manber of the University of Arizona.
- *          Michael F. Schwartz of the University of Colorado at Boulder.
- *          Duane Wessels of the University of Colorado at Boulder.
- *  
- *    This copyright notice applies to software in the Harvest
- *    ``src/'' directory only.  Users should consult the individual
- *    copyright notices in the ``components/'' subdirectories for
- *    copyright information about other software bundled with the
- *    Harvest source code distribution.
- *  
- *  TERMS OF USE
- *    
- *    The Harvest software may be used and re-distributed without
- *    charge, provided that the software origin and research team are
- *    cited in any use of the system.  Most commonly this is
- *    accomplished by including a link to the Harvest Home Page
- *    (http://harvest.cs.colorado.edu/) from the query page of any
- *    Broker you deploy, as well as in the query result pages.  These
- *    links are generated automatically by the standard Broker
- *    software distribution.
- *    
- *    The Harvest software is provided ``as is'', without express or
- *    implied warranty, and with no support nor obligation to assist
- *    in its use, correction, modification or enhancement.  We assume
- *    no liability with respect to the infringement of copyrights,
- *    trade secrets, or any patents, and are not responsible for
- *    consequential damages.  Proper use of the Harvest software is
- *    entirely the responsibility of the user.
- *  
- *  DERIVATIVE WORKS
- *  
- *    Users may make derivative works from the Harvest software, subject 
- *    to the following constraints:
- *  
- *      - You must include the above copyright notice and these 
- *        accompanying paragraphs in all forms of derivative works, 
- *        and any documentation and other materials related to such 
- *        distribution and use acknowledge that the software was 
- *        developed at the above institutions.
- *  
- *      - You must notify IRTF-RD regarding your distribution of 
- *        the derivative work.
- *  
- *      - You must clearly notify users that your are distributing 
- *        a modified version and not the original Harvest software.
- *  
- *      - Any derivative product is also subject to these copyright 
- *        and use restrictions.
- *  
- *    Note that the Harvest software is NOT in the public domain.  We
- *    retain copyright, as specified above.
- *  
- *  HISTORY OF FREE SOFTWARE STATUS
- *  
- *    Originally we required sites to license the software in cases
- *    where they were going to build commercial products/services
- *    around Harvest.  In June 1995 we changed this policy.  We now
- *    allow people to use the core Harvest software (the code found in
- *    the Harvest ``src/'' directory) for free.  We made this change
- *    in the interest of encouraging the widest possible deployment of
- *    the technology.  The Harvest software is really a reference
- *    implementation of a set of protocols and formats, some of which
- *    we intend to standardize.  We encourage commercial
- *    re-implementations of code complying to this set of standards.  
- *  
- *  
- */
+/* $Id$ */
+
 #ifndef FDSTAT_H
 #define FDSTAT_H
-
-#include "ansihelp.h"
 
 #define PREOPEN_FD 3		/* number of preopened fd when process start */
 
 typedef enum {
-    LOG, File, Socket, Pipe, Unknown
+    LOG,
+    File,
+    Socket,
+    Pipe,
+    Unknown
 } File_Desc_Type;
 
-extern int fdstat_biggest_fd();
-extern int fdstat_init _PARAMS((int preopen));
-extern void fdstat_open _PARAMS((int fd, File_Desc_Type type));
-extern void fdstat_close _PARAMS((int fd));
-extern int fdstat_isopen _PARAMS((int fd));
 extern File_Desc_Type fdstat_type _PARAMS((int fd));
 extern char *fd_describe _PARAMS((int fd));
+extern int fdstat_biggest_fd _PARAMS((void));
+extern int fdstat_init _PARAMS((int preopen));
+extern int fdstat_isopen _PARAMS((int fd));
+extern void fdstat_close _PARAMS((int fd));
+extern void fdstat_open _PARAMS((int fd, File_Desc_Type type));
+extern int fdstat_are_n_free_fd _PARAMS((int));
+extern File_Desc_Type fdstatGetType _PARAMS((int));
+
 
 #endif
