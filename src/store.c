d8250506839243f45bf4f3acb35a3711f9c61ced
Duane Wessels <wessels@packet-pushers.com>
1996-04-30 19:47:50 +0000

    got to recalc newkey becuase its static memory

--- src/store.c
+++ src/store.c
@@ -579,7 +579,8 @@ void storeSetPublicKey(e)
 	storeSetPrivateKey(e2);
 	storeReleaseRequest(e2);
 	if (loop_detect++ == 10)
-		fatal_dump("storeSetPublicKey() is looping!!");
+	    fatal_dump("storeSetPublicKey() is looping!!");
+	newkey = storeGeneratePublicKey(e->url, e->method);
     }
     if (e->key)
 	storeHashDelete(e);
