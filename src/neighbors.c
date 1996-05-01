5fca7d7c24c496bfce8f8746b62fbddef2f8339a
Duane Wessels <wessels@packet-pushers.com>
1996-05-01 19:18:29 +0000

    log milliseconds to hierarchy log

--- src/neighbors.c
+++ src/neighbors.c
@@ -104,15 +104,17 @@ void hierarchy_log_append(url, code, timeout, cache_host)
 	}
     } else {
 	if (cache_host) {
-	    fprintf(cache_hierarchy_log, "%d %s %s%s %s\n",
-		(int) squid_curtime,
+	    fprintf(cache_hierarchy_log, "%d.%03d %s %s%s %s\n",
+		(int) current_time.tv_sec,
+		(int) current_time.tv_usec / 1000,
 		url,
 		timeout ? "TIMEOUT_" : "",
 		hier_strings[code],
 		cache_host);
 	} else {
-	    fprintf(cache_hierarchy_log, "%d %s %s%s\n",
-		(int) squid_curtime,
+	    fprintf(cache_hierarchy_log, "%d.%03d %s %s%s\n",
+		(int) current_time.tv_sec,
+		(int) current_time.tv_usec / 1000,
 		url,
 		timeout ? "TIMEOUT_" : "",
 		hier_strings[code]);
