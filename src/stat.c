7f8ab5cbe883fe2909f0ea94e994e07a0c2bcffb
Duane Wessels <wessels@packet-pushers.com>
1996-05-01 19:15:34 +0000

    elaborate on logfile open error

--- src/stat.c
+++ src/stat.c
@@ -983,8 +983,7 @@ void stat_init(object, logfilename)
     memcpy(obj->logfilename, logfilename, (int) (strlen(logfilename) + 1) % 256);
     obj->logfile_fd = file_open(obj->logfilename, NULL, O_RDWR | O_CREAT);
     if (obj->logfile_fd == DISK_ERROR) {
-	debug(18, 0, "Cannot open logfile: %s\n", obj->logfilename);
-	obj->logfile_status = LOG_DISABLE;
+	debug(18, 0, "%s: %s\n", obj->logfilename, xstrerror());
 	fatal("Cannot open logfile.");
     }
     obj->logfile_access = file_write_lock(obj->logfile_fd);
