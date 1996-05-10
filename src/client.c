407924c2509bb985cb9d5f2166d7e01fb36daa4d
Duane Wessels <wessels@packet-pushers.com>
1996-05-10 21:32:20 +0000

    fixed usage instructions

--- src/client.c
+++ src/client.c
@@ -15,13 +15,14 @@ static void usage(progname)
      char *progname;
 {
     fprintf(stderr, "\
-Usage: %s [-rs] [-h host] [-p port] url\n\
+Usage: %s [-rs] [-i IMS_time] [-h host] [-p port] [-m method] url\n\
 Options:\n\
     -r         Force cache to reload URL.\n\
     -s         Silent.  Do not print data to stdout.\n\
+    -i IMS     If-Modified-Since time (in Epoch seconds).\n\
     -h host    Retrieve URL from cache on hostname.  Default is localhost.\n\
     -p port    Port number of cache.  Default is %d.\n\
-    -m method  Request method, default is GET\n\
+    -m method  Request method, default is GET.\n\
 ", progname, CACHE_HTTP_PORT);
     exit(1);
 }
