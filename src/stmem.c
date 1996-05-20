d735ac33996cffb5b47c0c1f25e5d2e03089d8a3
Duane Wessels <wessels@packet-pushers.com>
1996-05-20 15:13:16 +0000

    force coredump

--- src/stmem.c
+++ src/stmem.c
@@ -211,6 +211,9 @@ int memCopy(mem, offset, buf, size)
 
     debug(19, 6, "memCopy: offset %d: size %d\n", offset, size);
 
+    if (p == NULL)
+	fatal_dump("memCopy: NULL mem_node");
+
     if (size <= 0)
 	return size;
 
