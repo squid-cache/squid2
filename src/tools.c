90980b529b2289e7828e201dedf535abe6a0e03b
Duane Wessels <wessels@packet-pushers.com>
1996-05-21 16:21:43 +0000

    From:    Taavi Talvik <taavi@uninet.ee> It seems that some systems need following patch. At least in FreeBSD default datasize limit is not set to hard limit.

--- src/tools.c
+++ src/tools.c
@@ -381,7 +381,19 @@ void setMaxFD()
 #endif
 #else /* HAVE_SETRLIMIT */
     debug(21, 1, "setMaxFD: Cannot increase: setrlimit() not supported on this system");
-#endif
+#endif /* HAVE_SETRLIMIT */
+
+#if HAVE_SETRLIMIT && defined(RLIMIT_DATA)
+    if (getrlimit(RLIMIT_DATA, &rl) < 0) {
+	debug(21, 0, "getrlimit: RLIMIT_DATA: %s", xstrerror());
+    } else {
+	rl.rlim_cur = rl.rlim_max;	/* set it to the max */
+	if (setrlimit(RLIMIT_DATA, &rl) < 0) {
+	    sprintf(tmp_error_buf, "setrlimit: RLIMIT_DATA: %s", xstrerror());
+	    fatal_dump(tmp_error_buf);
+	}
+    }
+#endif /* RLIMIT_DATA */
 }
 
 time_t getCurrentTime()
