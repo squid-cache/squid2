8d0a00e9f8da6abda157f8bb67a64fdb260a4fbc
Duane Wessels <wessels@packet-pushers.com>
1996-05-29 17:37:03 +0000

    fix setuid problems with writing pid file

--- src/tools.c
+++ src/tools.c
@@ -351,17 +351,18 @@ void writePidFile()
     FILE *pid_fp = NULL;
     char *f = NULL;
 
-    enter_suid();
     if ((f = getPidFilename()) == NULL)
 	return;
-    if ((pid_fp = fopen(f, "w")) == NULL) {
+    enter_suid();
+    pid_fp = fopen(f, "w");
+    leave_suid();
+    if (pid_fp != NULL) {
+        fprintf(pid_fp, "%d\n", (int) getpid());
+        fclose(pid_fp);
+    } else {
 	debug(21, 0, "WARNING: Could not write pid file\n");
 	debug(21, 0, "         %s: %s\n", f, xstrerror());
-	return;
     }
-    fprintf(pid_fp, "%d\n", (int) getpid());
-    fclose(pid_fp);
-    leave_suid();
 }
 
 
