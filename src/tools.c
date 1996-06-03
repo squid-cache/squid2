5535cb6e5a2332bcafcfa4efbec9f04936786d4f
Duane Wessels <wessels@packet-pushers.com>
1996-06-03 03:42:25 +0000

    use >> 10 instead of / 1000

--- src/tools.c
+++ src/tools.c
@@ -108,7 +108,7 @@ static int PrintRusage(f, lf)
     fprintf(lf, "CPU Usage: user %d sys %d\n",
 	(int) rusage.ru_utime.tv_sec, (int) rusage.ru_stime.tv_sec);
     fprintf(lf, "Memory Usage: rss %ld KB\n",
-	rusage.ru_maxrss * getpagesize() / 1000);
+	rusage.ru_maxrss * getpagesize() >> 10);
     fprintf(lf, "Page faults with physical i/o: %ld\n",
 	rusage.ru_majflt);
 #endif
