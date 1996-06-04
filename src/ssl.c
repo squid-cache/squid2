4bb700de68ed8235b1dead332bf1878dc19a68ef
Duane Wessels <wessels@packet-pushers.com>
1996-06-04 03:23:36 +0000

    remove old FD mismatch check

--- src/ssl.c
+++ src/ssl.c
@@ -239,8 +239,6 @@ static void sslReadTimeout(fd, sslState)
      int fd;
      SslStateData *sslState;
 {
-    if (fd != sslState->server.fd)
-	fatal_dump("sslReadTimeout: FD mismatch!\n");
     debug(26, 3, "sslReadTimeout: FD %d\n", fd);
     comm_close(sslState->client.fd);
     comm_close(sslState->server.fd);
