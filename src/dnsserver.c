9c07ec9e99e432087b6b4d9f7a580a9280a68ac3
Duane Wessels <wessels@packet-pushers.com>
1996-07-01 20:51:17 +0000

    Removed extra newline on dnsserver error messages

--- src/dnsserver.c
+++ src/dnsserver.c
@@ -183,7 +183,7 @@ int main(argc, argv)
 		fflush(logfile);
 	    }
 	    printf("$fail %s\n", request);
-	    printf("$message %s\n", msg[0] ? msg : "Unknown Error");
+	    printf("$message %s", msg[0] ? msg : "Unknown Error\n");
 	    printf("$end\n");
 	    fflush(stdout);
 	    continue;
