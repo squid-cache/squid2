02f3710422f848608a2cccb4784e4f1492c5ec57
Duane Wessels <wessels@packet-pushers.com>
1996-07-09 23:01:16 +0000

    clean up fdstat_type muck

--- src/disk.c
+++ src/disk.c
@@ -228,7 +228,7 @@ int file_close(fd)
 	file_table[fd].write_daemon = NOT_PRESENT;
 	file_table[fd].filename[0] = '\0';
 
-	if (fdstat_type(fd) == FD_SOCKET) {
+	if (fdstatGetType(fd) == FD_SOCKET) {
 	    debug(6, 0, "FD %d: Someone called file_close() on a socket\n", fd);
 	    fatal_dump(NULL);
 	}
