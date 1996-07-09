b06ad850b81b25876c27eab4d5da2cd30434a553
Duane Wessels <wessels@packet-pushers.com>
1996-07-09 19:17:44 +0000

    Add CPU Usage percent

--- src/stat.c
+++ src/stat.c
@@ -573,8 +573,13 @@ void info_get(obj, sentry)
     storeAppendPrintf(sentry, "{Resource usage for %s:}\n", appname);
 
     getrusage(RUSAGE_SELF, &rusage);
-    storeAppendPrintf(sentry, "{\tCPU Usage: user %d sys %d}\n",
-	(int) rusage.ru_utime.tv_sec, (int) rusage.ru_stime.tv_sec);
+    storeAppendPrintf(sentry, "{\tCPU Time: %d seconds (%d user %d sys)}\n",
+	(int) rusage.ru_utime.tv_sec + (int) rusage.ru_stime.tv_sec,
+	(int) rusage.ru_utime.tv_sec,
+	(int) rusage.ru_stime.tv_sec);
+    storeAppendPrintf(sentry, "{\tCPU Usage: %d%%}\n",
+	percent(rusage.ru_utime.tv_sec + rusage.ru_stime.tv_sec,
+	    squid_curtime - squid_starttime));
     storeAppendPrintf(sentry, "{\tProcess Size: rss %ld KB}\n",
 	rusage.ru_maxrss * getpagesize() >> 10);
 
