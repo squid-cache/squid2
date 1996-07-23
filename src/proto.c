3f3c3b58eb34a1eb10ab700aec5288dea503030b
Duane Wessels <wessels@packet-pushers.com>
1996-07-23 04:16:55 +0000

    -fix up protoUndispatch().  Always call it, pass in src_addr so we can  unregister the reverse lookup.  Also, rename it protoUnregister().

--- src/proto.c
+++ src/proto.c
@@ -369,33 +369,33 @@ int protoDispatch(fd, url, entry, request)
     return 0;
 }
 
-/* Use to undispatch a particular url/fd from DNS pending list */
-/* I have it here because the code that understand protocol/url */
-/* should be here. */
-int protoUndispatch(fd, url, entry, request)
+void protoUnregister(fd, entry, request, src_addr)
      int fd;
-     char *url;
      StoreEntry *entry;
      request_t *request;
+     struct in_addr src_addr;
 {
+    char *url = NULL;
+    if (entry)
+	url = entry->url;
     debug(17, 5, "protoUndispatch FD %d <URL:%s>\n", fd, url);
 
     /* Cache objects don't need to be unregistered  */
     if (request->protocol == PROTO_CACHEOBJ)
-	return 0;
+	return;
 
-    (void) redirectUnregister(url, fd);
-    /* clean up DNS pending list for this name/fd look up here */
+    if (url)
+	(void) redirectUnregister(url, fd);
+    (void) fqdncacheUnregister(src_addr, fd);
     if (!ipcache_unregister(request->host, fd)) {
 	debug(17, 5, "protoUndispatch: ipcache failed to unregister '%s'\n",
 	    request->host);
-	return 0;
+	return;
     }
     /* The pending DNS lookup was cleared, now have to junk the entry */
-    debug(17, 5, "protoUndispatch: the entry is stranded with a pending DNS event\n");
+    debug(17, 5, "protoUndispatch: entry is stranded with pending DNS event\n");
     if (entry)
 	squid_error_entry(entry, ERR_CLIENT_ABORT, NULL);
-    return 1;
 }
 
 void protoCancelTimeout(fd, entry)
