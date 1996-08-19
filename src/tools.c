864612f27045429aef92da4db0c4976c04471d7d
Duane Wessels <wessels@packet-pushers.com>
1996-08-19 19:52:17 +0000

    check SA_RESETHAND instead of HAVE_SIGACTION when disabling signal handlers

--- src/tools.c
+++ src/tools.c
@@ -232,10 +232,10 @@ void death(sig)
     if (sig == SIGSEGV)
 	fprintf(debug_log, "FATAL: Received Segment Violation...dying.\n");
     else if (sig == SIGBUS)
-	fprintf(debug_log, "FATAL: Received bus error...dying.\n");
+	fprintf(debug_log, "FATAL: Received Bus Error...dying.\n");
     else
 	fprintf(debug_log, "FATAL: Received signal %d...dying.\n", sig);
-#if !HAVE_SIGACTION
+#if SA_RESETHAND == 0
     signal(SIGSEGV, SIG_DFL);
     signal(SIGBUS, SIG_DFL);
     signal(sig, SIG_DFL);
