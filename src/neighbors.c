c29280d1ee123a201de11d22dded6348b4d99cc3
Duane Wessels <wessels@packet-pushers.com>
1996-08-23 21:18:56 +0000

    check swap_status in neighborsUdpPing()

--- src/neighbors.c
+++ src/neighbors.c
@@ -422,6 +422,8 @@ int neighborsUdpPing(proto)
 	return 0;
 
     for (i = 0, e = friends->first_ping; i++ < friends->n; e = e->next) {
+	if (entry->swap_status != NO_SWAP)
+		fatal_dump("neighborsUdpPing: bad swap_status");
 	if (e == (edge *) NULL)
 	    e = friends->edges_head;
 	debug(15, 5, "neighborsUdpPing: Edge %s\n", e->host);
