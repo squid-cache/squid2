b44f82023aadf51d703b442b2a135d9d480f6aa4
Duane Wessels <wessels@packet-pushers.com>
1996-08-23 21:15:45 +0000

    remove restrictions on hot_vm_factor

--- src/cache_cf.c
+++ src/cache_cf.c
@@ -1442,15 +1442,4 @@ static void configDoConfigure()
 	getMyHostname(), Config.Port.http);
     if (Config.errHtmlText == NULL)
 	Config.errHtmlText = xstrdup("");
-
-#if !ALLOW_HOT_CACHE
-    if (!httpd_accel_mode || Config.Accel.withProxy) {
-	/* Not running strict httpd_accel--force hot_vm_factor to be 0 */
-	if (Config.hotVmFactor != 0.0) {
-	    printf("WARNING: Non-zero hot_vm_factor not allowed unless running only\n");
-	    printf("         in httpd_accel mode.  Setting hot_vm_factor to 0.\n");
-	    Config.hotVmFactor = 0.0;
-	}
-    }
-#endif /* !ALLOW_HOT_CACHE */
 }
