77340bede79e5aaead95d94066ecf7c471c45355
Duane Wessels <wessels@packet-pushers.com>
1996-08-26 19:07:13 +0000

    gindent

--- src/neighbors.c
+++ src/neighbors.c
@@ -496,9 +496,9 @@ int neighborsUdpPing(proto)
     if (friends->edges_head == NULL)
 	return 0;
     if (theOutIcpConnection < 0) {
-	debug(15,0,"neighborsUdpPing: There is no ICP socket!\n");
-	debug(15,0,"Cannot query neighbors for '%s'.\n", url);
-	debug(15,0,"Check 'icp_port' in your config file\n");
+	debug(15, 0, "neighborsUdpPing: There is no ICP socket!\n");
+	debug(15, 0, "Cannot query neighbors for '%s'.\n", url);
+	debug(15, 0, "Check 'icp_port' in your config file\n");
 	fatal_dump(NULL);
     }
     for (i = 0, e = friends->first_ping; i++ < friends->n; e = e->next) {
