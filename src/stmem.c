eaf18f110fc067c773f3ae5a21d33191653c6137
Duane Wessels <wessels@packet-pushers.com>
1996-08-27 05:17:50 +0000

    check for putting NULL pointers

--- src/stmem.c
+++ src/stmem.c
@@ -420,6 +420,8 @@ static void put_free_thing(thing, p)
      stmem_stats *thing;
      void *p;
 {
+    if (p == NULL)
+	fatal_dump("Somebody is putting a NULL pointer!");
     thing->n_pages_in_use--;
     if (thing->total_pages_allocated > thing->max_pages) {
 	xfree(p);
