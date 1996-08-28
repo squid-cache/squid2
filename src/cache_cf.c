6311fbf8a11d37e217167311294a824b210f0795
Duane Wessels <wessels@packet-pushers.com>
1996-08-28 20:11:30 +0000

    set announce_rate and clean_rate to one year if they are disabled

--- src/cache_cf.c
+++ src/cache_cf.c
@@ -1353,14 +1353,10 @@ int parseConfigFile(file_name)
 	printf("         Change your configuration file.\n");
 	fflush(stdout);		/* print message */
     }
-    if (Config.cleanRate > -1 && Config.cleanRate < 60) {
-	Config.cleanRate = (30 * 60);
-	printf("WARNING: clean_rate is less than one minute.\n");
-	printf("         This will cause serious problems with your cache!!!\n");
-	printf("         Change your configuration file.\n");
-	printf("         For this run, however, %s will use %d minutes for clean_rate.\n", appname, (int) (Config.cleanRate / 60));
-	fflush(stdout);		/* print message */
-    }
+    if (Config.cleanRate < 1)
+	Config.cleanRate = 86400 * 365;	/* one year */
+    if (Config.Announce.rate < 1)
+	Config.Announce.rate = 86400 * 365;	/* one year */
     if (Config.dnsChildren < 1) {
 	printf("WARNING: dns_children was set to a bad value: %d\n",
 	    Config.dnsChildren);
