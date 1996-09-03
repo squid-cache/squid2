41c36a41d1dafbcbe150e07e5bf80d308b26e053
Duane Wessels <wessels@packet-pushers.com>
1996-09-03 17:27:09 +0000

    gindent

--- src/http.c
+++ src/http.c
@@ -273,11 +273,11 @@ void httpParseHeaders(buf, reply)
 	t = strtok(NULL, "\n");
     }
 #if LOG_TIMESTAMPS
-	fprintf(timestamp_log, "T %9d D %9d L %9d E %9d\n",
-		squid_curtime,
-		parse_rfc850(reply->date),
-		parse_rfc850(reply->last_modified),
-		parse_rfc850(reply->expires));
+    fprintf(timestamp_log, "T %9d D %9d L %9d E %9d\n",
+	squid_curtime,
+	parse_rfc850(reply->date),
+	parse_rfc850(reply->last_modified),
+	parse_rfc850(reply->expires));
 #endif /* LOG_TIMESTAMPS */
     safe_free(headers);
 }
