acd41c2f4fe88cc66bd4c086d704450dd894cf38
Duane Wessels <wessels@packet-pushers.com>
1996-09-11 20:48:40 +0000

    - add Date: header to Squid-generated Gopher objects (Geoffrey Keating).

--- src/gopher.c
+++ src/gopher.c
@@ -239,7 +239,9 @@ void gopherMimeCreate(data)
     sprintf(tempMIME, "\
 HTTP/1.0 200 OK Gatewaying\r\n\
 Server: Squid/%s\r\n\
-MIME-version: 1.0\r\n", version_string);
+Date: %s\r\n\
+MIME-version: 1.0\r\n",
+	version_string,	mkrfc850(&squid_curtime));
 
     switch (data->type_id) {
 
