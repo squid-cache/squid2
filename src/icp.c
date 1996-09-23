160f20d19c1d203cc480bd59f6912b13bc220299
Duane Wessels <wessels@packet-pushers.com>
1996-09-23 22:14:17 +0000

    only use IMS for HTTP requests

--- src/icp.c
+++ src/icp.c
@@ -748,7 +748,7 @@ icpProcessRequest(int fd, icpStateData * icpState)
 	ipcacheReleaseInvalid(icpState->request->host);
 	entry = NULL;
 	icpState->log_type = LOG_TCP_USER_REFRESH;
-    } else if (!storeEntryValidToSend(entry)) {
+    } else if (!storeEntryValidToSend(entry) && request->protocol == PROTO_HTTP) {
 	/* The object is in the cache, but is not valid.  Use
 	 * LOG_TCP_EXPIRED_MISS for the time being, maybe change it to
 	 * _HIT later in icpHandleIMSReply() */
