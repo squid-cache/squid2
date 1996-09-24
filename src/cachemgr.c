3596f8e93e4b7374143f1bba7c09c8893fd3b3a8
Duane Wessels <wessels@packet-pushers.com>
1996-09-24 18:44:29 +0000

    From:    webadm@cc.fh-lippe.de (Cord Beermann, Webmaster der FH Lippe) Here is a small fix, which corrects the Cachemgr.cgi-Output for non-table-Browsers.

--- src/cachemgr.c
+++ src/cachemgr.c
@@ -881,13 +881,13 @@ main(int argc, char *argv[])
 			s1, &d1, &d2, &d3, &d4, &f1, &d5, &d6, &d7);
 		    if (sn == 1) {
 			if (hasTables)
-			    printf("<tr><td align=\"right\"><B>%s</B>", s1);
+			    printf("<tr><td align=\"right\"><STRONG>%s</STRONG>\n", s1);
 			else
-			    printf("%8s", s1);
+			    printf("%s-Requests\n", s1);
 			break;
 		    }
 		    if (hasTables)
-			printf("<tr><td align=\"right\"><B>%s</B><td align=\"right\">%d<td align=\"right\">%d<td align=\"right\">%d<td align=\"right\">%d<td align=\"right\">%4.2f<td align=\"right\">%d<td align=\"right\">%d<td align=\"right\">%d",
+			printf("<tr><td align=\"right\"><STRONG>%s</STRONG><td align=\"right\">%d<td align=\"right\">%d<td align=\"right\">%d<td align=\"right\">%d<td align=\"right\">%4.2f<td align=\"right\">%d<td align=\"right\">%d<td align=\"right\">%d\n",
 			    s1, d1, d2, d3, d4, f1, d5, d6, d7);
 		    else
 			printf("%8s %7d %9d %9d %9d %4.2f %6d %9d %10d\n",
