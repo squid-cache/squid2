86dca5ecc318b72def991cf137c3ba10dbb420eb
Duane Wessels <wessels@packet-pushers.com>
1996-09-24 20:16:37 +0000

    1.0.17 merge

--- src/ipcache.c
+++ src/ipcache.c
@@ -350,6 +350,8 @@ ipcache_purgelru(void)
 	    continue;
 	if (i->status == IP_DISPATCHED)
 	    continue;
+	if (i->locks != 0)
+	    continue;
 	local_ip_notpending_count++;
 	LRU_list[LRU_list_count++] = i;
     }
@@ -874,9 +876,10 @@ static void
 ipcacheStatPrint(ipcache_entry * i, StoreEntry * sentry)
 {
     int k;
-    storeAppendPrintf(sentry, " {%-32.32s  %c %6d %6d %d",
+    storeAppendPrintf(sentry, " {%-32.32s  %c%c %6d %6d %d",
 	i->name,
 	ipcache_status_char[i->status],
+	i->locks ? 'L' : ' ',
 	(int) (squid_curtime - i->lastref),
 	(int) (i->expires - squid_curtime),
 	i->addr_count);
--- src/tools.c
+++ src/tools.c
@@ -305,7 +305,7 @@ fatal_common(char *message)
 {
 #if HAVE_SYSLOG
     if (opt_syslog_enable)
-	syslog(LOG_ALERT, message);
+	syslog(LOG_ALERT, "%s", message);
 #endif
     fprintf(debug_log, "FATAL: %s\n", message);
     fprintf(debug_log, "Squid Cache (Version %s): Terminated abnormally.\n",
