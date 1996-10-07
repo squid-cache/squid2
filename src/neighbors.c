d838cd421972ae46ada0949f286b68a806ea6654
Duane Wessels <wessels@packet-pushers.com>
1996-10-07 14:59:05 +0000

    better explain "Found non-ICP cache as SIBLING"

--- src/neighbors.c
+++ src/neighbors.c
@@ -644,7 +644,8 @@ neighborsUdpAck(int fd, char *url, icp_common_t * header, struct sockaddr_in *fr
 	    debug(15, 1, "Ignoring DECHO from non-neighbor %s\n",
 		inet_ntoa(from->sin_addr));
 	} else if (e->type == EDGE_SIBLING) {
-	    fatal_dump("neighborsUdpAck: Found non-ICP cache as SIBLING\n");
+	    debug_trap("neighborsUdpAck: Found non-ICP cache as SIBLING\n");
+	    debug_trap("neighborsUdpAck: non-ICP neighbors must be a PARENT\n");
 	} else {
 	    w_rtt = tvSubMsec(mem->start_ping, current_time) / e->weight;
 	    if (mem->w_rtt == 0 || w_rtt < mem->w_rtt) {
