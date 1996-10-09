fdbf69f0d15bb664d9c6202eb788f947a83c40d4
Duane Wessels <wessels@packet-pushers.com>
1996-10-09 17:37:09 +0000

    null sockaddr

--- src/ftpget.c
+++ src/ftpget.c
@@ -860,6 +860,7 @@ connect_with_timeout(int fd, struct sockaddr_in *S, int len)
     if (outgoingTcpAddr.s_addr) {
 	struct sockaddr_in sock;
 
+        memset(&sock, '\0', sizeof(struct sockaddr_in));
 	sock.sin_family = AF_INET;
 	sock.sin_addr = outgoingTcpAddr;
 	sock.sin_port = 0;
