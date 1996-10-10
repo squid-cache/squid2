be276db0c78169210276990574e6b32634b8b999
Duane Wessels <wessels@packet-pushers.com>
1996-10-10 22:19:56 +0000

    squeeze another 4 bytes out of StoreEntry

--- src/store.h
+++ src/store.h
@@ -239,8 +239,8 @@ struct sentry {
     /* to stru which can be freed while object is purged out from memory */
     MemObject *mem_obj;
 
-    u_num32 flag;
-    u_num32 refcount;
+    u_short flag;
+    u_short refcount;
     time_t timestamp;
     time_t lastref;
     time_t expires;
