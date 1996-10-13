b69ccad0816776014aa4962d7e349a27d862062b
Duane Wessels <wessels@packet-pushers.com>
1996-10-13 09:17:13 +0000

    compiler warnings

--- src/main.c
+++ src/main.c
@@ -716,7 +716,7 @@ sendSignal(void)
 	    !(opt_send_signal == 0 && errno == EPERM)) {
 	    fprintf(stderr, "%s: ERROR: Could not send ", appname);
 	    fprintf(stderr, "signal %d to process %d: %s\n",
-		opt_send_signal, pid, xstrerror());
+		opt_send_signal, (int) pid, xstrerror());
 	    exit(1);
 	}
     } else {
