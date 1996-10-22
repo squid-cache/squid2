d5301483a738827b794a2f11eebef8102f6ffee0
Duane Wessels <wessels@packet-pushers.com>
1996-10-22 03:36:32 +0000

    - Fixed changing the wrong instance of 'opt_catch_signals'           in tools.c for beta8tools.c for beta8

--- src/tools.c
+++ src/tools.c
@@ -344,7 +344,7 @@ fatal_dump(char *message)
 {
     if (message)
 	fatal_common(message);
-    if (!opt_catch_signals)
+    if (opt_catch_signals)
 	storeWriteCleanLog();
     abort();
 }
@@ -353,7 +353,7 @@ fatal_dump(char *message)
 void
 _debug_trap(char *message)
 {
-    if (opt_catch_signals)
+    if (!opt_catch_signals)
 	fatal_dump(message);
     _db_print(0, 0, "WARNING: %s\n", message);
 }
