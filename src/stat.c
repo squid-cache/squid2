18c43fc5ef6ec2f058619a5596eae9bd8f6325dd
Duane Wessels <wessels@packet-pushers.com>
1996-10-24 06:11:56 +0000

    fix process RSS for SGI

--- src/stat.c
+++ src/stat.c
@@ -769,8 +769,13 @@ info_get(cacheinfo * obj, StoreEntry * sentry)
     storeAppendPrintf(sentry, "{\tCPU Usage: %d%%}\n",
 	percent(rusage.ru_utime.tv_sec + rusage.ru_stime.tv_sec,
 	    squid_curtime - squid_starttime));
+#ifdef _SQUID_SGI_
     storeAppendPrintf(sentry, "{\tProcess Size: rss %ld KB}\n",
-	rusage.ru_maxrss * getpagesize() >> 10);
+	rusage.ru_maxrss);
+#else /* _SQUID_SGI_ */
+    storeAppendPrintf(sentry, "{\tProcess Size: rss %ld KB}\n",
+	(rusage.ru_maxrss * getpagesize()) >> 10);
+#endif /* _SQUID_SGI_ */
     storeAppendPrintf(sentry, "{\tPage faults with physical i/o: %ld}\n",
 	rusage.ru_majflt);
 #endif
--- src/tools.c
+++ src/tools.c
@@ -210,11 +210,15 @@ PrintRusage(void (*f) (void), FILE * lf)
     getrusage(RUSAGE_SELF, &rusage);
     fprintf(lf, "CPU Usage: user %d sys %d\n",
 	(int) rusage.ru_utime.tv_sec, (int) rusage.ru_stime.tv_sec);
+#ifdef _SQUID_SGI_
+    fprintf(lf, "Memory Usage: rss %ld KB\n", rusage.ru_maxrss);
+#else
     fprintf(lf, "Memory Usage: rss %ld KB\n",
-	rusage.ru_maxrss * getpagesize() >> 10);
+	(rusage.ru_maxrss * getpagesize()) >> 10);
+#endif /* _SQUID_SGI_ */
     fprintf(lf, "Page faults with physical i/o: %ld\n",
 	rusage.ru_majflt);
-#endif
+#endif /* HAVE_GETRUSAGE */
     dumpMallocStats(lf);
     if (f)
 	f();
