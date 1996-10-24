5eb9a7ad0a9996bae357c5783cfaf3ce4241c293
Duane Wessels <wessels@packet-pushers.com>
1996-10-24 21:52:53 +0000

    print URL for releaseRequest on unlocked objects

--- src/store.c
+++ src/store.c
@@ -565,6 +565,7 @@ storeReleaseRequest(StoreEntry * e)
 	return;
     if (!storeEntryLocked(e)) {
 	debug_trap("Someone called storeReleaseRequest on an unlocked entry");
+	debug(20,0,"  --> '%s'\n", e->url ? e->url : "NULL URL");
 	return;
     }
     debug(20, 3, "storeReleaseRequest: FOR '%s'\n", e->key ? e->key : e->url);
