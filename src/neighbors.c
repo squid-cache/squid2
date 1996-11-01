d19a3f55e34abc156c7f6c6efea6044c075c49a5
Duane Wessels <wessels@packet-pushers.com>
1996-11-01 06:52:13 +0000

    change order of sanity checks in neighborsUdpAck()

--- src/neighbors.c
+++ src/neighbors.c
@@ -582,24 +582,24 @@ neighborsUdpAck(int fd, char *url, icp_common_t * header, struct sockaddr_in *fr
     if (opcode > ICP_OP_END)
 	return;
     opcode_d = IcpOpcodeStr[opcode];
+    /* check if someone is already fetching it */
+    if (BIT_TEST(entry->flag, ENTRY_DISPATCHED)) {
+	debug(15, 3, "neighborsUdpAck: '%s' already being fetched.\n", url);
+	neighborCountIgnored(e, opcode);
+	return;
+    }
     if (mem == NULL) {
 	debug(15, 1, "Ignoring %s for missing mem_obj: %s\n", opcode_d, url);
 	neighborCountIgnored(e, opcode);
 	return;
     }
-    /* check if someone is already fetching it */
-    if (BIT_TEST(entry->flag, ENTRY_DISPATCHED)) {
-	debug(15, 5, "neighborsUdpAck: '%s' already being fetched.\n", url);
-	neighborCountIgnored(e, opcode);
-	return;
-    }
     if (entry->ping_status != PING_WAITING) {
-	debug(15, 5, "neighborsUdpAck: Unexpected %s for %s\n", opcode_d, url);
+	debug(15, 1, "neighborsUdpAck: Unexpected %s for %s\n", opcode_d, url);
 	neighborCountIgnored(e, opcode);
 	return;
     }
     if (entry->lock_count == 0) {
-	debug(12, 3, "neighborsUdpAck: '%s' has no locks\n", url);
+	debug(12, 1, "neighborsUdpAck: '%s' has no locks\n", url);
 	neighborCountIgnored(e, opcode);
 	return;
     }
