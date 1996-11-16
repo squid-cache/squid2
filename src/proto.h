08eadc0192ef15e36448e9f82cecc36bc05daf75
Duane Wessels <wessels@packet-pushers.com>
1996-11-16 07:08:15 +0000

    rename

--- src/proto.h
+++ src/proto.h
@@ -118,7 +118,7 @@ struct icp_common_s {
 };
 
 #define ICP_FLAG_HIT_OBJ     0x80000000ul
-#define ICP_FLAG_MISS_GUNK   0x40000000ul
+#define ICP_FLAG_NETDB_GUNK   0x40000000ul
 
 #define ICP_COMMON_SZ (sizeof(icp_common_t))
 #define ICP_HDR_SZ (sizeof(icp_common_t)+sizeof(u_num32))
