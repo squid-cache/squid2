c1fe79b9061a479b8238105aaef262a20490c7df
Duane Wessels <wessels@packet-pushers.com>
1996-11-18 18:21:41 +0000

    typo

--- src/refresh.c
+++ src/refresh.c
@@ -164,7 +164,7 @@ refreshCheck(const StoreEntry * entry, const request_t * request)
     factor = 100 * age / (entry->timestamp - entry->lastmod);
     debug(22, 3, "refreshCheck: factor = %d\n", factor);
     if (factor > pct) {
-	debug(22, 3, "refreshCheck: YES: factor > pc\n");
+	debug(22, 3, "refreshCheck: YES: factor > pct\n");
 	return 1;
     }
     return 0;
