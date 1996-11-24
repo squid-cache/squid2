a872bdef4036e2c7b3c59cdaeb1cd10d55c97e4f
Duane Wessels <wessels@packet-pushers.com>
1996-11-24 05:32:33 +0000

    removed fatal_dump in storeReleaseRequest()

--- src/store.c
+++ src/store.c
@@ -517,7 +517,7 @@ void storeReleaseRequest(e)
     if (e->flag & RELEASE_REQUEST)
 	return;
     if (!storeEntryLocked(e))
-	fatal_dump("Somebody called storeReleaseRequest on an unlocked entry");
+	storeExpireNow(e);
     debug(20, 3, "storeReleaseRequest: FOR '%s'\n", e->key ? e->key : e->url);
     e->flag |= RELEASE_REQUEST;
 }
