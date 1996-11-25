8bd889379d4656dfe0fa25128bf5283e4f92bde7
Duane Wessels <wessels@packet-pushers.com>
1996-11-25 17:42:54 +0000

    remove trailing slashes from FTP urls

--- src/url.c
+++ src/url.c
@@ -238,6 +238,10 @@ urlParse(method_t method, char *url)
 	debug(23, 0, "urlParse: Invalid port == 0\n");
 	return NULL;
     }
+    /* remove trailing slashes from FTP URLs */
+    t = urlpath + strlen(urlpath);
+    while (*(--t) == '/')
+	*t = '\0';
     request = get_free_request_t();
     request->method = method;
     request->protocol = protocol;
