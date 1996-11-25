96b40351452fd28b71f753eab5b8168e21a5c0bf
Duane Wessels <wessels@packet-pushers.com>
1996-11-25 02:36:02 +0000

    make FTP fork fatal

--- src/ftp.c
+++ src/ftp.c
@@ -732,7 +732,7 @@ ftpInitialize(void)
     if ((pid = fork()) < 0) {
 	debug(50, 0, "ftpInitialize: fork: %s\n", xstrerror());
 	comm_close(cfd);
-	return -1;
+	fatal("Failed to fork() for ftpget.");
     }
     if (pid != 0) {		/* parent */
 	comm_close(cfd);
