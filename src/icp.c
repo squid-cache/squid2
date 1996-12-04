329ca0e2c920113f1beedfbf647b20a82f945b39
Duane Wessels <wessels@packet-pushers.com>
1996-12-04 16:16:21 +0000

    wrong number args to debug()

--- src/icp.c
+++ src/icp.c
@@ -657,7 +657,7 @@ icpGetHeadersForIMS(int fd, icpStateData * icpState)
 
     lmt = mem->reply->last_modified;
     if (lmt < 0)
-	debug(12, 0, "WARNING: '%s' has LMTIME=%s\n", mkrfc1123(lmt));
+	debug(12, 0, "WARNING: '%s' has LMTIME=%s\n", entry->url, mkrfc1123(lmt));
 
     /* Find size of the object */
     if (mem->reply->content_length)
