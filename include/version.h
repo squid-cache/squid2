1ec57791c5e63d2cdd1b453c09f0d38e8b363199
Duane Wessels <wessels@packet-pushers.com>
1996-12-06 21:52:16 +0000

    1.1.0

--- include/version.h
+++ include/version.h
@@ -3,7 +3,7 @@
  *  SQUID_VERSION - String for version id of this distribution
  */
 #ifndef SQUID_VERSION
-#define SQUID_VERSION	"1.1.beta28"
+#define SQUID_VERSION	"1.1.0"
 #endif
 
 #ifndef SQUID_RELEASE_TIME
