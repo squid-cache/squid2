f509e380e25320d2c7ae669269df59a4fba34e9f
Duane Wessels <wessels@packet-pushers.com>
1996-12-20 23:46:15 +0000

    gindent

--- src/neighbors.c
+++ src/neighbors.c
@@ -443,7 +443,7 @@ neighborsUdpPing(protodispatch_data * proto)
 	fatal_dump(NULL);
     }
     if (entry->swap_status != NO_SWAP)
-        fatal_dump("neighborsUdpPing: bad swap_status");
+	fatal_dump("neighborsUdpPing: bad swap_status");
     for (i = 0, e = friends.first_ping; i++ < friends.n; e = e->next) {
 	if (e == NULL)
 	    e = friends.edges_head;
