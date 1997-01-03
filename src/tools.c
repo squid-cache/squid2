04229eb38eaf89341a0fa5a3530e28acc67b5895
Duane Wessels <wessels@packet-pushers.com>
1997-01-03 23:09:24 +0000

    add storeWriteCleanLog() call to fatal()

--- src/tools.c
+++ src/tools.c
@@ -368,6 +368,11 @@ fatal_common(const char *message)
 void
 fatal(const char *message)
 {
+    /* check for store_rebuilding flag because fatal() is often
+     * used in early initialization phases, long before we ever
+     * get to the store log. */
+    if (!store_rebuilding)
+	storeWriteCleanLog();
     fatal_common(message);
     exit(1);
 }
