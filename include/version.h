dec9294bd5a4e054119df75f653d0641084113a6
Duane Wessels <wessels@packet-pushers.com>
1997-01-10 23:23:27 +0000

    1.1.4

--- include/version.h
+++ include/version.h
@@ -3,7 +3,7 @@
  *  SQUID_VERSION - String for version id of this distribution
  */
 #ifndef SQUID_VERSION
-#define SQUID_VERSION	"NOVM.1.1.2"
+#define SQUID_VERSION	"1.1.4"
 #endif
 
 #ifndef SQUID_RELEASE_TIME
