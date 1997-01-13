1963edf1b5456fe154d258a851f9262178b9df27
Duane Wessels <wessels@packet-pushers.com>
1997-01-13 22:58:03 +0000

    fix ru.ru_maxrss in various ways

--- src/stat.c
+++ src/stat.c
@@ -772,13 +772,13 @@ info_get(const cacheinfo * obj, StoreEntry * sentry)
     storeAppendPrintf(sentry, "{\tCPU Usage: %d%%}\n",
 	percent(rusage.ru_utime.tv_sec + rusage.ru_stime.tv_sec,
 	    squid_curtime - squid_starttime));
-#if defined(_SQUID_SGI_) || defined(_SQUID_OSF_)
-    storeAppendPrintf(sentry, "{\tProcess Size: rss %ld KB}\n",
+#if defined(_SQUID_SGI_) || defined(_SQUID_OSF_) || defined(BSD4_4)
+    storeAppendPrintf(sentry, "{\tMaximum Resident Size: %ld KB}\n",
 	rusage.ru_maxrss);
-#else /* _SQUID_SGI_ */
-    storeAppendPrintf(sentry, "{\tProcess Size: rss %ld KB}\n",
+#else
+    storeAppendPrintf(sentry, "{\tMaximum Resident Size: %ld KB}\n",
 	(rusage.ru_maxrss * getpagesize()) >> 10);
-#endif /* _SQUID_SGI_ */
+#endif
     storeAppendPrintf(sentry, "{\tPage faults with physical i/o: %ld}\n",
 	rusage.ru_majflt);
 #endif
