5e2f240dce8015a572ec439e2de1dbe2c97edb8b
Duane Wessels <wessels@packet-pushers.com>
1997-01-19 08:14:07 +0000

    oops, divide by days

--- src/stat.c
+++ src/stat.c
@@ -761,9 +761,8 @@ info_get(const cacheinfo * obj, StoreEntry * sentry)
 	storeGetSwapSize() >> 10);
     storeAppendPrintf(sentry, "{\tStorage Mem size:\t%d KB}\n",
 	store_mem_size >> 10);
-    if (Config.referenceAge < 0)
-        storeAppendPrintf(sentry, "{\tStorage LRU Expiration Age:\t%d days}\n",
-    	    (int) storeExpiredReferenceAge());
+    storeAppendPrintf(sentry, "{\tStorage LRU Expiration Age:\t%6.2f days}\n",
+    	(double) storeExpiredReferenceAge());
 
 #if HAVE_GETRUSAGE && defined(RUSAGE_SELF)
     storeAppendPrintf(sentry, "{Resource usage for %s:}\n", appname);
