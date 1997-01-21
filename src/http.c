9c0bd42e16a030adb23cb53fd7d5b4456682dea0
Duane Wessels <wessels@packet-pushers.com>
1997-01-21 18:33:30 +0000

    formatting

--- src/http.c
+++ src/http.c
@@ -1034,7 +1034,7 @@ httpReplyHeaderStats(StoreEntry * entry)
 	    HttpHdrMiscStr[j],
 	    ReplyHeaderStats.misc[j]);
     for (i = SCC_PUBLIC; i < SCC_ENUM_END; i++)
-	storeAppendPrintf(entry, "{Cache-Control %7.7s: %d}\n",
+	storeAppendPrintf(entry, "{Cache-Control %s: %d}\n",
 	    HttpServerCCStr[i],
 	    ReplyHeaderStats.cc[i]);
     storeAppendPrintf(entry, close_bracket);
