fc33667869f366d806bf8538c6300d80af5da64d
Duane Wessels <wessels@packet-pushers.com>
1997-01-31 21:34:52 +0000

    check firewalls before foo_proxy

--- src/ssl.c
+++ src/ssl.c
@@ -492,12 +492,12 @@ sslSelectNeighbor(int fd, const ipcache_addrs * ia, void *data)
     int fw_ip_match = IP_ALLOW;
     if (ia && Config.firewall_ip_list)
 	fw_ip_match = ip_access_check(ia->in_addrs[ia->cur], Config.firewall_ip_list);
-    if ((e = Config.sslProxy)) {
-	hierarchyNote(request, HIER_SSL_PARENT, 0, e->host);
-    } else if (matchInsideFirewall(request->host)) {
+    if (matchInsideFirewall(request->host)) {
 	hierarchyNote(request, HIER_DIRECT, 0, request->host);
     } else if (fw_ip_match == IP_DENY) {
 	hierarchyNote(request, HIER_DIRECT, 0, request->host);
+    } else if ((e = Config.sslProxy)) {
+	hierarchyNote(request, HIER_SSL_PARENT, 0, e->host);
     } else if ((e = getDefaultParent(request))) {
 	hierarchyNote(request, HIER_DEFAULT_PARENT, 0, e->host);
     } else if ((e = getSingleParent(request))) {
