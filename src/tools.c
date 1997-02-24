cada0c0e3a456af1158d64160bb9bb2f9bb85ea4
Duane Wessels <wessels@packet-pushers.com>
1997-02-24 23:43:14 +0000

    Solaris bug requires suid privs for getrusage() (Arjan de Vet)

--- src/tools.c
+++ src/tools.c
@@ -209,7 +209,14 @@ PrintRusage(void (*f) (void), FILE * lf)
 {
 #if HAVE_GETRUSAGE && defined(RUSAGE_SELF)
     struct rusage rusage;
+#ifdef _SQUID_SOLARIS_
+    /* Solaris 2.5 has getrusage() permission bug -- Arjan de Vet */
+    enter_suid();
+#endif
     getrusage(RUSAGE_SELF, &rusage);
+#ifdef _SQUID_SOLARIS_
+    leave_suid();
+#endif
     fprintf(lf, "CPU Usage: user %d sys %d\n",
 	(int) rusage.ru_utime.tv_sec, (int) rusage.ru_stime.tv_sec);
 #if defined(_SQUID_SGI_) || defined(_SQUID_OSF_) || defined(BSD4_4)
