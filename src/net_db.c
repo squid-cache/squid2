2efca4f4f8d02f797b67ce7fd0a05f9a15a65cfe
Duane Wessels <wessels@packet-pushers.com>
1997-03-28 05:25:49 +0000

    compiler

--- src/net_db.c
+++ src/net_db.c
@@ -557,7 +557,7 @@ netdbDump(StoreEntry * sentry)
     qsort((char *) list,
 	i,
 	sizeof(netdbEntry *),
-	(QS) sortByRtt);
+	(QS) sortPeerByRtt);
     for (k = 0; k < i; k++) {
 	n = *(list + k);
 	storeAppendPrintf(sentry, "{%-16.16s %4d/%4d %7.1f %5.1f",	/* } */
