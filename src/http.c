cf54998fee05d63650d51308cba9071841b8777c
Duane Wessels <wessels@packet-pushers.com>
1997-04-13 06:13:48 +0000

    fix: set httpState->fd

--- src/http.c
+++ src/http.c
@@ -907,8 +907,9 @@ proxyhttpStart(const char *url,
     httpState->request = requestLink(request);
     httpState->neighbor = e;
     httpState->orig_request = requestLink(orig_request);
+    httpState->fd = sock;
     /* register the handler to free HTTP state data when the FD closes */
-    comm_add_close_handler(sock,
+    comm_add_close_handler(httpState->fd,
 	httpStateFree,
 	(void *) httpState);
     request->method = orig_request->method;
