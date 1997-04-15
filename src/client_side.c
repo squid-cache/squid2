cb4922fed7321ac34302c20378895449fe804db4
Duane Wessels <wessels@packet-pushers.com>
1997-04-15 23:45:17 +0000

    redirect_state bug, set to PENDING before calling redirectStart because redirectStart may directly call redirectDone.

--- src/client_side.c
+++ src/client_side.c
@@ -232,8 +232,8 @@ clientAccessCheckDone(icpStateData * icpState, int answer)
 	urlCanonical(icpState->request, icpState->url);
 	if (icpState->redirect_state != REDIRECT_NONE)
 	    fatal_dump("clientAccessCheckDone: wrong redirect_state");
-	redirectStart(fd, icpState, clientRedirectDone, icpState);
 	icpState->redirect_state = REDIRECT_PENDING;
+	redirectStart(fd, icpState, clientRedirectDone, icpState);
     } else {
 	debug(33, 5, "Access Denied: %s\n", icpState->url);
 	redirectUrl = aclGetDenyInfoUrl(&DenyInfoList, AclMatchedName);
@@ -265,7 +265,7 @@ clientRedirectDone(void *data, char *result)
     debug(33, 5, "clientRedirectDone: '%s' result=%s\n", icpState->url,
 	result ? result : "NULL");
     if (icpState->redirect_state != REDIRECT_PENDING)
-	fatal_dump("clientAccessCheckDone: wrong redirect_state");
+	fatal_dump("clientRedirectDone: wrong redirect_state");
     icpState->redirect_state = REDIRECT_DONE;
     if (result)
 	new_request = urlParse(old_request->method, result);
