fc8b876e7c70e1d4dd8e51f794d7666a1a747cf4
Duane Wessels <wessels@packet-pushers.com>
1997-04-21 18:23:11 +0000

    gindent

--- src/main.c
+++ src/main.c
@@ -166,7 +166,7 @@ usage(void)
     fprintf(stderr,
 	"Usage: %s [-hsvzCDFRUVY] [-f config-file] [-[au] port] [-k signal]\n"
 	"       -a port   Specify ASCII port number (default: %d).\n"
-        "       -b        Buffer log output (default is unbuffered).\n"
+	"       -b        Buffer log output (default is unbuffered).\n"
 	"       -f file   Use given config-file instead of\n"
 	"                 %s\n"
 	"       -h        Print help message.\n"
