04327fa5129eef8aad00ef83d22bd176469d34e0
Duane Wessels <wessels@packet-pushers.com>
1997-04-22 15:26:20 +0000

    Under the cachemgr.cgi stats/filedescriptors display the unlinkd pipe fd is blank.

--- src/unlinkd.c
+++ src/unlinkd.c
@@ -192,6 +192,7 @@ unlinkdInit(void)
     unlinkd_fd = unlinkdCreate();
     if (unlinkd_fd < 0)
 	fatal("unlinkdInit: failed to start unlinkd\n");
+    fd_note(unlinkd_fd, Config.Program.unlinkd);
     debug(43, 0, "Unlinkd pipe opened on FD %d\n", unlinkd_fd);
 }
 
