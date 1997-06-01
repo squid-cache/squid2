9048938b8efd541629be9835a92b56e722ccadbb
Duane Wessels <wessels@packet-pushers.com>
1997-06-01 23:23:46 +0000

    new storeCreateEntry has fewer args

--- src/neighbors.c
+++ src/neighbors.c
@@ -1058,7 +1058,7 @@ peerCountMcastPeersStart(void *data)
 	fatal_dump("peerCountMcastPeersStart: non-multicast peer");
     p->mcast.flags &= ~PEER_COUNT_EVENT_PENDING;
     sprintf(url, "http://%s/", inet_ntoa(p->in_addr.sin_addr));
-    fake = storeCreateEntry(url, NULL, 0, 0, METHOD_GET);
+    fake = storeCreateEntry(url, 0, METHOD_GET);
     psstate->request = requestLink(urlParse(METHOD_GET, url));
     psstate->entry = fake;
     psstate->callback = NULL;
