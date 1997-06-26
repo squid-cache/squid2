206833137cd5ef1578a5b92bad7587c0c521ef09
Duane Wessels <wessels@packet-pushers.com>
1997-06-26 15:04:25 +0000

    fix setrlimit(RLIMIT_VMEM) bug on DUNIX 4.x

--- src/tools.c
+++ src/tools.c
@@ -649,7 +649,7 @@ setMaxFD(void)
 #if HAVE_SETRLIMIT && defined(RLIMIT_DATA)
     if (getrlimit(RLIMIT_DATA, &rl) < 0) {
 	debug(50, 0, "getrlimit: RLIMIT_DATA: %s\n", xstrerror());
-    } else {
+    } else if (rl.rlim_max > rl.rlim_cur) {
 	rl.rlim_cur = rl.rlim_max;	/* set it to the max */
 	if (setrlimit(RLIMIT_DATA, &rl) < 0) {
 	    sprintf(tmp_error_buf, "setrlimit: RLIMIT_DATA: %s", xstrerror());
@@ -660,7 +660,7 @@ setMaxFD(void)
 #if HAVE_SETRLIMIT && defined(RLIMIT_VMEM)
     if (getrlimit(RLIMIT_VMEM, &rl) < 0) {
 	debug(50, 0, "getrlimit: RLIMIT_VMEM: %s\n", xstrerror());
-    } else {
+    } else if (rl.rlim_max > rl.rlim_cur) {
 	rl.rlim_cur = rl.rlim_max;	/* set it to the max */
 	if (setrlimit(RLIMIT_VMEM, &rl) < 0) {
 	    sprintf(tmp_error_buf, "setrlimit: RLIMIT_VMEM: %s", xstrerror());
