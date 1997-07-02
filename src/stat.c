22526af7182a60a2f1d7c9d7fe8a02610120fc48
Duane Wessels <wessels@packet-pushers.com>
1997-07-02 18:55:17 +0000

    Fix Available number of file descriptors since fdstat_are_n_free_fd() changed

--- src/stat.c
+++ src/stat.c
@@ -851,14 +851,14 @@ info_get(const cacheinfo * obj, StoreEntry * sentry)
 #endif /* HAVE_MALLINFO */
 
     storeAppendPrintf(sentry, "{File descriptor usage for %s:}\n", appname);
-    storeAppendPrintf(sentry, "{\tMax number of file desc available:    %4d}\n",
+    storeAppendPrintf(sentry, "{\tMaximum number of file descriptors:   %4d}\n",
 	Squid_MaxFD);
     storeAppendPrintf(sentry, "{\tLargest file desc currently in use:   %4d}\n",
 	Biggest_FD);
     storeAppendPrintf(sentry, "{\tNumber of file desc currently in use: %4d}\n",
 	Number_FD);
     storeAppendPrintf(sentry, "{\tAvailable number of file descriptors: %4d}\n",
-	fdstat_are_n_free_fd(0));
+	Squid_MaxFD - Number_FD);
     storeAppendPrintf(sentry, "{\tReserved number of file descriptors:  %4d}\n",
 	RESERVED_FD);
 
