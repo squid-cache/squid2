a4f76284baa7362895af838ed0ec4a9032ec4f15
Duane Wessels <wessels@packet-pushers.com>
1997-07-07 03:37:56 +0000

    deleting

--- src/cbdata.h
+++
@@ -1,8 +0,0 @@
-
-extern void cbdataInit _PARAMS((void));
-extern void cbdataAdd _PARAMS((void *p));
-extern void cbdataFree _PARAMS((void *p));
-extern void cbdataLock _PARAMS((void *p));
-extern void cbdataUnlock _PARAMS((void *p));
-extern int cbdataValid _PARAMS((void *p));
-extern void cbdataDump _PARAMS((StoreEntry *));
