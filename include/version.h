a861691c67c7e732dbf0c404b6684be5e5ba9f7e
Duane Wessels <wessels@packet-pushers.com>
1997-07-10 21:45:30 +0000

    1.1.14

--- include/version.h
+++ include/version.h
@@ -3,7 +3,7 @@
  *  SQUID_VERSION - String for version id of this distribution
  */
 #ifndef SQUID_VERSION
-#define SQUID_VERSION	"1.1.13"
+#define SQUID_VERSION	"1.1.14"
 #endif
 
 #ifndef SQUID_RELEASE_TIME
