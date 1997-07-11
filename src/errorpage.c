10b5fe9a166d6c7fa747d03760170094b596828a
Duane Wessels <wessels@packet-pushers.com>
1997-07-11 00:59:32 +0000

    log_url in debug()

--- src/errorpage.c
+++ src/errorpage.c
@@ -164,7 +164,8 @@ squid_error_entry(StoreEntry * entry, log_type type, const char *msg)
     if (type < ERR_MIN || type > ERR_MAX)
 	fatal_dump("squid_error_entry: type out of range.");
     error_index = (int) (type - ERR_MIN);
-    debug(4, 1, "%s: %s\n", ErrorData[error_index].tag, entry->url);
+    debug(4, 1, "%s: %s\n", ErrorData[error_index].tag,
+	entry->mem_obj ? entry->mem_obj->log_url ? entry->url);
     sprintf(tmp_error_buf, SQUID_ERROR_MSG_P1,
 	entry->url,
 	entry->url,
