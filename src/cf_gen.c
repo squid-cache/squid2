ad0a45408137542e53a9768213674a78892dc752
Duane Wessels <wessels@packet-pushers.com>
1997-07-14 19:56:14 +0000

    default_all() needs to copy input lines to static memory.  this also lets us fake config_lineno. other misc fixes

--- src/cf_gen.c
+++ src/cf_gen.c
@@ -288,9 +288,22 @@ gen_default(Entry * head, FILE * fp)
     int rc = 0;
     fprintf(fp,
 	"void\n"
+	"default_line(const char *s)\n"
+	"{\n"
+	"\tLOCAL_ARRAY(char, tmp_line, BUFSIZ);\n"
+        "\txstrncpy(tmp_line, s, BUFSIZ);\n"
+        "\txstrncpy(config_input_line, s, BUFSIZ);\n"
+	"\tconfig_lineno++;\n"
+        "\tparse_line(tmp_line);\n"
+	"}\n"
+	);
+
+    fprintf(fp,
+	"void\n"
 	"default_all(void)\n"
 	"{\n"
 	"\tcfg_filename = \"Default Configuration\";\n"
+	"\tconfig_lineno = 0;\n"
 	);
     for (entry = head; entry != NULL; entry = entry->next) {
 	assert(entry->name);
@@ -308,7 +321,7 @@ gen_default(Entry * head, FILE * fp)
 	if (strcmp(entry->default_value, "none") == 0) {
 	    fprintf(fp, "\t/* No default for %s */\n", entry->name);
 	} else {
-	    fprintf(fp, "\tparse_line(\"%s %s\");\n",
+	    fprintf(fp, "\tdefault_line(\"%s %s\");\n",
 		entry->name,
 		entry->default_value);
 	}
