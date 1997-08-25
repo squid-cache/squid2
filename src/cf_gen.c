6247fe9e7589fa8ed4b5f80c1e51279ab11cdc3b
Duane Wessels <wessels@packet-pushers.com>
1997-08-25 03:52:16 +0000

    gindent

--- src/cf_gen.c
+++ src/cf_gen.c
@@ -409,9 +409,9 @@ gen_dump(Entry * head, FILE * fp)
 	if (strcmp(entry->loc, "none") == 0)
 	    continue;
 	fprintf(fp, "\tdump_%s(entry, \"%s\", %s);\n",
-		entry->type,
-		entry->name,
-		entry->loc);
+	    entry->type,
+	    entry->name,
+	    entry->loc);
     }
     fprintf(fp, "}\n\n");
 }
