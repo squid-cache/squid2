471b365f01b0a18959bc0cf66219293b48f930ef
Duane Wessels <wessels@packet-pushers.com>
1997-10-22 20:04:11 +0000

    change debug to level 1

--- src/unlinkd.c
+++ src/unlinkd.c
@@ -197,7 +197,7 @@ unlinkdInit(void)
     if (unlinkd_fd < 0)
 	fatal("unlinkdInit: failed to start unlinkd\n");
     fd_note(unlinkd_fd, Config.Program.unlinkd);
-    debug(43, 0, "Unlinkd pipe opened on FD %d\n", unlinkd_fd);
+    debug(43, 1, "Unlinkd pipe opened on FD %d\n", unlinkd_fd);
 }
 
 #endif /* ndef UNLINK_DAEMON */
