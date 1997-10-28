cf24c8d36d97b2772ec9c9b3352a261e6b1e0a0a
Duane Wessels <wessels@packet-pushers.com>
1997-10-28 21:55:12 +0000

    storeGetSwapSpace(int) is now gone.  If we run out of file numbers, make it fatal.

--- src/filemap.c
+++ src/filemap.c
@@ -147,8 +147,7 @@ file_map_bit_set(fileMap * fm, int file_number)
 	fm->toggle++;
 	debug(8, 0) ("You should increment MAX_SWAP_FILE\n");
     } else if (fm->n_files_in_map > (fm->max_n_files - 100)) {
-	debug(8, 0) ("You've run out of swap file numbers. Freeing 1MB\n");
-	storeGetSwapSpace(1000000);
+	fatal("You've run out of swap file numbers.");
     }
     return (file_number);
 }
