43b7e1f9e96d5ab45e8dd9e77b676cc1a3fb872d
Duane Wessels <wessels@packet-pushers.com>
1997-10-30 22:40:51 +0000

    wait_child() munging

--- src/main.c
+++ src/main.c
@@ -735,6 +735,7 @@ watch_child(char *argv[])
 #else
     int status;
 #endif
+    pid_t pid;
     if (*(argv[0]) == '(')
 	return;
     for (;;) {
@@ -747,11 +748,14 @@ watch_child(char *argv[])
 	}
 	/* parent */
 	time(&start);
+	squid_signal(SIGINT, SIG_IGN, SA_RESTART);
+	do {
 #ifdef _SQUID_NEXT_
-	wait3(&status, 0, NULL);
+	    pid = wait3(&status, 0, NULL);
 #else
-	waitpid(-1, &status, 0);
+	    pid = waitpid(-1, &status, 0);
 #endif
+	} while (pid > 0);
 	time(&stop);
 	if (stop - start < 10)
 	    failcount++;
