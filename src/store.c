c4f36b2b499f0c46da00bd8cedf8f6a1fab116d0
Duane Wessels <wessels@packet-pushers.com>
1997-10-31 17:06:13 +0000

    dont show 'store_swap_size =' message until validation is finished

--- src/store.c
+++ src/store.c
@@ -1300,6 +1300,7 @@ storeCleanup(void *data)
 	if (++bucketnum >= store_buckets) {
 	    debug(20, 1) ("  Completed Validation Procedure\n");
 	    debug(20, 1) ("  Validated %d Entries\n", validnum);
+	    debug(20, 1) ("  store_swap_size = %dk\n", store_swap_size);
 	    store_rebuilding = 0;
 	    return;
 	}
@@ -1420,11 +1421,11 @@ storeRebuiltFromDisk(struct storeRebuildState *data)
     debug(20, 1) ("  %7d Swapfile clashes avoided.\n", data->clashcount);
     debug(20, 1) ("  Took %d seconds (%6.1lf objects/sec).\n",
 	r > 0 ? r : 0, (double) data->objcount / (r > 0 ? r : 1));
-    debug(20, 1) ("  store_swap_size = %dk\n", store_swap_size);
     if (data->need_to_validate && data->linecount) {
 	debug(20, 1) ("Beginning Validation Procedure\n");
 	eventAdd("storeCleanup", storeCleanup, NULL, 0);
     } else {
+	debug(20, 1) ("  store_swap_size = %dk\n", store_swap_size);
 	store_rebuilding = 0;
     }
     safe_free(data->line_in);
