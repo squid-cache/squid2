dc8cb420231fd314d8d5497f17175c06a380c5ce
Duane Wessels <wessels@packet-pushers.com>
1997-11-15 05:43:29 +0000

    add tvSubS~Dsec()

--- lib/util.c
+++ lib/util.c
@@ -482,6 +482,13 @@ tvSubUsec(struct timeval t1, struct timeval t2)
 	(t2.tv_usec - t1.tv_usec);
 }
 
+double
+tvSubDsec(struct timeval t1, struct timeval t2)
+{
+    return (double) (t2.tv_sec - t1.tv_sec) +
+	(double) (t2.tv_usec - t1.tv_usec) / 1000000.0;
+}
+
 /*
  *  xstrncpy() - similar to strncpy(3) but terminates string
  *  always with '\0' if n != 0, and doesn't do padding
