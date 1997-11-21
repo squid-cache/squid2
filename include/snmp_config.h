19d92d78deba7dac1f0cce47575dd2af281d01da
kostas <kostas>
1997-11-21 00:34:06 +0000

    Include files for snmp support

---
+++ include/snmp_config.h
@@ -0,0 +1,36 @@
+
+/* viewTypes */
+#define VIEWINCLUDED    1
+#define VIEWEXCLUDED    2
+
+typedef struct _viewEntry {
+	char	   	 viewName[32];
+	int         	 viewIndex;
+	int         	 viewType;
+	int         	 viewSubtreeLen;
+	oid         	 viewSubtree[32];
+
+	struct _viewEntry *next;
+} viewEntry;
+
+typedef struct _communityEntry {
+	char 		 name[64];
+	int   		 readView;
+	int   		 writeView;
+
+	struct _communityEntry *next;
+} communityEntry;
+
+typedef struct _usecEntry {
+	u_char 		 userName[32];
+	int    		 userLen;
+	int    		 qoS;
+	u_char 		 authKey[16];
+	u_char 		 privKey[16];
+	int    		 noauthReadView;
+	int    		 noauthWriteView;
+	int    		 authReadView;
+	int    		 authWriteView;
+
+	struct _usecEntry *next;
+} usecEntry;
