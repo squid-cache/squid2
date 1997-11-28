13b81e271a2ba4199e678d3c2ec537cc9daa40d4
Duane Wessels <wessels@packet-pushers.com>
1997-11-28 08:02:31 +0000

    Add 'mgr:foo' hack

--- src/client.c
+++ src/client.c
@@ -220,6 +220,11 @@ main(int argc, char *argv[])
 	    }
     }
     /* Build the HTTP request */
+    if (strncmp(url, "mgr:", 4) == 0) {
+	char *t = xstrdup(url + 4);
+	snprintf(url, BUFSIZ, "cache_object://%s/%s", hostname, t);
+	free(t);
+    }
     snprintf(msg, BUFSIZ, "%s %s HTTP/1.0\r\n", method, url);
     if (reload) {
 	snprintf(buf, BUFSIZ, "Pragma: no-cache\r\n");
