0864902629185982715bf74ed5d71ee44732cf32
Duane Wessels <wessels@packet-pushers.com>
1998-01-06 02:47:20 +0000

    compiler

--- src/proto.c
+++ src/proto.c
@@ -229,12 +229,13 @@ protoDispatch(int fd, StoreEntry * entry, request_t * request)
     pctrl_t *pctrl;
     debug(17, 3) ("protoDispatch: '%s'\n", storeUrl(entry));
     entry->mem_obj->request = requestLink(request);
-    if (request->protocol == PROTO_CACHEOBJ)
+    if (request->protocol == PROTO_CACHEOBJ) {
 	protoStart(fd, entry, NULL, request);
 	return;
-    else if (request->protocol == PROTO_WAIS)
+    } else if (request->protocol == PROTO_WAIS) {
 	protoStart(fd, entry, NULL, request);
 	return;
+    }
     pctrl = xcalloc(1, sizeof(pctrl_t));
     cbdataAdd(pctrl);
     pctrl->entry = entry;
