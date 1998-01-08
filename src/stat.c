86362ee47275ba4f9dce8e46fb10fb20de85985d
Duane Wessels <wessels@packet-pushers.com>
1998-01-08 03:39:22 +0000

    typo

--- src/stat.c
+++ src/stat.c
@@ -972,6 +972,6 @@ statAvgDump(StoreEntry * sentry)
 	XAVG(select_loops));
     storeAppendPrintf(sentry, "cpu_time = %f seconds\n", ct);
     storeAppendPrintf(sentry, "wall_time = %f seconds\n", dt);
-    storeAppendPrintf(sentry, "cpu_usage %f%%\n", 100.0*ct/dt);
+    storeAppendPrintf(sentry, "cpu_usage = %f%%\n", 100.0*ct/dt);
     storeBufferFlush(sentry);
 }
