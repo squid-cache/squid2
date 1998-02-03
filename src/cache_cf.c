95b1fac22750c42030ecd1bf70e2275daae4f859
Duane Wessels <wessels@packet-pushers.com>
1998-02-03 17:26:37 +0000

    add bogus value detection to parse_cachedir()

--- src/cache_cf.c
+++ src/cache_cf.c
@@ -506,10 +506,16 @@ parse_cachedir(struct _cacheSwap *swap)
 	self_destruct();
     GetInteger(i);
     size = i << 10;		/* Mbytes to kbytes */
+    if (size <= 0)
+	fatal("parse_cachedir: invalid size value");
     GetInteger(i);
     l1 = i;
+    if (l1 <= 0)
+	fatal("parse_cachedir: invalid level 1 directories value");
     GetInteger(i);
     l2 = i;
+    if (l2 <= 0)
+	fatal("parse_cachedir: invalid level 2 directories value");
     if ((token = strtok(NULL, w_space)))
 	if (!strcasecmp(token, "read-only"))
 	    readonly = 1;
