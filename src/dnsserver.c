2ed36c1eeb9fb3e55ba01402a2b5e8e73bf45635
Duane Wessels <wessels@packet-pushers.com>
1998-02-05 17:37:43 +0000

    remove GOT EOF message

--- src/dnsserver.c
+++ src/dnsserver.c
@@ -313,7 +313,6 @@ main(int argc, char *argv[])
 
 	/* read from ipcache */
 	if (fgets(request, REQ_SZ, stdin) == NULL) {
-	    fprintf(stderr, "dnsserver %d got EOF\n", (int) getpid());
 	    exit(1);
 	}
 	t = strrchr(request, '\n');
