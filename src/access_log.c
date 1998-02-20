7b4613c0866cab64daf9576133da6af20cae0865
Duane Wessels <wessels@packet-pushers.com>
1998-02-20 18:47:49 +0000

    make accessLogTime public so it can be used by debug.c

--- src/access_log.c
+++ src/access_log.c
@@ -317,3 +317,17 @@ accessLogInit(void)
     assert(sizeof(log_tags) == (LOG_TYPE_MAX + 1) * sizeof(char *));
     accessLogOpen(Config.Log.access);
 }
+
+const char *
+accessLogTime(time_t t)
+{
+    struct tm *tm;
+    static char buf[128];
+    static time_t last_t = 0;
+    if (t != last_t) {
+	tm = localtime(&t);
+	strftime(buf, 127, "%Y/%m/%d %H:%M:%S", tm);
+	last_t = t;
+    }
+    return buf;
+}
