dc8e2b1a3f233eda40c9ff0db9e204bae491e8b7
rousskov <rousskov>
1998-02-26 00:19:52 +0000

    Canceled "Status:" line change from the previous commit. Status line already contains '\n' and we do not have to add one.

--- src/cachemgr.c
+++ src/cachemgr.c
@@ -359,7 +359,7 @@ read_reply(int s, cachemgr_request * req)
 	    }
 	    /* this is a way to pass HTTP status to the Web server */
 	    if (statusStr)
-		printf("Status: %d %s\n", status, statusStr);
+		printf("Status: %d %s", status, statusStr); /* statusStr has '\n' */
 	    break;
 	case isHeaders:
 	    /* forward header field */
