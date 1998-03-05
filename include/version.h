35dd1bb9693effd05491a686da848c27dc07933c
Duane Wessels <wessels@packet-pushers.com>
1998-03-05 00:38:55 +0000

    1.2.beta16

--- include/version.h
+++ include/version.h
@@ -4,13 +4,13 @@
  *  SQUID_VERSION - String for version id of this distribution
  */
 #ifndef SQUID_VERSION
-#define SQUID_VERSION	"1.2.beta15"
-#endif
-#ifndef CURRENT_MIB_VERSION
-#define CURRENT_MIB_VERSION "Id: mib.txt,v 1.11 1998/02/26 23:16:52 kostas Exp $"
-
+#define SQUID_VERSION	"1.2.beta16"
 #endif
 
 #ifndef SQUID_RELEASE_TIME
 #define SQUID_RELEASE_TIME squid_curtime
+#endif
+
+#ifndef CURRENT_MIB_VERSION
+#define CURRENT_MIB_VERSION "Id: mib.txt,v 1.11 1998/02/26 23:16:52 kostas Exp $"
 #endif
