7a6924a3829d19dbcbb72ddda060a8ccca25fcd4
rousskov <rousskov>
1998-03-06 05:34:44 +0000

    - Changed POST action in host/port form to GET.

--- src/cachemgr.c
+++ src/cachemgr.c
@@ -235,7 +235,7 @@ auth_html(char *host, int port, const char *user_name)
     printf("<P>This is a WWW interface to the instrumentation interface\n");
     printf("for the Squid object cache.</P>\n");
     printf("<HR>\n");
-    printf("<FORM METHOD=\"POST\" ACTION=\"%s\">\n", script_name);
+    printf("<FORM METHOD=\"GET\" ACTION=\"%s\">\n", script_name);
     printf("<TABLE BORDER=0>\n");
     printf("<TR><TH ALIGN=\"left\">Cache Host:</TH><TD><INPUT NAME=\"host\" ");
     printf("SIZE=30 VALUE=\"%s\"></TD></TR>\n", host);
