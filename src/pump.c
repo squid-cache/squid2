8ffdbf0055d756c16d773ec860ed40a092beb397
Duane Wessels <wessels@packet-pushers.com>
1998-03-12 00:15:00 +0000

    debugging

--- src/pump.c
+++ src/pump.c
@@ -274,7 +274,8 @@ static int
 pumpReadDefer(int fd, void *data)
 {
     PumpStateData *p = data;
-    debug(61, 9) ("pumpReadDefer: %d %d\n", p->rcvd, p->sent);
+    debug(61, 9) ("pumpReadDefer: FD %d, Rcvd %d bytes, Sent %d bytes\n",
+	fd, p->rcvd, p->sent);
     assert(p->rcvd >= p->sent);
     if ((p->rcvd - p->sent) < PUMP_MAXBUFFER)
 	return 0;
