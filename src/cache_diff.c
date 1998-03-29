5d67a5fb4093dd16d5ddac7a2cbcea0cf784b755
rousskov <rousskov>
1998-03-29 20:42:46 +0000

    - Report polishing

--- src/cache_diff.c
+++ src/cache_diff.c
@@ -197,7 +197,8 @@ cacheIndexCmpReport(CacheIndex *idx, int shared_count)
     printf("%s:\t %7d = %7d + %7d (%7.2f%% + %7.2f%%)\n",
 	idx->name,
 	idx->count,
-	shared_count, idx->count - shared_count,
+	idx->count - shared_count,
+	shared_count,
 	xpercent(idx->count-shared_count, idx->count),
 	xpercent(shared_count, idx->count));
 }
