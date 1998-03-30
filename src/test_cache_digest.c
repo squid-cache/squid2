eb6fc8fe5d49fd90d7158632420a83d78eae89f5
rousskov <rousskov>
1998-03-30 21:16:10 +0000

    - Added a timer for a no-load hash scan

--- src/test_cache_digest.c
+++ src/test_cache_digest.c
@@ -140,6 +140,15 @@ cacheIndexInitDigest(CacheIndex *idx)
 	idx->name,
 	tvSubDsec(t_start, t_end),
 	(double)1e6*tvSubDsec(t_start, t_end)/idx->count);
+    /* check how long it takes to traverse the hash */
+    gettimeofday(&t_start, NULL);
+    for (hashr = hash_first(idx->hash); hashr; hashr = hash_next(idx->hash)) {
+    }
+    gettimeofday(&t_end, NULL);
+    fprintf(stderr, "%s: hash scan took: %f sec, %f sec/M\n",
+	idx->name,
+	tvSubDsec(t_start, t_end),
+	(double)1e6*tvSubDsec(t_start, t_end)/idx->count);
 }
 
 static int
