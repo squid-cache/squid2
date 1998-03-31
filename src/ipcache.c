f7bf5d3a7127c6b4df1ae6f4a6903d21ac8d869e
Duane Wessels <wessels@packet-pushers.com>
1998-03-31 04:08:32 +0000

    use assert instead of fatal_dump

--- src/ipcache.c
+++ src/ipcache.c
@@ -563,10 +563,7 @@ ipcache_nbgethostbyname(const char *name, IPH * handler, void *handlerData)
     ipcache_entry *i = NULL;
     dnsserver_t *dnsData = NULL;
     const ipcache_addrs *addrs = NULL;
-
-    if (!handler)
-	fatal_dump("ipcache_nbgethostbyname: NULL handler");
-
+    assert(handler != NULL);
     debug(14, 4) ("ipcache_nbgethostbyname: Name '%s'.\n", name);
     IpcacheStats.requests++;
 
