2360ba7f6ccb58da6e1f92ce47aef7b62a7a4551
Duane Wessels <wessels@packet-pushers.com>
1998-03-31 08:45:34 +0000

    debugging

--- src/client_side.c
+++ src/client_side.c
@@ -1551,16 +1551,18 @@ parseHttpRequest(ConnStateData * conn, method_t * method_p, int *status,
     while (isspace(*t))
 	if (*(t++) == '\n')
 	    break;
+    req_hdr = t;
+debug(0,0)("parseHttpRequest: req_hdr = {%s}\n", req_hdr);
 
     /* Check if headers are received */
-    header_sz = headersEnd(t, conn->in.offset);
+    header_sz = headersEnd(req_hdr, conn->in.offset - (req_hdr - inbuf));
     if (0 == header_sz) {
 	xfree(inbuf);
 	*status = 0;
 	return NULL;
     }
-    req_hdr = t;
     end = req_hdr + header_sz;
+debug(0,0)("parseHttpRequest: end = {%s}\n", end);
 
     if (end <= req_hdr) {
 	/* Invalid request */
@@ -1578,6 +1580,8 @@ parseHttpRequest(ConnStateData * conn, method_t * method_p, int *status,
 	return http;
     }
     req_sz = end - inbuf;
+debug(0,0)("parseHttpRequest: req_sz = %d\n", (int) req_sz);
+assert(req_sz <= conn->in.offset);
 
     /* Ok, all headers are received */
     http = xcalloc(1, sizeof(clientHttpRequest));
