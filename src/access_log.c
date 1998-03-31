727b2b36a66f3aee85506672293cac8c9c82d75f
Duane Wessels <wessels@packet-pushers.com>
1998-03-31 08:37:29 +0000

    need to fully free and recreate FV hash_tables

--- src/access_log.c
+++ src/access_log.c
@@ -431,7 +431,11 @@ static void
 fvdbClear(void)
 {
     hashFreeItems(via_table, fvdbFreeEntry);
+    hashFreeMemory(via_table);
+    via_table = hash_create((HASHCMP *) strcmp, 977, hash4);
     hashFreeItems(forw_table, fvdbFreeEntry);
+    hashFreeMemory(forw_table);
+    forw_table = hash_create((HASHCMP *) strcmp, 977, hash4);
 }
 
 #endif
