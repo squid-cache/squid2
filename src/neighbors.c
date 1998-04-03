47b5e238ce71a24380bcea45b8704aa92c9343d0
Duane Wessels <wessels@packet-pushers.com>
1998-04-03 18:07:31 +0000

    missing newline in cachemgr output

--- src/neighbors.c
+++ src/neighbors.c
@@ -1085,6 +1085,7 @@ dump_peers(StoreEntry * sentry, peer * peers)
 	    else
 		storeAppendPrintf(sentry, "!%s ", d->domain);
 	}
+	storeAppendPrintf(sentry, "\n");
 	storeAppendPrintf(sentry, "Keep-Alive Ratio: %d%%\n",
 	    percent(e->stats.n_keepalives_recv, e->stats.n_keepalives_sent));
     }
