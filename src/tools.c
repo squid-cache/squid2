79da85e1d4375f45265996a5044ccf1852893da4
Duane Wessels <wessels@packet-pushers.com>
1998-04-08 00:34:19 +0000

    remove dlinkAddTail

--- src/tools.c
+++ src/tools.c
@@ -801,19 +801,6 @@ dlinkAdd(void *data, dlink_node * m, dlink_list * list)
 }
 
 void
-dlinkAddTail(void *data, dlink_node * m, dlink_list * list)
-{
-    m->data = data;
-    m->next = NULL;
-    m->prev = list->tail;
-    if (list->tail)
-	list->tail->next = m;
-    list->tail = m;
-    if (list->head == NULL)
-	list->head = m;
-}
-
-void
 dlinkDelete(dlink_node * m, dlink_list * list)
 {
     if (m->next)
