d151fe8b5622f3cd017994e1ef1228b57d007237
rousskov <rousskov>
1998-04-09 00:11:31 +0000

    - temporary paranoid debug "lost peer" message

--- src/client_side.c
+++ src/client_side.c
@@ -596,9 +596,13 @@ clientUpdateCounters(clientHttpRequest * http)
 	    cacheDigestGuessStatsUpdate(&peer->digest.stats.guess,
 		real_hit, guess_hit);
 	else {
-	    debug(33,2) ("clientUpdateCounters: lost peer %s for %s!\n", 
-		http->request->hier.cd_host, http->request->host);
-	    fatal_dump("lost peer");
+	    /* temporary paranoid debug */
+	    static int max_count = 200;
+	    if (max_count > 0) {
+		debug(33,1) ("clientUpdateCounters: lost peer %s for %s! (%d)\n", 
+		    http->request->hier.cd_host, http->request->host, max_count);
+		max_count--;
+	    }
 	}
     }
 #endif
