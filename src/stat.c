a10b2a20991b25f3b0908e319529a7cf8b6061de
Duane Wessels <wessels@packet-pushers.com>
1998-04-09 23:26:08 +0000

    add averages to digest blob

--- src/stat.c
+++ src/stat.c
@@ -1125,6 +1125,8 @@ statDigestBlob(StoreEntry * sentry)
 {
     storeAppendPrintf(sentry, "\nCounters:\n");
     statCountersDump(sentry);
+    storeAppendPrintf(sentry, "\n5 Min Averages:\n");
+    statAvgDump(sentry, 5, 0);
     storeAppendPrintf(sentry, "\nHistograms:\n");
     statCountersHistograms(sentry);
     storeAppendPrintf(sentry, "\nPeer Digests:\n");
