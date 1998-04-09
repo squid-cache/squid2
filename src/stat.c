ca3ea536daadd9a44d95c47aebe762ef96a60b65
rousskov <rousskov>
1998-04-09 23:53:47 +0000

    - more per-peer CDigest stats

--- src/stat.c
+++ src/stat.c
@@ -1106,6 +1106,18 @@ statPeerSelect(StoreEntry * sentry)
 	storeAppendPrintf(sentry, "peer.local_memory = %d\n",
 	    peer->digest.cd ? peer->digest.cd->mask_size/1024 : 0);
 	storeAppendPrintf(sentry, "\n");
+	if (peer->digest.cd) {
+	    cacheDigestReport(peer->digest.cd, peer->host, sentry);
+	    storeAppendPrintf(sentry, "digest inited: %d, disabled: %d usable: %d requested: %d\n",
+		0 < EBIT_TEST(peer->digest.flags, PD_INITED),
+		0 < EBIT_TEST(peer->digest.flags, PD_DISABLED),
+		0 < EBIT_TEST(peer->digest.flags, PD_USABLE),
+		0 < EBIT_TEST(peer->digest.flags, PD_REQUESTED)
+		);
+	} else {
+	    storeAppendPrintf(sentry, "peer %s has no cache digest\n", peer->host);
+	}
+	storeAppendPrintf(sentry, "\n");
     }
 
     storeAppendPrintf(sentry, "\nAlgorithm usage:\n");
