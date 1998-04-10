7c2d3ac8225114a672258cafd1e625640af79169
rousskov <rousskov>
1998-04-10 00:49:39 +0000

    - polish

--- src/stat.c
+++ src/stat.c
@@ -1061,7 +1061,7 @@ statCountersDump(StoreEntry * sentry)
 	f->cd.msgs_recv);
     storeAppendPrintf(sentry, "cd.memory = %d\n",
 	(int)f->cd.memory.kb);
-    storeAppendPrintf(sentry, "cd.store_memory = %d\n",
+    storeAppendPrintf(sentry, "cd.local_memory = %d\n",
         (int) (store_digest ? store_digest->mask_size/1024 : 0));
     storeAppendPrintf(sentry, "cd.kbytes_sent = %d\n",
 	(int) f->cd.kbytes_sent.kb);
