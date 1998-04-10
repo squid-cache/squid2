d2b69d32ecd9c22ca8fddcd49300b63e2ae1de08
rousskov <rousskov>
1998-04-10 00:45:22 +0000

    - polish

--- src/stat.c
+++ src/stat.c
@@ -1141,7 +1141,7 @@ statDigestBlob(StoreEntry * sentry)
     statCountersHistograms(sentry);
     storeAppendPrintf(sentry, "\nPeer Digests:\n");
     statPeerSelect(sentry);
-    storeAppendPrintf(sentry, "\nDigest Report:\n");
+    storeAppendPrintf(sentry, "\nLocal Digest:\n");
     storeDigestReport(sentry);
 }
 
