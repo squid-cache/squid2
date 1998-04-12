b30e0214338f4ddd2d8fac22cb750ca67084ce2c
rousskov <rousskov>
1998-04-12 06:13:56 +0000

    - temporary disabled to 304 replies for special entries to prevent coredumps   on caches running old buggy code that could not handle 304 replies for Cache   Digests

--- src/client_side.c
+++ src/client_side.c
@@ -1419,7 +1419,11 @@ clientProcessRequest2(clientHttpRequest * http)
     } else if (EBIT_TEST(e->flag, ENTRY_SPECIAL)) {
 	/* ideally, special entries should be processed later, 
 	 * so we can use std processing routines for IMS and such */
-	if (EBIT_TEST(r->flags, REQ_IMS) && e->lastmod <= r->ims)
+/*
+ * temporary disabled to prevent coredumps on caches running old buggy code
+ * that could not handle 304 replies for Cache Digests
+ */
+	if (0 && EBIT_TEST(r->flags, REQ_IMS) && e->lastmod <= r->ims)
 	    return LOG_TCP_IMS_HIT;
 	else if (e->mem_status == IN_MEMORY)
 	    return LOG_TCP_MEM_HIT;
