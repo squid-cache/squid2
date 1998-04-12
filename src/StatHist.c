adb49970a3656cc39f59266f5f7c03387aa58dfb
rousskov <rousskov>
1998-04-12 06:00:44 +0000

    - added dumper for integer-based histograms

--- src/StatHist.c
+++ src/StatHist.c
@@ -223,3 +223,11 @@ statHistEnumInit(StatHist * H, int last_enum)
 {
     statHistInit(H, last_enum + 3, &Null, &Null, -1, last_enum + 1 + 1);
 }
+
+void
+statHistIntDumper(StoreEntry * sentry, int idx, double val, double size, int count)
+{
+    if (count)
+        storeAppendPrintf(sentry, "%2d\t %5d\t %5d\n",
+            idx, (int) val, count);
+}
