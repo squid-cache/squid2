e89dd3330db6a7bc85c36001c8501b8fae507416
rousskov <rousskov>
1998-04-15 00:34:34 +0000

    - added printouts of server_svc_time histograms

--- src/stat.c
+++ src/stat.c
@@ -955,8 +955,12 @@ statCountersHistograms(StoreEntry *sentry)
     statHistDump(&f->icp.query_svc_time, sentry, NULL);
     storeAppendPrintf(sentry, "\nicp.reply_svc_time histogram:\n");
     statHistDump(&f->icp.reply_svc_time, sentry, NULL);
+    storeAppendPrintf(sentry, "\nicp.server_svc_time histogram:\n");
+    statHistDump(&f->icp.server_svc_time, sentry, NULL);
     storeAppendPrintf(sentry, "\nicp.client_svc_time histogram:\n");
     statHistDump(&f->icp.client_svc_time, sentry, NULL);
+    storeAppendPrintf(sentry, "\ncd.server_svc_time histogram:\n");
+    statHistDump(&f->cd.server_svc_time, sentry, NULL);
     storeAppendPrintf(sentry, "\ncd.client_svc_time histogram:\n");
     statHistDump(&f->cd.client_svc_time, sentry, NULL);
     storeAppendPrintf(sentry, "\ncd.peer_choice_count histogram:\n");
