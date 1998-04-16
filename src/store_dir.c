9dd9ad62f9591f6c2fc62cfde483ab53f3ff0960
Duane Wessels <wessels@packet-pushers.com>
1998-04-16 17:47:15 +0000

    show percent of file map bits used

--- src/store_dir.c
+++ src/store_dir.c
@@ -507,8 +507,9 @@ storeDirStats(StoreEntry * sentry)
 	storeAppendPrintf(sentry, "Current Size: %d KB\n", SD->cur_size);
 	storeAppendPrintf(sentry, "Percent Used: %0.2f%%\n",
 	    100.0 * SD->cur_size / SD->max_size);
-	storeAppendPrintf(sentry, "Filemap bits in use: %d\n",
-	    SD->map->n_files_in_map);
+	storeAppendPrintf(sentry, "Filemap bits in use: %d of %d (%d%%)\n",
+	    SD->map->n_files_in_map, SD->map->max_n_files,
+	    percent(SD->map->n_files_in_map, SD->map->max_n_files));
     }
 }
 
