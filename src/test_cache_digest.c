4d569afd99ca7ec12845b9064790c921b97345e3
rousskov <rousskov>
1998-04-22 21:28:24 +0000

    - synced with current CacheDigest interface

--- src/test_cache_digest.c
+++ src/test_cache_digest.c
@@ -272,7 +272,7 @@ cacheResetDigest(Cache * cache)
     if (cache->digest)
 	cacheDigestDestroy(cache->digest);
     hash = cache->hash;
-    cache->digest = cacheDigestCreate(2 * cache->count + 1);	/* 50% utilization */
+    cache->digest = cacheDigestCreate(cache->count+1, 6);
     if (!cache->count)
 	return;
     gettimeofday(&t_start, NULL);
