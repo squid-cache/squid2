e1a9adfbc8198237ff3450e2e6664c4ad44374e4
Duane Wessels <wessels@packet-pushers.com>
1998-04-24 06:40:05 +0000

    remove wierd , CUR in ipcache output

--- src/ipcache.c
+++ src/ipcache.c
@@ -757,8 +757,6 @@ ipcacheStatPrint(ipcache_entry * i, StoreEntry * sentry)
     for (k = 0; k < (int) i->addrs.count; k++) {
 	storeAppendPrintf(sentry, " %15s-%3s", inet_ntoa(i->addrs.in_addrs[k]),
 	    i->addrs.bad_mask[k] ? "BAD" : "OK ");
-	if (i->addrs.count > 1 && k == i->addrs.cur)
-	    storeAppendPrintf(sentry, ",CUR");
     }
     storeAppendPrintf(sentry, "\n");
 }
