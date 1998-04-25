ddef206ff7995798a47468f8a5b60dcf2f224e56
Duane Wessels <wessels@packet-pushers.com>
1998-04-25 07:25:41 +0000

    make Generated string like errorpage.c

--- src/ftp.c
+++ src/ftp.c
@@ -368,11 +368,11 @@ ftpListingFinish(FtpStateData * ftpState)
     }
     storeAppendPrintf(e, "<HR>\n");
     storeAppendPrintf(e, "<ADDRESS>\n");
-    storeAppendPrintf(e, "Generated %s, by %s/%s@%s\n",
+    storeAppendPrintf(e, "Generated %s by %s (<a href=\"http://squid.nlanr.net/Squid/\">%s</a>)\n",
 	mkrfc1123(squid_curtime),
-	appname,
-	version_string,
-	getMyHostname());
+	getMyHostname(),
+	full_appname_string,
+	version_string);
     storeAppendPrintf(e, "</ADDRESS></BODY></HTML>\n");
     storeBufferFlush(e);
 }
