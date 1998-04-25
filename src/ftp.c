97a0c5d44003839770665a208c8cd666b77a89b8
Duane Wessels <wessels@packet-pushers.com>
1998-04-25 07:19:55 +0000

    DW broke ftp directory parsing again

--- src/ftp.c
+++ src/ftp.c
@@ -718,6 +718,7 @@ ftpParseListing(FtpStateData * ftpState, int len)
     while (*end != '\r' && *end != '\n' && end > sbuf)
 	end--;
     usable = end - sbuf;
+    debug(9,3)("ftpParseListing: usable = %d\n", usable);
     if (usable == 0) {
 	debug(9, 3) ("ftpParseListing: didn't find end for %s\n", storeUrl(e));
 	xfree(sbuf);
@@ -727,8 +728,9 @@ ftpParseListing(FtpStateData * ftpState, int len)
     line = memAllocate(MEM_4K_BUF);
     end++;
     storeBuffer(e);
-    for (s = sbuf; s < end; s += strcspn(s, crlf)) {
-	s += strspn(s, crlf);
+    s = sbuf;
+    s += strspn(s, crlf);
+    for (; s < end; s += strcspn(s, crlf), s += strspn(s, crlf)) {
 	debug(9, 3) ("ftpParseListing: s = {%s}\n", s);
 	linelen = strcspn(s, crlf) + 1;
 	if (linelen < 2)
