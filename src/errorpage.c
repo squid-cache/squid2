53eda7a9075e20c289e79c51759d546c62623639
Duane Wessels <wessels@packet-pushers.com>
1998-05-02 06:41:58 +0000

    buffer error pages

--- src/errorpage.c
+++ src/errorpage.c
@@ -250,10 +250,12 @@ errorAppendEntry(StoreEntry * entry, ErrorState * err)
 #endif
     assert(mem != NULL);
     assert(mem->inmem_hi == 0);
+    storeBuffer(entry);
     rep = errorBuildReply(err);
     httpReplySwapOut(rep, entry);
     httpReplyDestroy(rep);
     mem->reply->sline.status = err->http_status;
+    storeBufferFlush(entry);
     storeComplete(entry);
     storeNegativeCache(entry);
     storeReleaseRequest(entry);
