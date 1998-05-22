d42a22cb4171a6f64b58c2e61eba595ca9210aef
Duane Wessels <wessels@packet-pushers.com>
1998-05-22 23:42:12 +0000

    generate a coredump for running out of swap file numbers

--- src/filemap.c
+++ src/filemap.c
@@ -150,7 +150,7 @@ file_map_bit_set(fileMap * fm, int file_number)
 	    "\tConsider decreasing store_avg_object_size in squid.conf\n",
             percent(fm->n_files_in_map, fm->max_n_files));
     } else if (fm->n_files_in_map > (fm->max_n_files - 100)) {
-	fatal("You've run out of swap file numbers.");
+	fatal_dump("You've run out of swap file numbers.");
     }
     return (file_number);
 }
