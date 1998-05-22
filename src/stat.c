4ad01fd27dd79818cf5516fb11c9f94a1b40f27a
Duane Wessels <wessels@packet-pushers.com>
1998-05-22 23:42:58 +0000

    report # on-disk objects

--- src/stat.c
+++ src/stat.c
@@ -606,6 +606,8 @@ info_get(StoreEntry * sentry)
 	hot_obj_count);
     storeAppendPrintf(sentry, "\t%6d Filemap bits set\n",
 	storeDirMapBitsInUse());
+    storeAppendPrintf(sentry, "\t%6d on-disk objects\n",
+	n_disk_objects);
 
 #if XMALLOC_STATISTICS
     storeAppendPrintf(sentry, "Memory allocation statistics\n");
