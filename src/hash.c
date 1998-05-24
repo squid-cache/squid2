c4d6b509867c0b08611ae24771d8d6a999778a0a
Duane Wessels <wessels@packet-pushers.com>
1998-05-24 05:05:52 +0000

    was always skipping first bucket

--- src/hash.c
+++ src/hash.c
@@ -253,7 +253,9 @@ hash_first(hash_table * hid)
 {
     assert(NULL == hid->next);
     hid->current_slot = 0;
-    hash_next_bucket(hid);
+    hid->next = hid->buckets[hid->current_slot];
+    if (NULL == hid->next)
+        hash_next_bucket(hid);
 }
 
 /*
