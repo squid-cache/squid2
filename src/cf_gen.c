19d1b4b43fe79f1659b700b30b02230399119fc0
Duane Wessels <wessels@packet-pushers.com>
1998-05-28 23:41:20 +0000

    debug

--- src/cf_gen.c
+++ src/cf_gen.c
@@ -157,7 +157,7 @@ main(int argc, char *argv[])
 		state = sDOC;
 	    } else {
 		printf("Error on line %d\n", linenum);
-		printf("--> %d bytes, %s\n", strlen(buff), buff);
+		printf("--> %s\n", buff);
 		exit(1);
 	    }
 	    break;
