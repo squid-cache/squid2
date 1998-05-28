9754e919264756c01ec030b7d1e20d88aca32860
Duane Wessels <wessels@packet-pushers.com>
1998-05-28 23:43:45 +0000

    neighbors.c:192: warning: unused variable a

--- src/neighbors.c
+++ src/neighbors.c
@@ -189,7 +189,6 @@ peerAllowedToUse(const peer * p, request_t * request)
 {
     const struct _domain_ping *d = NULL;
     int do_ping = 1;
-    const struct _acl_list *a = NULL;
     aclCheck_t checklist;
     assert(request != NULL);
     if (EBIT_TEST(request->flags, REQ_NOCACHE))
