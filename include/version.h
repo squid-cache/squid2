c018e855b8fdd5d752bdada6ea1209a189624f05
Duane Wessels <wessels@packet-pushers.com>
1998-06-02 04:15:28 +0000

    1.2.beta22

--- include/version.h
+++ include/version.h
@@ -4,7 +4,7 @@
  *  SQUID_VERSION - String for version id of this distribution
  */
 #ifndef SQUID_VERSION
-#define SQUID_VERSION	"1.2.beta21"
+#define SQUID_VERSION	"1.2.beta22"
 #endif
 
 #ifndef SQUID_RELEASE_TIME
