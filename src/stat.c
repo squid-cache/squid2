a86f4ae05d993d55f4e36ebe2d4e18f3f8d80bd9
Duane Wessels <wessels@packet-pushers.com>
1998-07-14 23:20:33 +0000

    had sample_start_time & sample_end_time reversed

--- src/stat.c
+++ src/stat.c
@@ -642,12 +642,12 @@ statAvgDump(StoreEntry * sentry, int minutes, int hours)
     ct = f->cputime - l->cputime;
 
     storeAppendPrintf(sentry, "sample_start_time = %d.%d (%s)\n",
-	(int) f->timestamp.tv_sec,
-	(int) f->timestamp.tv_usec,
-	mkrfc1123(f->timestamp.tv_sec));
-    storeAppendPrintf(sentry, "sample_end_time = %d.%d (%s)\n",
 	(int) l->timestamp.tv_sec,
 	(int) l->timestamp.tv_usec,
+	mkrfc1123(f->timestamp.tv_sec));
+    storeAppendPrintf(sentry, "sample_end_time = %d.%d (%s)\n",
+	(int) f->timestamp.tv_sec,
+	(int) f->timestamp.tv_usec,
 	mkrfc1123(l->timestamp.tv_sec));
 
     storeAppendPrintf(sentry, "client_http.requests = %f/sec\n",
