18eb99afddef2ad404dbc7e3c061bfb22762015e
Duane Wessels <wessels@packet-pushers.com>
1998-07-30 22:59:45 +0000

    adding NEIGHBOR_NO_NETDB_EXCHANGE for stats output

--- src/neighbors.c
+++ src/neighbors.c
@@ -1066,6 +1066,8 @@ dump_peer_options(StoreEntry * sentry, peer * p)
     if (EBIT_TEST(p->options, NEIGHBOR_HTCP))
 	storeAppendPrintf(sentry, " htcp");
 #endif
+    if (EBIT_TEST(p->options, NEIGHBOR_NO_NETDB_EXCHANGE))
+	storeAppendPrintf(sentry, " no-netdb-exchange");
     if (p->mcast.ttl > 0)
 	storeAppendPrintf(sentry, " ttl=%d", p->mcast.ttl);
     storeAppendPrintf(sentry, "\n");
