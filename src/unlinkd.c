503a5b8a203f48494d7a219ce353e5804f0c28a3
Duane Wessels <wessels@packet-pushers.com>
1998-08-03 19:29:09 +0000

    DUNIX poll() and pipe() are incompatible

--- src/unlinkd.c
+++ src/unlinkd.c
@@ -132,7 +132,12 @@ unlinkdInit(void)
     struct timeval slp;
     args[0] = "(unlinkd)";
     args[1] = NULL;
+#if HAVE_POLL && defined(_SQUID_OSF_)
+    /* pipes and poll() don't get along on DUNIX -DW */
+    x = ipcCreate(IPC_TCP_SOCKET,
+#else
     x = ipcCreate(IPC_FIFO,
+#endif
 	Config.Program.unlinkd,
 	args,
 	"unlinkd",
