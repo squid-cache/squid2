9bcfcf194b6f71c0c671c46628e37991dd6b9b03
Duane Wessels <wessels@packet-pushers.com>
1998-08-17 23:49:56 +0000

    wrong default port for https

--- src/url.c
+++ src/url.c
@@ -166,8 +166,9 @@ urlDefaultPort(protocol_t p)
 {
     switch (p) {
     case PROTO_HTTP:
-    case PROTO_HTTPS:
 	return 80;
+    case PROTO_HTTPS:
+	return 443;
     case PROTO_FTP:
 	return 21;
     case PROTO_GOPHER:
