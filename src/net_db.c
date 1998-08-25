bcd51a977729d4a8ed2863cc190235ede6f4f38d
Duane Wessels <wessels@packet-pushers.com>
1998-08-25 19:09:20 +0000

    added netdbHostData as a faster way to get #samples, RTT, and HOPS

--- src/net_db.c
+++ src/net_db.c
@@ -790,6 +790,19 @@ netdbHostRtt(const char *host)
     return 0;
 }
 
+void
+netdbHostData(const char *host, int *samp, int *rtt, int *hops)
+{
+#if USE_ICMP
+	netdbEntry *n = netdbLookupHost(host);
+	if (n == NULL)
+		return;
+	*samp = n->pings_recv;
+	*rtt = (int) (n->rtt + 0.5);
+	*hops = (int) (n->hops + 0.5);
+#endif
+}
+
 int
 netdbHostPeerRtt(const char *host, peer * peer)
 {
