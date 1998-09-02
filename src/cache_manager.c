a79998eedde2cf365c8edc9cb969718abe3d4472
Duane Wessels <wessels@packet-pushers.com>
1998-09-02 17:21:00 +0000

    debug

--- src/cache_manager.c
+++ src/cache_manager.c
@@ -112,12 +112,12 @@ cachemgrParseUrl(const char *url)
     if (t < 2) {
 	xstrncpy(request, "menu", MAX_URL);
     } else if ((a = cachemgrFindAction(request)) == NULL) {
-	debug(16, 0) ("cachemgrParseUrl: action '%s' not found\n", request);
+	debug(16, 1) ("cachemgrParseUrl: action '%s' not found\n", request);
 	return NULL;
     } else {
 	prot = cachemgrActionProtection(a);
 	if (!strcmp(prot, "disabled") || !strcmp(prot, "hidden")) {
-	    debug(16, 0) ("cachemgrParseUrl: action '%s' is %s\n", request, prot);
+	    debug(16, 1) ("cachemgrParseUrl: action '%s' is %s\n", request, prot);
 	    return NULL;
 	}
     }
