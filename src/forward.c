f64f5c60314e76a81d150ed21b8dca004cd6fe68
Duane Wessels <wessels@packet-pushers.com>
1998-09-21 20:53:37 +0000

    don't call peerSelect() for URNs

--- src/forward.c
+++ src/forward.c
@@ -290,10 +290,8 @@ fwdDispatch(FwdState * fwdState)
 	    break;
 	case PROTO_CACHEOBJ:
 	case PROTO_INTERNAL:
-	    fatal_dump("Should never get here");
-	    break;
 	case PROTO_URN:
-	    urnStart(request, entry);
+	    fatal_dump("Should never get here");
 	    break;
 	case PROTO_WHOIS:
 	    whoisStart(fwdState, fwdState->server_fd);
@@ -351,6 +349,9 @@ fwdStart(int fd, StoreEntry * e, request_t * r, struct in_addr peer_addr)
 	return;
     case PROTO_CACHEOBJ:
 	cachemgrStart(fd, r, e);
+	return;
+    case PROTO_URN:
+	urnStart(r, e);
 	return;
     default:
 	break;
