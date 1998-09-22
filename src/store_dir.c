62dd22feaaa44a48bb82b39fd0d0a5c7295330a4
Duane Wessels <wessels@packet-pushers.com>
1998-09-22 19:47:46 +0000

    use fundamental filesystem block size

--- src/store_dir.c
+++ src/store_dir.c
@@ -602,9 +602,9 @@ storeDirStats(StoreEntry * sentry)
         (((fsbs) != 0 && (fsbs) < (bs)) ? \
                 (num) / ((bs) / (fsbs)) : (num) * ((fsbs) / (bs)))
 	if (!statvfs(SD->path, &sfs)) {
-	    storeAppendPrintf(sentry, "Filesystem Space in use: %d/%d (%d%%)\n",
-		fsbtoblk((sfs.f_blocks - sfs.f_bfree), sfs.f_bsize, 1024),
-		fsbtoblk(sfs.f_blocks, sfs.f_bsize, 1024),
+	    storeAppendPrintf(sentry, "Filesystem Space in use: %d/%d KB (%d%%)\n",
+		fsbtoblk((sfs.f_blocks - sfs.f_bfree), sfs.f_frsize, 1024),
+		fsbtoblk(sfs.f_blocks, sfs.f_frsize, 1024),
 		percent(sfs.f_blocks - sfs.f_bfree, sfs.f_blocks));
 	    storeAppendPrintf(sentry, "Filesystem Inodes in use: %d/%d (%d%%)\n",
 		sfs.f_files - sfs.f_ffree, sfs.f_files,
