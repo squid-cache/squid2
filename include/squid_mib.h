18f5a2d0b26d61497dd8488f9db550467a74c2ad
glenn <glenn>
1998-09-23 21:31:57 +0000

    Added and removed a number of squid objects

--- include/squid_mib.h
+++ include/squid_mib.h
@@ -5,13 +5,7 @@
 struct MIBListEntry squidMIBList[] =
 {
     {
-	{SYSMIB}, LEN_SYSMIB, basicGetFn, basicGetNextFn},
-    {
 	{SQ_SYS}, LEN_SQ_SYS, sysGetFn, sysGetNextFn},
-    {
-	{SQ_SYS, 3}, LEN_SQ_SYS + 1, sysConnGetFn, sysConnGetNextFn},
-    {
-	{SQ_SYS, 4}, LEN_SQ_SYS + 1, sysFdGetFn, sysFdGetNextFn},
     {
 	{SQ_CONF}, LEN_SQ_CONF, confGetFn, confGetNextFn},
     {
@@ -21,9 +15,11 @@ struct MIBListEntry squidMIBList[] =
     {
 	{SQ_PRF, 2}, LEN_SQ_PRF + 1, prfProtoGetFn, prfProtoGetNextFn},
     {
-	{SQ_NET, 1}, LEN_SQ_NET + 1, netdbGetFn, netdbGetNextFn},
+        {SQ_NET, 1}, LEN_SQ_NET + 1, netIpGetFn, netIpGetNextFn},
     {
-	{SQ_NET, 2}, LEN_SQ_NET + 1, dnsGetFn, dnsGetNextFn},
+        {SQ_NET, 2}, LEN_SQ_NET + 1, netFqdnGetFn, netFqdnGetNextFn},
+    {
+        {SQ_NET, 3}, LEN_SQ_NET + 1, netDnsGetFn, netDnsGetNextFn},
     {
 	{SQ_MESH, 1}, LEN_SQ_MESH + 1, meshGetFn, meshPtblGetNextFn},
     {
