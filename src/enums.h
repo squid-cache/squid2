1ea9941efe85e205fd05079c3a360288087c3889
rousskov <rousskov>
1998-09-24 20:20:20 +0000

    - added support for undocumented Request-Range header   the header is used by Netscape (with Adobe plugin working)   Request-Range is assumed to match Range header so only   the former is actually used when both are provided.

--- src/enums.h
+++ src/enums.h
@@ -212,6 +212,7 @@ typedef enum {
     HDR_PROXY_CONNECTION,
     HDR_PUBLIC,
     HDR_RANGE,
+    HDR_REQUEST_RANGE,          /* some clients use this, sigh */
     HDR_REFERER,
     HDR_RETRY_AFTER,
     HDR_SERVER,
