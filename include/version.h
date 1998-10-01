d71d8aa50255c0cc074cf72d71f7316238de4cd0
Duane Wessels <wessels@packet-pushers.com>
1998-10-01 22:57:11 +0000

    2.1.PRE1

--- include/version.h
+++ include/version.h
@@ -4,7 +4,7 @@
  *  SQUID_VERSION - String for version id of this distribution
  */
 #ifndef SQUID_VERSION
-#define SQUID_VERSION	"2.0.PRE1"
+#define SQUID_VERSION	"2.1.PRE1"
 #endif
 
 #ifndef SQUID_RELEASE_TIME
