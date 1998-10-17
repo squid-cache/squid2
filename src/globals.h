f3972d023dae9092a70a4385e33ed1890bdeebc7
rousskov <rousskov>
1998-10-17 04:53:40 +0000

    - updated current Cache Digests version to "4" (required version remains "3")   the change will help to track down caches that use old refresh algorithm

--- src/globals.h
+++ src/globals.h
@@ -132,7 +132,7 @@ extern const int CacheDigestHashFuncCount;	/* 4 */
 extern CacheDigest *store_digest;	/* NULL */
 extern const char *StoreDigestUrlPath;	/* "store_digest" */
 extern const char *StoreDigestMimeStr;	/* "application/cache-digest" */
-extern const Version CacheDigestVer;	/* { 3, 3 } */
+extern const Version CacheDigestVer;	/* { 4, 3 } */
 extern const char *MultipartMsgBoundaryStr;	/* "Unique-Squid-Separator" */
 extern icpUdpData *IcpQueueHead;	/* NULL */
 #if DELAY_POOLS
