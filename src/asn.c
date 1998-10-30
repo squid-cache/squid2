e37f900dd942f9a58bdb94d6975a1cf23166611a
Duane Wessels <wessels@packet-pushers.com>
1998-10-30 17:13:29 +0000

    assert bugs

--- src/asn.c
+++ src/asn.c
@@ -411,11 +411,12 @@ printRadixNode(struct radix_node *rn, void *w)
     struct in_addr addr;
     struct in_addr mask;
     assert(e);
+    assert(e->e_info);
     (void) get_m_int(addr.s_addr, e->e_addr);
     (void) get_m_int(mask.s_addr, e->e_mask);
     storeAppendPrintf(sentry, "%15s/%d\t",
 	inet_ntoa(addr), mask_len(ntohl(mask.s_addr)));
-    assert(as_info == e->e_info);
+    as_info = e->e_info;
     assert(as_info->as_number);
     for (q = as_info->as_number; q; q = q->next)
 	storeAppendPrintf(sentry, " %d", q->i);
