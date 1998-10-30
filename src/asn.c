98a5be1990a1705865eb29ecf8f8e5a1a9398c6c
Duane Wessels <wessels@packet-pushers.com>
1998-10-30 00:39:02 +0000

    typo

--- src/asn.c
+++ src/asn.c
@@ -415,7 +415,7 @@ printRadixNode(struct radix_node *rn, void *w)
     (void) get_m_int(mask.s_addr, e->e_mask);
     storeAppendPrintf(sentry, "%15s/%d\t",
 	inet_ntoa(addr), mask_len(ntohl(mask.s_addr)));
-    assert(as_info = e->e_info);
+    assert(as_info == e->e_info);
     assert(as_info->as_number);
     for (q = as_info->as_number; q; q = q->next)
 	storeAppendPrintf(sentry, " %d", q->i);
