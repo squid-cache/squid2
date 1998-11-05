75ee8ac469b63a04e166b6e78d8c9c777aab5387
Duane Wessels <wessels@packet-pushers.com>
1998-11-05 21:59:06 +0000

    config dump debugging

--- src/cache_cf.c
+++ src/cache_cf.c
@@ -410,8 +410,10 @@ dump_acl(StoreEntry * entry, const char *name, acl * ae)
     wordlist *w;
     wordlist *v;
     while (ae != NULL) {
+	debug(3, 3) ("dump_acl: %s %s\n", name, ae->name);
 	v = w = aclDumpGeneric(ae);
 	while (v != NULL) {
+	    debug(3, 3) ("dump_acl: %s %s %s\n", name, ae->name, v->key);
 	    storeAppendPrintf(entry, "%s %s %s %s\n",
 		name,
 		ae->name,
