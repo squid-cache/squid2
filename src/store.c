091597ad9e96edcdf4ed7cb6146fcbe7fce020e6
rousskov <rousskov>
1998-11-13 22:06:36 +0000

    - added title row to storeCheckCachableStats to avoid first line being treated   as a title

--- src/store.c
+++ src/store.c
@@ -529,6 +529,8 @@ storeCheckCachable(StoreEntry * e)
 static void
 storeCheckCachableStats(StoreEntry * sentry)
 {
+    storeAppendPrintf(sentry, "Category\t Count\n");
+
     storeAppendPrintf(sentry, "no.non_get\t%d\n",
 	store_check_cachable_hist.no.non_get);
     storeAppendPrintf(sentry, "no.not_entry_cachable\t%d\n",
