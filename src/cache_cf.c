1c90cf3cd3e6e884ad647ecd09c2d2d4ea4211b3
Duane Wessels <wessels@packet-pushers.com>
1999-01-29 21:36:47 +0000

    go ahead and print "none" and "disable" cachemgr passwords

--- src/cache_cf.c
+++ src/cache_cf.c
@@ -862,7 +862,10 @@ dump_cachemgrpasswd(StoreEntry * entry, const char *name, cachemgr_passwd * list
 {
     wordlist *w;
     while (list != NULL) {
-	storeAppendPrintf(entry, "%s XXXXXXXXXX", name);
+	if (strcmp(list->passwd, "none") && strcmp(list->passwd, "disable"))
+	    storeAppendPrintf(entry, "%s XXXXXXXXXX", name);
+	else
+	    storeAppendPrintf(entry, "%s %s", name, list->passwd);
 	for (w = list->actions; w != NULL; w = w->next) {
 	    storeAppendPrintf(entry, " %s", w->key);
 	}
