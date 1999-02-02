6df4a60f4a07d0f954f6640c5c762d1e974cdb44
Duane Wessels <wessels@packet-pushers.com>
1999-02-02 23:57:40 +0000

    2.2.PRE3

--- include/version.h
+++ include/version.h
@@ -4,7 +4,7 @@
  *  SQUID_VERSION - String for version id of this distribution
  */
 #ifndef SQUID_VERSION
-#define SQUID_VERSION	"2.2.PRE2"
+#define SQUID_VERSION	"2.2.PRE3"
 #endif
 
 #ifndef SQUID_RELEASE_TIME
