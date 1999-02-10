069ec9204ffee7e5866ddfdb423102be7f0a27e0
Duane Wessels <wessels@packet-pushers.com>
1999-02-10 23:21:49 +0000

    henrik: proxy authentication

--- src/cachemgr.c
+++ src/cachemgr.c
@@ -725,6 +725,7 @@ static const char *
 make_auth_header(const cachemgr_request * req)
 {
     static char buf[1024];
+    off_t l = 0;
     const char *str64;
     if (!req->passwd)
 	return "";
@@ -734,6 +735,8 @@ make_auth_header(const cachemgr_request * req)
 	req->passwd);
 
     str64 = base64_encode(buf);
-    snprintf(buf, sizeof(buf), "Authorization: Basic %s\r\n", str64);
+    l += snprintf(buf, sizeof(buf), "Authorization: Basic %s\r\n", str64);
+    l += snprintf(&buf[l], sizeof(buf) - l,
+	"Proxy-Authorization: Basic %s\r\n", str64);
     return buf;
 }
