56ef05791366a54cf34755d4132aa1fc8f67844f
Duane Wessels <wessels@packet-pushers.com>
1999-02-12 22:32:15 +0000

    Opening_FD tracks queued file open requests

--- src/stat.c
+++ src/stat.c
@@ -587,6 +587,8 @@ info_get(StoreEntry * sentry)
 	Biggest_FD);
     storeAppendPrintf(sentry, "\tNumber of file desc currently in use: %4d\n",
 	Number_FD);
+    storeAppendPrintf(sentry, "\nFiles queued for open:                %4d\n",
+	Opening_FD);
     storeAppendPrintf(sentry, "\tAvailable number of file descriptors: %4d\n",
 	fdNFree());
     storeAppendPrintf(sentry, "\tReserved number of file descriptors:  %4d\n",
