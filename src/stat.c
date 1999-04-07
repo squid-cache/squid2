9eeae94df9b0b25d7a7ae80ccf1fdb3c1a7d7cdc
Duane Wessels <wessels@packet-pushers.com>
1999-04-07 23:27:31 +0000

    added very cryptic asterisk on filedescriptor read/write counters to indicate a handler is present.

--- src/stat.c
+++ src/stat.c
@@ -407,7 +407,7 @@ statFiledescriptors(StoreEntry * sentry)
     int i;
     fde *f;
     storeAppendPrintf(sentry, "Active file descriptors:\n");
-    storeAppendPrintf(sentry, "%-4s %-6s %-4s %-7s %-7s %-21s %s\n",
+    storeAppendPrintf(sentry, "%-4s %-6s %-4s %-7s* %-7s* %-21s %s\n",
 	"File",
 	"Type",
 	"Tout",
@@ -415,17 +415,19 @@ statFiledescriptors(StoreEntry * sentry)
 	"Nwrite",
 	"Remote Address",
 	"Description");
-    storeAppendPrintf(sentry, "---- ------ ---- ------- ------- --------------------- ------------------------------\n");
+    storeAppendPrintf(sentry, "---- ------ ---- -------- -------- --------------------- ------------------------------\n");
     for (i = 0; i < Squid_MaxFD; i++) {
 	f = &fd_table[i];
 	if (!f->flags.open)
 	    continue;
-	storeAppendPrintf(sentry, "%4d %-6.6s %4d %7d %7d %-21s %s\n",
+	storeAppendPrintf(sentry, "%4d %-6.6s %4d %7d%c %7d%c %-21s %s\n",
 	    i,
 	    fdTypeStr[f->type],
 	    f->timeout_handler ? (int) (f->timeout - squid_curtime) / 60 : 0,
 	    f->bytes_read,
+	    f->read_handler ? '*' : ' ',
 	    f->bytes_written,
+	    f->write_handler ? '*' : ' ',
 	    fdRemoteAddr(f),
 	    f->desc);
     }
