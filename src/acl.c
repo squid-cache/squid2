9d8ec448411ff46c50faeefb9e6dbc3043bb6231
Duane Wessels <wessels@packet-pushers.com>
1999-04-07 20:03:48 +0000

    missing ACL_SRC_DOM_REGEX and ACL_DST_DOM_REGEX in case statement for destroying ACL entries.  caused fall throught to default case and assert(0);

--- src/acl.c
+++ src/acl.c
@@ -1786,6 +1786,8 @@ aclDestroyAcls(acl ** head)
 	    break;
 	case ACL_URL_REGEX:
 	case ACL_URLPATH_REGEX:
+	case ACL_SRC_DOM_REGEX:
+	case ACL_DST_DOM_REGEX:
 	case ACL_BROWSER:
 	    aclDestroyRegexList(a->data);
 	    break;
