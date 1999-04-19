edfa14f3bd6f8d936ba798d1cbd430f44ca6c068
Duane Wessels <wessels@packet-pushers.com>
1999-04-19 04:42:37 +0000

    try putting border on tables

--- src/cachemgr.c
+++ src/cachemgr.c
@@ -239,7 +239,7 @@ auth_html(char *host, int port, const char *user_name)
     printf("for the Squid object cache.</P>\n");
     printf("<HR>\n");
     printf("<FORM METHOD=\"GET\" ACTION=\"%s\">\n", script_name);
-    printf("<TABLE BORDER=0>\n");
+    printf("<TABLE BORDER=\"0\" CELLPADDING=\"10\" CELLSPACING=\"1\">\n");
     printf("<TR><TH ALIGN=\"left\">Cache Host:</TH><TD><INPUT NAME=\"host\" ");
     printf("SIZE=30 VALUE=\"%s\"></TD></TR>\n", host);
     printf("<TR><TH ALIGN=\"left\">Cache Port:</TH><TD><INPUT NAME=\"port\" ");
