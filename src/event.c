231791c78957d086a9995435acbde8895791b832
Duane Wessels <wessels@packet-pushers.com>
1999-04-19 03:53:36 +0000

    show if cbdata valid in event queue for cachemgr

--- src/event.c
+++ src/event.c
@@ -165,13 +165,15 @@ static void
 eventDump(StoreEntry * sentry)
 {
     struct ev_entry *e = tasks;
-    storeAppendPrintf(sentry, "%s\t%s\t%s\n",
+    storeAppendPrintf(sentry, "%s\t%s\t%s\t%s\n",
 	"Operation",
 	"Next Execution",
-	"Weight");
+	"Weight",
+	"Callback Valid?");
     while (e != NULL) {
-	storeAppendPrintf(sentry, "%s\t%f seconds\t%d\n",
-	    e->name, e->when - current_dtime, e->weight);
+	storeAppendPrintf(sentry, "%s\t%f seconds\t%d\t%s\n",
+	    e->name, e->when - current_dtime, e->weight,
+	    e->arg ? cbdataValid(e->arg) ? "yes" : "no" : "N/A");
 	e = e->next;
     }
 }
