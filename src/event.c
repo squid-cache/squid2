5d7de6e82c5b1cbbf7cf2a602bd5c2b00cdb3038
Duane Wessels <wessels@packet-pushers.com>
1999-04-19 03:37:29 +0000

    add weight to cachemgr stats

--- src/event.c
+++ src/event.c
@@ -165,12 +165,13 @@ static void
 eventDump(StoreEntry * sentry)
 {
     struct ev_entry *e = tasks;
-    storeAppendPrintf(sentry, "%s\t%s\n",
+    storeAppendPrintf(sentry, "%s\t%s\t%s\n",
 	"Operation",
-	"Next Execution");
+	"Next Execution",
+	"Weight");
     while (e != NULL) {
-	storeAppendPrintf(sentry, "%s\t%f seconds\n",
-	    e->name, e->when - current_dtime);
+	storeAppendPrintf(sentry, "%s\t%f seconds\t%d\n",
+	    e->name, e->when - current_dtime, e->weight);
 	e = e->next;
     }
 }
