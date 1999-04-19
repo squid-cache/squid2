8988f66fa618a9551c049e6ffc2c59d4a404ca77
Duane Wessels <wessels@packet-pushers.com>
1999-04-19 07:16:29 +0000

    Fixed queue traversal.  we only check the last queue entry because n->prev was always NULL after removing it from the double linked list.

--- src/dns_internal.c
+++ src/dns_internal.c
@@ -318,14 +318,16 @@ static void
 idnsCheckQueue(void *unused)
 {
     dlink_node *n;
+    dlink_node *p = NULL;
     idns_query *q;
     event_queued = 0;
-    for (n = lru_list.tail; n; n = n->prev) {
+    for (n = lru_list.tail; n; n = p) {
 	q = n->data;
 	if (tvSubDsec(q->sent_t, current_time) < 5.0)
 	    break;
 	debug(78, 3) ("idnsCheckQueue: ID %#04x timeout\n",
 	    q->id);
+	p = n->prev;
 	dlinkDelete(&q->lru, &lru_list);
 	if (q->nsends < IDNS_MAX_TRIES) {
 	    idnsSendQuery(q);
