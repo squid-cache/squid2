e3e567c6569faa972fd0914debd5258dfa752bad
glenn <glenn>
1999-04-29 21:48:21 +0000

    Corrected SNMP GET crash from uninitialised memory being read

--- src/snmp_agent.c
+++ src/snmp_agent.c
@@ -412,7 +412,13 @@ snmp_prfProtoFn(variable_list * Var, snint * ErrP)
 	return Answer;
     case PERF_PROTOSTAT_MEDIAN:
 
-	minutes = Var->name[LEN_SQ_PRF + 4];
+	if(Var->name_length == LEN_SQ_PRF + 5)
+	    minutes = Var->name[LEN_SQ_PRF + 4];
+	else
+	    break;
+
+	if((minutes < 1) || (minutes > 60))
+	    break;
 
 	f = snmpStatGet(0);
 	l = snmpStatGet(minutes);
