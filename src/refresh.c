4e416d06abf366d6bd899505e061f644ce130a23
Duane Wessels <wessels@packet-pushers.com>
1999-05-03 20:37:49 +0000

    fix refresh stats    - show 0 entries too    - put a new line after "total" and before FYI entries

--- src/refresh.c
+++ src/refresh.c
@@ -328,7 +328,6 @@ refreshCountsStats(StoreEntry * sentry, struct RefreshCounts *rc)
     storeAppendPrintf(sentry, "Category\tCount\t%%Total\n");
 
 #define refreshCountsStatsEntry(name) { \
-    if (rc->name || !strcmp(#name, "total")) \
 	storeAppendPrintf(sentry, "%s\t%6d\t%6.2f\n", \
 	    #name, rc->name, xpercent(rc->name, tot)); \
     sum += rc->name; \
@@ -350,6 +349,7 @@ refreshCountsStats(StoreEntry * sentry, struct RefreshCounts *rc)
     tot = sum;			/* paranoid: "total" line shows 100% if we forgot nothing */
     refreshCountsStatsEntry(total);
     /* maybe counters */
+    storeAppendPrintf(sentry, "\n");
     refreshCountsStatsEntry(request_reload_ignore_maybe);
     refreshCountsStatsEntry(response_lmt_future_maybe);
 }
