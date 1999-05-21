6cb74574865b21f3e3b95e69af02ff41dfbf1268
Duane Wessels <wessels@packet-pushers.com>
1999-05-21 22:23:25 +0000

    handle the case when storeOpen() fails.

--- src/store_client.c
+++ src/store_client.c
@@ -270,7 +270,13 @@ storeClientCopy2(StoreEntry * e, store_client * sc)
 	} else if (!sc->flags.disk_io_pending) {
 	    sc->flags.disk_io_pending = 1;
 	    sc->swapin_sio = storeSwapInStart(e);
-	    storeClientFileRead(sc);
+	    if (NULL == sc->swapin_sio) {
+		sc->flags.disk_io_pending = 0;
+		sc->callback = NULL;
+		callback(sc->callback_data, sc->copy_buf, -1);
+	    } else {
+		storeClientFileRead(sc);
+	    }
 	} else {
 	    debug(20, 2) ("storeClientCopy2: Averted multiple fd operation\n");
 	}
