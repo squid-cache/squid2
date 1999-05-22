f7890686c832b6f3b1cce77ffca845f7a76d86a2
Duane Wessels <wessels@packet-pushers.com>
1999-05-22 02:28:56 +0000

    Added log_type to 'active_requests' output

--- src/stat.c
+++ src/stat.c
@@ -1332,6 +1332,7 @@ statClientRequests(StoreEntry * s)
 		conn->defer.n, conn->defer.until);
 	}
 	storeAppendPrintf(s, "uri %s\n", http->uri);
+	storeAppendPrintf(s, "log_type %s\n", log_tags[http->log_type]);
 	storeAppendPrintf(s, "out.offset %d, out.size %d\n",
 	    http->out.offset, http->out.size);
 	storeAppendPrintf(s, "req_sz %d\n", http->req_sz);
