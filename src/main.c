1e9a717bee5f60f4a3ed30d48e50cf84a83006f9
Duane Wessels <wessels@packet-pushers.com>
1999-06-17 05:02:21 +0000

    reword -N usage description

--- src/main.c
+++ src/main.c
@@ -91,7 +91,7 @@ usage(void)
 	"       -z        Create swap directories\n"
 	"       -C        Do not catch fatal signals.\n"
 	"       -D        Disable initial DNS tests.\n"
-	"       -F        Foreground fast store rebuild.\n"
+	"       -F        Don't serve any requests until store is rebuilt.\n"
 	"       -N        No daemon mode.\n"
 	"       -R        Do not set REUSEADDR on port.\n"
 	"       -V        Virtual host httpd-accelerator.\n"
