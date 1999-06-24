24212fc9f64aa30c4b613b249a77334d6e618516
Duane Wessels <wessels@packet-pushers.com>
1999-06-24 22:40:21 +0000

    missing %'s on snprintf()!

--- src/acl.c
+++ src/acl.c
@@ -2392,7 +2392,7 @@ aclDumpArpListWalkee(void *node, void *state)
     static char buf[24];
     while (*W != NULL)
 	W = &(*W)->next;
-    snprintf(buf, sizeof(buf), "%02x:%02x:02x:02x:02x:02x",
+    snprintf(buf, sizeof(buf), "%02x:%02x:%02x:%02x:%02x:%02x",
 	arp->eth[0], arp->eth[1], arp->eth[2], arp->eth[3],
 	arp->eth[4], arp->eth[5]);
     wordlistAdd(state, buf);
