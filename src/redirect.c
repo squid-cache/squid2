1703e972e7586ed8fc63551a5097a6eafc8326eb
Duane Wessels <wessels@packet-pushers.com>
1999-06-24 22:08:43 +0000

    squidGuard is unable to do access-control on a per-user basis unless the following patch is made... From: Antony T Curtis <antony@abacus.co.uk>

--- src/redirect.c
+++ src/redirect.c
@@ -113,7 +113,9 @@ redirectStart(clientHttpRequest * http, RH * handler, void *data)
     cbdataAdd(r, cbdataXfree, 0);
     r->orig_url = xstrdup(http->uri);
     r->client_addr = conn->log_addr;
-    if (conn->ident == NULL || *conn->ident == '\0') {
+    if (http->request->user_ident[0])
+	r->client_ident = http->request->user_ident;
+    else if (conn->ident == NULL || *conn->ident == '\0') {
 	r->client_ident = dash_str;
     } else {
 	r->client_ident = conn->ident;
