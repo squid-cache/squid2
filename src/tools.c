0bebcf29cf462ba1b86f3b07332fcabc179d8b02
Duane Wessels <wessels@packet-pushers.com>
1999-07-11 15:21:15 +0000

    a NULL urlpath for error StoreEntry's caused xstrdup on null in ACL code
    check for struct mallinfo too

--- src/tools.c
+++ src/tools.c
@@ -105,7 +105,7 @@ dumpMallocStats(void)
     fprintf(debug_log, "\tTotal free:            %6d KB %d%%\n",
 	(int) (ms.bytes_free >> 10),
 	percent(ms.bytes_free, ms.bytes_total));
-#elif HAVE_MALLINFO
+#elif HAVE_MALLINFO && HAVE_STRUCT_MALLINFO
     struct mallinfo mp;
     int t;
     if (!do_mallinfo)
@@ -305,9 +305,9 @@ fatal_common(const char *message)
 #if HAVE_SYSLOG
     syslog(LOG_ALERT, "%s", message);
 #endif
-    fprintf(debug_log, "FATAL: pid %d %s\n", (int) getpid(), message);
+    fprintf(debug_log, "FATAL: %s\n", message);
     if (opt_debug_stderr && debug_log != stderr)
-	fprintf(stderr, "FATAL: pid %d %s\n", (int) getpid(), message);
+	fprintf(stderr, "FATAL: %s\n", message);
     fprintf(debug_log, "Squid Cache (Version %s): Terminated abnormally.\n",
 	version_string);
     fflush(debug_log);
