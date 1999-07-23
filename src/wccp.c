d4572c950ec8087975d2644b6b8ab6e53e7094e5
Duane Wessels <wessels@packet-pushers.com>
1999-07-23 21:36:38 +0000

    wccpConnectionOpen a little broken

--- src/wccp.c
+++ src/wccp.c
@@ -115,44 +115,42 @@ wccpConnectionOpen(void)
     u_short port = WCCP_PORT;
     struct sockaddr_in router, local;
     int local_len, router_len;
-
     debug(80, 5) ("wccpConnectionOpen: Called\n");
-    if (Config.Wccp.router.s_addr != inet_addr("0.0.0.0")) {
-	theInWccpConnection = comm_open(SOCK_DGRAM,
+    if (Config.Wccp.router.s_addr == inet_addr("0.0.0.0")) {
+	debug(1, 1) ("WCCP Disabled.\n");
+	return;
+    }
+    theInWccpConnection = comm_open(SOCK_DGRAM,
+	0,
+	Config.Wccp.incoming,
+	port,
+	COMM_NONBLOCKING,
+	"WCCP Port");
+    if (theInWccpConnection < 0)
+	fatal("Cannot open WCCP Port");
+    commSetSelect(theInWccpConnection, COMM_SELECT_READ, wccpHandleUdp, NULL, 0);
+    debug(1, 1) ("Accepting WCCP messages on port %d, FD %d.\n",
+	(int) port, theInWccpConnection);
+    if (Config.Wccp.outgoing.s_addr != no_addr.s_addr) {
+	theOutWccpConnection = comm_open(SOCK_DGRAM,
 	    0,
-	    Config.Wccp.incoming,
+	    Config.Wccp.outgoing,
 	    port,
 	    COMM_NONBLOCKING,
 	    "WCCP Port");
-	if (theInWccpConnection < 0)
-	    fatal("Cannot open WCCP Port");
-	commSetSelect(theInWccpConnection, COMM_SELECT_READ, wccpHandleUdp, NULL, 0);
-	debug(1, 1) ("Accepting WCCP messages on port %d, FD %d.\n",
-	    (int) port, theInWccpConnection);
-	if (Config.Wccp.outgoing.s_addr != no_addr.s_addr) {
-	    theOutWccpConnection = comm_open(SOCK_DGRAM,
-		0,
-		Config.Wccp.outgoing,
-		port,
-		COMM_NONBLOCKING,
-		"WCCP Port");
-	    if (theOutWccpConnection < 0)
-		fatal("Cannot open Outgoing WCCP Port");
-	    commSetSelect(theOutWccpConnection,
-		COMM_SELECT_READ,
-		wccpHandleUdp,
-		NULL, 0);
-	    debug(1, 1) ("Outgoing WCCP messages on port %d, FD %d.\n",
-		(int) port, theOutWccpConnection);
-	    fd_note(theOutWccpConnection, "Outgoing WCCP socket");
-	    fd_note(theInWccpConnection, "Incoming WCCP socket");
-	} else {
-	    theOutWccpConnection = theInWccpConnection;
-	}
+	if (theOutWccpConnection < 0)
+	    fatal("Cannot open Outgoing WCCP Port");
+	commSetSelect(theOutWccpConnection,
+	    COMM_SELECT_READ,
+	    wccpHandleUdp,
+	    NULL, 0);
+	debug(1, 1) ("Outgoing WCCP messages on port %d, FD %d.\n",
+	    (int) port, theOutWccpConnection);
+	fd_note(theOutWccpConnection, "Outgoing WCCP socket");
+	fd_note(theInWccpConnection, "Incoming WCCP socket");
     } else {
-	debug(1, 1) ("WCCP Disabled.\n");
+	theOutWccpConnection = theInWccpConnection;
     }
-
     router_len = sizeof(router);
     memset(&router, '\0', router_len);
     router.sin_family = AF_INET;
@@ -160,7 +158,6 @@ wccpConnectionOpen(void)
     router.sin_addr = Config.Wccp.router;
     if (connect(theOutWccpConnection, (struct sockaddr *) &router, router_len))
 	fatal("Unable to connect WCCP out socket");
-
     local_len = sizeof(local);
     memset(&local, '\0', local_len);
     if (getsockname(theOutWccpConnection, (struct sockaddr *) &local, &local_len))
