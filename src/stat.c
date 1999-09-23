442f23612d44d618424da875111593db849a91d7
Duane Wessels <wessels@packet-pushers.com>
1999-09-23 05:55:33 +0000

    check for struct mallinfo too

--- src/stat.c
+++ src/stat.c
@@ -441,7 +441,7 @@ info_get(StoreEntry * sentry)
     double runtime;
 #if HAVE_MSTATS && HAVE_GNUMALLOC_H
     struct mstats ms;
-#elif HAVE_MALLINFO
+#elif HAVE_MALLINFO && HAVE_STRUCT_MALLINFO
     struct mallinfo mp;
     int t;
 #endif
@@ -543,7 +543,7 @@ info_get(StoreEntry * sentry)
 	ms.bytes_total >> 10);
     storeAppendPrintf(sentry, "\tTotal free:            %6d KB %d%%\n",
 	ms.bytes_free >> 10, percent(ms.bytes_free, ms.bytes_total));
-#elif HAVE_MALLINFO
+#elif HAVE_MALLINFO && HAVE_STRUCT_MALLINFO
     mp = mallinfo();
     storeAppendPrintf(sentry, "Memory usage for %s via mallinfo():\n",
 	appname);
