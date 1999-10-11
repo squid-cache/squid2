4acafa03f1ef97d6147c16a2d4c5e7e9a6388b07
Duane Wessels <wessels@packet-pushers.com>
1999-10-11 19:02:34 +0000

    FMR bug.   If the lastNode is the one being deleted then we were accessing freed memory.

--- lib/heap.c
+++ lib/heap.c
@@ -177,7 +177,13 @@ heap_delete(heap * hp, heap_node * elm)
     _heap_swap_element(hp, lastNode, elm);
     heap_extractlast(hp);
 
-    if (hp->last > 0) {
+    if (elm == lastNode) {
+	/*
+	 * lastNode just got freed, so don't access it in the next
+	 * block.
+	 */
+	(void) 0;
+    } else if (hp->last > 0) {
 	if (lastNode->key < hp->nodes[Parent(lastNode->id)]->key)
 	    _heap_ify_up(hp, lastNode);		/* COOL! */
 	_heap_ify_down(hp, lastNode);
