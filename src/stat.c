3a576e5317d1308d2cf29f5ca757597f54bceb22
Duane Wessels <wessels@packet-pushers.com>
1999-10-14 21:08:09 +0000

    add tStorage Replacement Threshold to stats

--- src/stat.c
+++ src/stat.c
@@ -490,7 +490,8 @@ info_get(StoreEntry * sentry)
     storeAppendPrintf(sentry, "\tStorage Mem size:\t%d KB\n",
 	(int) (store_mem_size >> 10));
 #if HEAP_REPLACEMENT
-    /* The non-LRU policies do not use referenceAge */
+    storeAppendPrintf(sentry, "\tStorage Replacement Threshold:\t%f\n",
+	storeReplacementThreshold());
 #else
     storeAppendPrintf(sentry, "\tStorage LRU Expiration Age:\t%6.2f days\n",
 	(double) storeExpiredReferenceAge() / 86400.0);
