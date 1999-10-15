b871331b3d5644e676f0fa1088588dd8731cd0b4
Duane Wessels <wessels@packet-pushers.com>
1999-10-15 05:13:49 +0000

    DW became confused from the bug report from Alexy Leko.  The continue does NOT belong inside the if.

--- src/store_dir_ufs.c
+++ src/store_dir_ufs.c
@@ -627,11 +627,11 @@ storeGetNextFile(RebuildState * rb, int *sfileno, int *size)
 		rb->fullpath, rb->entry->d_name);
 	    debug(20, 3) ("storeGetNextFile: Opening %s\n", rb->fullfilename);
 	    fd = file_open(rb->fullfilename, O_RDONLY);
-	    if (fd < 0) {
+	    if (fd < 0)
 		debug(50, 1) ("storeGetNextFile: %s: %s\n", rb->fullfilename, xstrerror());
-		continue;
-	    }
-	    store_open_disk_fd++;
+	    else
+		store_open_disk_fd++;
+	    continue;
 	}
 	rb->in_dir = 0;
 	if (++rb->curlvl2 < rb->sd->u.ufs.l2)
