31b1471a1549e500e3adf2669458447234be196d
Duane Wessels <wessels@packet-pushers.com>
1999-11-22 04:03:30 +0000

    always generate Host: header from URL when forwarding, unless (a) URL was redirected and (b) redirect_rewrites_host_hdr is off.

--- src/http.c
+++ src/http.c
@@ -678,9 +678,16 @@ httpBuildRequestHeader(request_t * request,
 		request->flags.auth = 0;	/* We have used the authentication */
 	    break;
 	case HDR_HOST:
-	    /* Don't use client's Host: header for redirected requests */
-	    if (!request->flags.redirected || !Config.onoff.redir_rewrites_host)
-		httpHeaderAddEntry(hdr_out, httpHeaderEntryClone(e));
+	    /*
+	     * Normally Squid does not copy the Host: header from
+	     * a client request into the forwarded request headers.
+	     * However, there is one case when we do: If the URL
+	     * went through our redirector and the admin configured
+	     * 'redir_rewrites_host' to be off.
+	     */
+	    if (request->flags.redirected)
+		if (!Config.onoff.redir_rewrites_host)
+		    httpHeaderAddEntry(hdr_out, httpHeaderEntryClone(e));
 	    break;
 	case HDR_IF_MODIFIED_SINCE:
 	    /* append unless we added our own;
