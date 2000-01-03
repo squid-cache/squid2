73ab8314410c4f7f59db1d6d30b6b3523f51829f
Duane Wessels <wessels@packet-pushers.com>
2000-01-03 19:39:16 +0000

    define our assert macro to a NULL function if NODEBUG is defined.

--- src/squid.h
+++ src/squid.h
@@ -81,6 +81,8 @@
 
 #if PURIFY
 #define assert(EX) ((void)0)
+#elif defined(NODEBUG)
+#define assert(EX) ((void)0)
 #elif STDC_HEADERS
 #define assert(EX)  ((EX)?((void)0):xassert( # EX , __FILE__, __LINE__))
 #else
