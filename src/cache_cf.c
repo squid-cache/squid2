3e85d18ca8b14c364d9aebafc8dfc96e4f900364
Duane Wessels <wessels@packet-pushers.com>
2000-01-09 19:51:00 +0000

    DW:  - Fixed an 'anonymize_headers' bug that affects unknown HTTP headers.    With the bug, if you list a header that Squid doesn't know about    (such as CHARSET), it would add HDR_OTHER to the allow/deny mask.    This caused all unknown headers to be allowed or denied (depending    on the scheme you use).  Now, with the bug fixed, an unknown header    in the 'anonymize_headers' list is simply ignored.

--- src/cache_cf.c
+++ src/cache_cf.c
@@ -742,8 +742,8 @@ parse_http_header(HttpHeaderMask * header)
     }
     while ((t = strtok(NULL, w_space))) {
 	if ((id = httpHeaderIdByNameDef(t, strlen(t))) == -1)
-	    id = HDR_OTHER;
-	if (allowed)
+	    debug(3, 0) ("parse_http_header: Ignoring unknown header '%s'\n", t);
+	else if (allowed)
 	    CBIT_CLR(*header, id);
 	else
 	    CBIT_SET(*header, id);
@@ -1270,7 +1270,6 @@ parse_onoff(int *var)
 }
 
 #define free_onoff free_int
-#define free_httpanonymizer free_int
 #define dump_eol dump_string
 #define free_eol free_string
 
