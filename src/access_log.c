14a089a4bfd375774146da52ab4fae6e6ee99ebd
Duane Wessels <wessels@packet-pushers.com>
2000-01-09 04:33:57 +0000

    Oskar Pearson:  - More informative error messages when opening access log.

--- src/access_log.c
+++ src/access_log.c
@@ -234,8 +234,21 @@ accessLogOpen(const char *fname)
     xstrncpy(LogfileName, fname, SQUID_MAXPATHLEN);
     LogfileFD = file_open(LogfileName, O_WRONLY | O_CREAT);
     if (LogfileFD == DISK_ERROR) {
-	debug(50, 0) ("%s: %s\n", LogfileName, xstrerror());
-	fatalf("Cannot open %s: %s", LogfileName, xstrerror());
+	if (ENOENT == errno) {
+	    fatalf("%s cannot be created, since the\n"
+		"\tdirectory it is to reside in does not exist."
+		"\t(%s)\n", LogfileName, xstrerror());
+	} else if (EACCESS == errno) {
+	    fatalf("cannot create %s:\n"
+		"\t%s.\n"
+		"\tThe directory access.log is to reside in needs to be\n"
+		"\twriteable by the user %s, the cache_effective_user\n"
+		"\tset in squid.conf.",
+		LogfileName, xstrerror(), Config.effectiveUser);
+	} else {
+	    debug(50, 0) ("%s: %s\n", LogfileName, xstrerror());
+	    fatalf("Cannot open %s: %s", LogfileName, xstrerror());
+	}
     }
     LogfileStatus = LOG_ENABLE;
 }
