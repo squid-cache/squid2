5a78828fecdd59e5b46134141cfbfd2bc10d1eab
Duane Wessels <wessels@packet-pushers.com>
2000-02-09 23:29:47 +0000

    DW:  - UCSD legal discovered inappropriate joint copyright statement.

--- src/mime.c
+++ src/mime.c
@@ -12,10 +12,10 @@
  *  Internet community.  Development is led by Duane Wessels of the
  *  National Laboratory for Applied Network Research and funded by the
  *  National Science Foundation.  Squid is Copyrighted (C) 1998 by
- *  Duane Wessels and the University of California San Diego.  Please
- *  see the COPYRIGHT file for full details.  Squid incorporates
- *  software developed and/or copyrighted by other sources.  Please see
- *  the CREDITS file for full details.
+ *  the Regents of the University of California.  Please see the
+ *  COPYRIGHT file for full details.  Squid incorporates software
+ *  developed and/or copyrighted by other sources.  Please see the
+ *  CREDITS file for full details.
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -286,7 +286,6 @@ mimeInit(char *filename)
     regex_t re;
     mimeEntry *m;
     int re_flags = REG_EXTENDED | REG_NOSUB | REG_ICASE;
-return;
     if (filename == NULL)
 	return;
     if ((fp = fopen(filename, "r")) == NULL) {
