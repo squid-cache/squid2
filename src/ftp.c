2a48caf26eb572d07289fde7151e8292c4ed13e9
Duane Wessels <wessels@packet-pushers.com>
2000-03-08 17:59:58 +0000

    Koblinger Egmont (egmont at fazekas dot hu):  - ftp listings have old squid.nlanr.net address DW:  - remove link  - remove extra version_string printf arg

--- src/ftp.c
+++ src/ftp.c
@@ -422,11 +422,10 @@ ftpListingFinish(FtpStateData * ftpState)
     }
     storeAppendPrintf(e, "<HR>\n");
     storeAppendPrintf(e, "<ADDRESS>\n");
-    storeAppendPrintf(e, "Generated %s by %s (<a href=\"http://squid.nlanr.net/Squid/\">%s</a>)\n",
+    storeAppendPrintf(e, "Generated %s by %s (%s)\n",
 	mkrfc1123(squid_curtime),
 	getMyHostname(),
-	full_appname_string,
-	version_string);
+	full_appname_string);
     storeAppendPrintf(e, "</ADDRESS></BODY></HTML>\n");
     storeBufferFlush(e);
 }
