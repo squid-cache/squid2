9925145ad92f0aa6f916a1d082d0d1fcf8c002d1
Duane Wessels <wessels@packet-pushers.com>
2000-03-15 01:16:03 +0000

    Antonino Iannella <Antonino.Iannella@camtech.com.au>:  - Here is the new version of my MSNT authentication module.  It    has improved documentation, plus it includes a patch for the    guess user access problem reported about a month ago by Vadim    Popov.

---
+++ auth_modules/MSNT/smbauth.c
@@ -0,0 +1,69 @@
+
+/*
+  msntauth
+
+  Modified to act as a Squid authenticator
+  Removed all Pike stuff.
+  Returns OK for a successful authentication, or ERR upon error.
+
+  Antonino Iannella, Camtech SA Pty Ltd
+  Thu Sep 16 15:25:28 CST 1999
+
+  Uses code from -
+    Andrew Tridgell 1997
+    Richard Sharpe 1996
+    Bill Welliver 1999
+
+  Released under GNU Public License
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#include <stdio.h>
+
+/* You must specifiy these for your site! */
+
+#define PRIMARY_DC "my_pdc"
+#define BACKUP_DC  "my_bdc"
+#define NTDOMAIN   "my_domain"
+
+/* Main program for simple authentication.
+   This code could probably be better, might be
+   susceptible to buffer overflows. */
+
+int main()
+{
+  char username[256];
+  char password[256];
+
+  while (1)
+  {
+    scanf("%s %s", username, password);
+
+    if (Valid_User(username, password, PRIMARY_DC, BACKUP_DC, NTDOMAIN) == 0)
+       puts("OK");
+    else
+       puts("ERR");
+  }
+}
+
+/* Valid_User return codes -
+
+   0 - User authenticated successfully.
+   1 - Server error.
+   2 - Protocol error.
+   3 - Logon error; Incorrect password or username given.
+*/
+
