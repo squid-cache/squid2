42dc7093ec87f7693c52908c8e6700a9931d155b
Duane Wessels <wessels@packet-pushers.com>
2000-03-30 16:04:47 +0000

    Jens-S. Voeckler <voeckler@rvs.uni-hannover.de>:  - We're not checking EOF conditions after recv() from    Squid process.  Log EOF condition and return -1.

--- src/pinger.c
+++ src/pinger.c
@@ -304,6 +304,12 @@ pingerReadRequest(void)
     n = recv(0, (char *) &pecho, sizeof(pecho), 0);
     if (n < 0)
 	return n;
+    if (0 == n) {
+	/* EOF indicator */
+	fprintf(stderr, "EOF encountered\n");
+	errno = 0;
+	return -1;
+    }
     guess_size = n - (sizeof(pingerEchoData) - PINGER_PAYLOAD_SZ);
     if (guess_size != pecho.psize) {
 	fprintf(stderr, "size mismatch, guess=%d psize=%d\n",
