21950104c80a0c837fdaaefaee03e903ce51c42a
Henrik Nordstrom <hno@squid-cache.org>
2000-05-02 19:43:30 +0000

    hno squid-2.3.DEVEL2.cache_swap_log_name-2.patch Squid-2.3.DEVEL2: cache_swap_log name based on cache_dir name
    %s can now be used in cache_swap_log to build a log file name based on
    the cache_dir name instead of numbering the files according to their
    corresponding cache_dir location in squid.conf. This is very useful if
    you'd like to be able to add or remove cache directories while using
    cache_swap_log to have the index files stored outside the cache
    directories.

--- src/store_dir_ufs.c
+++ src/store_dir_ufs.c
@@ -202,12 +202,22 @@ static char *
 storeUfsDirSwapLogFile(SwapDir * sd, const char *ext)
 {
     LOCAL_ARRAY(char, path, SQUID_MAXPATHLEN);
+    LOCAL_ARRAY(char, pathtmp, SQUID_MAXPATHLEN);
     LOCAL_ARRAY(char, digit, 32);
+    char *pathtmp2;
     if (Config.Log.swap) {
-	xstrncpy(path, Config.Log.swap, SQUID_MAXPATHLEN - 64);
-	strcat(path, ".");
-	snprintf(digit, 32, "%02d", sd->index);
-	strncat(path, digit, 3);
+	xstrncpy(pathtmp, sd->path, SQUID_MAXPATHLEN - 64);
+	while (index(pathtmp,'/'))
+	    *index(pathtmp,'/')='.';
+	while (strlen(pathtmp) && pathtmp[strlen(pathtmp)-1]=='.')
+	    pathtmp[strlen(pathtmp)-1]= '\0';
+	for(pathtmp2 = pathtmp; *pathtmp2 == '.'; pathtmp2++);
+	snprintf(path, SQUID_MAXPATHLEN-64, Config.Log.swap, pathtmp2);
+	if (strncmp(path, Config.Log.swap, SQUID_MAXPATHLEN - 64) == 0) {
+	    strcat(path, ".");
+	    snprintf(digit, 32, "%02d", sd->index);
+	    strncat(path, digit, 3);
+	}
     } else {
 	xstrncpy(path, sd->path, SQUID_MAXPATHLEN - 64);
 	strcat(path, "/swap.state");
