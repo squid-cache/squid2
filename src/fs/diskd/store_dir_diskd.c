20e30fece6f93ab26f1028d264bc727021bfcabb
Duane Wessels <wessels@packet-pushers.com>
2000-05-28 22:42:39 +0000

    DW:  - Uninitialized memory is a bad thing.  Use xcalloc instead.  - xcalloc never returns NULL.  It either succeeds or the process aborts.

--- src/fs/diskd/store_dir_diskd.c
+++ src/fs/diskd/store_dir_diskd.c
@@ -2022,14 +2022,10 @@ storeDiskdDirParse(SwapDir * sd, int index, char *path)
 	if (!strcasecmp(token, "read-only"))
 	    read_only = 1;
 
-    diskdinfo = xmalloc(sizeof(diskdinfo_t));
-    if (diskdinfo == NULL)
-	fatal("storeDiskdDirParse: couldn't xmalloc() diskdinfo_t!\n");
-
+    sd->fsdata = diskdinfo = xcalloc(1, sizeof(*diskdinfo));
     sd->index = index;
     sd->path = xstrdup(path);
     sd->max_size = size;
-    sd->fsdata = diskdinfo;
     diskdinfo->l1 = l1;
     diskdinfo->l2 = l2;
     diskdinfo->swaplog_fd = -1;
