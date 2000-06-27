7bb8e1c0f4865444b1a17fa7ed1a3ffcb2c7bb52
Duane Wessels <wessels@packet-pushers.com>
2000-06-27 18:08:06 +0000

    DW:  - Patch from Daniel Kiracofe.  If internal DNS doesn't find any    name servers in resolv.conf or squid.conf, then exit with a    FATAL message.

--- src/dns_internal.c
+++ src/dns_internal.c
@@ -401,6 +401,10 @@ idnsInit(void)
     idnsParseNameservers();
     if (0 == nns)
 	idnsParseResolvConf();
+    if (0 == nns)
+	fatal("Could not find any nameservers.\n"
+	    "       Please check your /etc/resolv.conf file\n"
+	    "       or use the 'dns_nameservers' option in squid.conf.");
     if (!init) {
 	memDataInit(MEM_IDNS_QUERY, "idns_query", sizeof(idns_query), 0);
 	cachemgrRegister("idns",
