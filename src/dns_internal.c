0a2170c9ac2e953cffddae4941670d7095fa5b06
Duane Wessels <wessels@packet-pushers.com>
2000-07-22 17:56:55 +0000

    DW:  - missing newline on RCODE table output

--- src/dns_internal.c
+++ src/dns_internal.c
@@ -194,6 +194,7 @@ idnsStats(StoreEntry * sentry)
     storeAppendPrintf(sentry, "RCODE");
     for (i = 0; i < MAX_ATTEMPT; i++)
 	storeAppendPrintf(sentry, " ATTEMPT%d", i + 1);
+    storeAppendPrintf(sentry, "\n");
     for (j = 0; j < MAX_RCODE; j++) {
 	storeAppendPrintf(sentry, "%5d", j);
 	for (i = 0; i < MAX_ATTEMPT; i++)
