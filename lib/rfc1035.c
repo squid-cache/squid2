023ff7359e4d1d624fdf6e652eabfae87bf04023
Duane Wessels <wessels@packet-pushers.com>
2000-09-14 15:41:03 +0000

    DW:  - Note that the parsing code has a bug that treats names like    www...foo....com the same as www.foo.com.  No fix at this time.

--- lib/rfc1035.c
+++ lib/rfc1035.c
@@ -180,6 +180,9 @@ rfc1035NamePack(char *buf, size_t sz, const char *name)
     off_t off = 0;
     char *copy = strdup(name);
     char *t;
+    /*
+     * NOTE: use of strtok here makes names like foo....com valid.
+     */
     for (t = strtok(copy, "."); t; t = strtok(NULL, "."))
 	off += rfc1035LabelPack(buf + off, sz - off, t);
     free(copy);
