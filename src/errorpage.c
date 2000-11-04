d96f77a8a5834dfbf2f6d923c3e81b0cc0c2af04
Henrik Nordstrom <hno@squid-cache.org>
2000-11-04 23:04:50 +0000

    Cross-site scripting fixes by Robert Collins and Henrik Nordstrom
    Everywhere where Squid inserts text received from the network into
    a HTML page (error pages, FTP listings, Gopher listings, ...) care
    must be taken to ensure that the text is properly encoded as HTML,
    or a malicious user might be able to insert script code or other
    HTML tags, and exploit the web browser of any user visiting their
    page or clicking on that funny link received in a email..

--- src/errorpage.c
+++ src/errorpage.c
@@ -423,6 +423,7 @@ errorConvert(char token, ErrorState * err)
     request_t *r = err->request;
     static MemBuf mb = MemBufNULL;
     const char *p = NULL;	/* takes priority over mb if set */
+    int do_quote = 1;
 
     memBufReset(&mb);
     switch (token) {
@@ -524,6 +525,7 @@ errorConvert(char token, ErrorState * err)
 	    memBufPrintf(&mb, "%s", sign_mb.buf);
 	    memBufClean(&sign_mb);
 	    err->page_id = saved_id;
+	    do_quote = 0;
 	} else {
 	    /* wow, somebody put %S into ERR_SIGNATURE, stop recursion */
 	    p = "[%S]";
@@ -561,6 +563,8 @@ errorConvert(char token, ErrorState * err)
 	p = mb.buf;		/* do not use mb after this assignment! */
     assert(p);
     debug(4, 3) ("errorConvert: %%%c --> '%s'\n", token, p);
+    if (do_quote)
+	p = html_quote(p);
     return p;
 }
 
