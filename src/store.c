22ae7d131c3abce420ab58adc786b2c8dceb616c
Adrian Chadd <adrian@creative.net.au>
2000-11-18 10:46:32 +0000

    Don't return NULL in createRemovalPolicy() if there is no match for the given policy type. None of the FS code handles this correctly, and it was causing SIGSEGVs when a cache_replacement_policy was not set in squid.conf, defaulting to lru, and lru was not compiled in.
    We now warn very loudly and die with an error. cache.log will tell the
    user to check cache_replacement_policy and memory_replacement_policy
    in their squid.conf file.

--- src/store.c
+++ src/store.c
@@ -1292,8 +1292,10 @@ createRemovalPolicy(RemovalPolicySettings * settings)
 	if (strcmp(r->typestr, settings->type) == 0)
 	    return r->create(settings->args);
     }
-    debug(20, 1) ("Unknown policy %s\n", settings->type);
-    return NULL;
+    debug(20, 1) ("ERROR: Unknown policy %s\n", settings->type);
+    debug(20, 1) ("ERROR: Be sure to have set cache_replacement_policy\n");
+    debug(20, 1) ("ERROR:   and memory_replacement_policy in squid.conf!\n");
+    fatalf("ERROR: Unknown policy %s\n", settings->type);
 }
 
 #if 0
