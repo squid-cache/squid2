b1e6cbcf7cb790b4d6e359961b57a98908fd57fe
Duane Wessels <wessels@packet-pushers.com>
2000-12-08 23:58:08 +0000

    Patch from Marcos Barreto de Castro <mbdecastro@yahoo.com>.  Allows auth process to send back a one-line message.  If the ERR_ACCESS_DENIED page has %m, it gets replaced with this message.

--- src/acl.c
+++ src/acl.c
@@ -1770,10 +1770,15 @@ aclLookupProxyAuthDone(void *data, char *result)
     checklist->state[ACL_PROXY_AUTH] = ACL_LOOKUP_DONE;
     debug(28, 4) ("aclLookupProxyAuthDone: result = %s\n",
 	result ? result : "NULL");
-    if (result && (strncasecmp(result, "OK", 2) == 0))
-	checklist->auth_user->passwd_ok = 1;
-    else
+    if (NULL == result)
 	checklist->auth_user->passwd_ok = 0;
+    else if (0 == strncasecmp(result, "OK", 2))
+	checklist->auth_user->passwd_ok = 1;
+    else {
+	if (strlen(result) > sizeof("ERR "))
+	    checklist->auth_user->message = xstrdup(result+4);
+	checklist->auth_user->passwd_ok = 0;
+    }
     aclCheck(checklist);
 }
 
