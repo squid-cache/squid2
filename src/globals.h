f634dcfdb118b64714b6aa9239716cb8968b6fab
Duane Wessels <wessels@packet-pushers.com>
2000-12-09 01:48:18 +0000

    MFC: backing out store rebuild double-check patch

--- src/globals.h
+++ src/globals.h
@@ -87,7 +87,7 @@ extern int opt_reload_hit_only;	/* 0 */
 extern int opt_syslog_enable;	/* 0 */
 extern int opt_udp_hit_obj;	/* 0 */
 extern int opt_create_swap_dirs;	/* 0 */
-extern dblcheck_t opt_store_doublecheck;	/* DBLCHECK_NONE */
+extern int opt_store_doublecheck;	/* 0 */
 extern int syslog_enable;	/* 0 */
 extern int theInIcpConnection;	/* -1 */
 extern int theOutIcpConnection;	/* -1 */
