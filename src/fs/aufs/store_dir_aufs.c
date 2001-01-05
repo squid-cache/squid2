efd91e67e4ab9cc960c9644fe16a450da6df7741
Duane Wessels <wessels@packet-pushers.com>
2001-01-05 00:28:22 +0000

    Argh, the FS blocksize code assumed that blocksize would always be 1K or greater.  If the blocksize is 512 bytes, the 'kperblk' variable would be set to zero, which screws up cache size calculations.

--- src/fs/aufs/store_dir_aufs.c
+++ src/fs/aufs/store_dir_aufs.c
@@ -372,6 +372,8 @@ storeAufsDirInit(SwapDir * sd)
     }
     if (0 == storeDirGetBlkSize(sd->path, &sd->fs.blksize))
 	sd->fs.kperblk = sd->fs.blksize >> 10;
+    if (sd->fs.kperblk < 1)
+	sd->fs.kperblk = 1;
 }
 
 static void
