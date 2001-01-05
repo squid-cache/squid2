2b95db756bbfaeecaaa8d54019cdd969dff0b04d
Henrik Nordstrom <hno@squid-cache.org>
2001-01-05 20:40:52 +0000

    Removed an old cache cleanup which is no longer needed. It removed ftp://user@host/.. entries that erronously got cached by Squid-2.0.

--- src/ftp.c
+++ src/ftp.c
@@ -1078,10 +1078,6 @@ ftpStart(FwdState * fwd)
 	    snprintf(realm, 8192, "ftp %s port %d",
 		ftpState->user, request->port);
 	}
-	/* eject any old cached object */
-	key = storeKeyPublic(entry->mem_obj->url, entry->mem_obj->method);
-	if ((pe = storeGet(key)) != NULL)
-	    storeRelease(pe);
 	/* create reply */
 	reply = entry->mem_obj->reply;
 	assert(reply != NULL);
