35c41ffb62ee8c8e0428e6b9c3aaf4098b169240
Henrik Nordstrom <hno@squid-cache.org>
2001-01-07 14:44:32 +0000

    Protect splay.h from multiple inclusion

--- include/splay.h
+++ include/splay.h
@@ -2,6 +2,8 @@
  * $Id$
  */
 
+#ifndef _SPLAY_H
+#define _SPLAY_H
 
 typedef struct _splay_node {
     void *data;
@@ -19,3 +21,5 @@ extern splayNode *splay_insert(void *, splayNode *, SPLAYCMP *);
 extern splayNode *splay_splay(const void *, splayNode *, SPLAYCMP *);
 extern void splay_destroy(splayNode *, SPLAYFREE *);
 extern void splay_walk(splayNode *, SPLAYWALKEE *, void *);
+
+#endif /* _SPLAY_H */
