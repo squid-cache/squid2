319b2f5a5822089c77df9d678280a4b56cbcfea8
Henrik Nordstrom <hno@squid-cache.org>
2001-01-09 14:41:33 +0000

    login=*:password cache_peer option to pass the authenticated username to a upstream proxy in another adminstrative domain.

--- src/http.c
+++ src/http.c
@@ -789,9 +789,19 @@ httpBuildRequestHeader(request_t * request,
 	}
     }
     /* append Proxy-Authorization if configured for peer, and proxying */
-    if (!httpHeaderHas(hdr_out, HDR_PROXY_AUTHORIZATION)) {
-	if (request->flags.proxying && orig_request->peer_login &&
+    if (request->flags.proxying && orig_request->peer_login &&
+	    !httpHeaderHas(hdr_out, HDR_PROXY_AUTHORIZATION) &&
 	    strcmp(orig_request->peer_login, "PASS") != 0) {
+	if (*orig_request->peer_login == '*') {
+	    /* Special mode, to pass the username to the upstream cache */
+	    char loginbuf[256];
+	    char *username = "-";
+	    if (orig_request->auth_user_request)
+		username = authenticateUserRequestUsername(orig_request->auth_user_request);
+	    snprintf(loginbuf, sizeof(loginbuf), "%s%s", username, orig_request->peer_login+1);
+	    httpHeaderPutStrf(hdr_out, HDR_PROXY_AUTHORIZATION, "Basic %s",
+		base64_encode(loginbuf));
+	} else {
 	    httpHeaderPutStrf(hdr_out, HDR_PROXY_AUTHORIZATION, "Basic %s",
 		base64_encode(orig_request->peer_login));
 	}
