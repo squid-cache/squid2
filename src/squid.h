7d68973b4620a019fb34b18564c5e2c9f59e9853
Henrik Nordstrom <hno@squid-cache.org>
2001-01-10 20:18:42 +0000

    match squid.h's varargs include logic to that in include/snprintf.h (Adrian, GNATS bug id 20)

--- src/squid.h
+++ src/squid.h
@@ -231,10 +231,24 @@
 #endif /* HAVE_POLL_H */
 #endif /* HAVE_POLL */
 
-#if STDC_HEADERS
+#if defined(HAVE_STDARG_H)
 #include <stdarg.h>
+#define HAVE_STDARGS            /* let's hope that works everywhere (mj) */
+#define VA_LOCAL_DECL va_list ap;
+#define VA_START(f) va_start(ap, f)
+#define VA_SHIFT(v,t) ;         /* no-op for ANSI */
+#define VA_END va_end(ap)
 #else
+#if defined(HAVE_VARARGS_H)
 #include <varargs.h>
+#undef HAVE_STDARGS
+#define VA_LOCAL_DECL va_list ap;
+#define VA_START(f) va_start(ap)        /* f is ignored! */
+#define VA_SHIFT(v,t) v = va_arg(ap,t)
+#define VA_END va_end(ap)
+#else
+#error XX **NO VARARGS ** XX
+#endif
 #endif
 
 /* Make sure syslog goes after stdarg/varargs */
