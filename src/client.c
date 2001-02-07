df60cabc73c099e78e45d5da02fbebaa6f326377
Henrik Nordstrom <hno@squid-cache.org>
2001-02-07 19:10:12 +0000

    Cleaned up the namespace (local functions made static)

--- src/client.c
+++ src/client.c
@@ -46,7 +46,7 @@ static void usage(const char *progname);
 static int Now(struct timeval *);
 static SIGHDLR catch;
 static SIGHDLR pipe_handler;
-static void set_our_signal();
+static void set_our_signal(void);
 static ssize_t myread(int fd, void *buf, size_t len);
 static ssize_t mywrite(int fd, void *buf, size_t len);
 static int put_fd;
