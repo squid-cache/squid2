cfcfc644ab844850c68f212231d221170a57bd6b
Henrik Nordstrom <hno@squid-cache.org>
2001-02-25 21:49:07 +0000

    From Chemolli Francesco:
    Support for the types int16_t, int32_t, int64_t and their unsigned
    counterparts u_int16_t, u_int32_t, u_int64_t. Legacy support for num32
    is provided, but I recommend we deprecate num* types and (where struct
    packing is useful) we move in time fully to the new types.

--- acconfig.h
+++ acconfig.h
@@ -19,7 +19,10 @@
  * in the Makefiles, wich is done automatically by configure.
  *
  */
-@ TOP @
+
+#ifndef __CONFIGURE_H__
+#define __CONFIGURE_H__
+@TOP@
 /* $Id$ */
 
 /*********************************
@@ -290,3 +293,25 @@
  * Enable support for Transparent Proxy on Linux 2.4 systems
  */
 #undef LINUX_NETFILTER
+
+/*
+ * Known-size intgers
+ */
+
+#undef int16_t
+
+#undef u_int16_t
+
+#undef int32_t
+
+#undef u_int32_t
+
+#undef int64_t
+
+#undef u_int64_t
+
+
+
+@BOTTOM@
+
+#endif /* __CONFIGURE_H__ */
