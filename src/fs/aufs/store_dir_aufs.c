adc1a91f9c39eeac19ef44320088ce4ff1a0236c
Duane Wessels <wessels@packet-pushers.com>
2001-03-14 22:31:22 +0000

    MFC: delaypools/delay-pools typo and dangling HEAP_REPLACEMENTs

--- src/fs/aufs/store_dir_aufs.c
+++ src/fs/aufs/store_dir_aufs.c
@@ -1471,15 +1471,6 @@ storeAufsDirStats(SwapDir * SD, StoreEntry * sentry)
     if (SD->flags.read_only)
 	storeAppendPrintf(sentry, " READ-ONLY");
     storeAppendPrintf(sentry, "\n");
-#if OLD_UNUSED_CODE
-#if !HEAP_REPLACEMENT
-    storeAppendPrintf(sentry, "LRU Expiration Age: %6.2f days\n",
-	(double) storeAufsDirExpiredReferenceAge(SD) / 86400.0);
-#else
-    storeAppendPrintf(sentry, "Storage Replacement Threshold:\t%f\n",
-	heap_peepminkey(sd.repl.heap.heap));
-#endif
-#endif /* OLD_UNUSED_CODE */
 }
 
 static struct cache_dir_option options[] =
