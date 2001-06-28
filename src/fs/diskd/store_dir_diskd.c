6e19feb19a7afdda0171ca53f22583be6cd287ef
Duane Wessels <wessels@packet-pushers.com>
2001-06-28 05:19:57 +0000

    MFC: Bugzilla #185

--- src/fs/diskd/store_dir_diskd.c
+++ src/fs/diskd/store_dir_diskd.c
@@ -1701,7 +1701,8 @@ storeDiskdDirParseQ1(SwapDir * sd, const char *name, const char *value, int reco
     int old_magic1 = diskdinfo->magic1;
     diskdinfo->magic1 = atoi(value);
     if (reconfiguring && old_magic1 != diskdinfo->magic1)
-	debug(3, 1) ("cache_dir '%s' new Q1 value '%d'\n", diskdinfo->magic1);
+	debug(3, 1) ("cache_dir '%s' new Q1 value '%d'\n",
+	    sd->path, diskdinfo->magic1);
 }
 
 static void
@@ -1711,7 +1712,8 @@ storeDiskdDirParseQ2(SwapDir * sd, const char *name, const char *value, int reco
     int old_magic2 = diskdinfo->magic2;
     diskdinfo->magic2 = atoi(value);
     if (reconfiguring && old_magic2 != diskdinfo->magic2)
-	debug(3, 1) ("cache_dir '%s' new Q2 value '%d'\n", diskdinfo->magic2);
+	debug(3, 1) ("cache_dir '%s' new Q2 value '%d'\n", sd->path,
+	    diskdinfo->magic2);
 }
 
 struct cache_dir_option options[] =
