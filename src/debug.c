2b36c1ecba9def97dd969322989946efdc7515de
Duane Wessels <wessels@packet-pushers.com>
2001-06-29 21:21:53 +0000

    MFC: Bugzilla #177: LinuxPPC 2000 segfault bug

--- src/debug.c
+++ src/debug.c
@@ -39,75 +39,90 @@ static char *debug_log_file = NULL;
 static int Ctx_Lock = 0;
 static const char *debugLogTime(time_t);
 static void ctx_print();
+#if HAVE_SYSLOG
+static void _db_print_syslog(const char *format, va_list args);
+#endif
+static void _db_print_stderr(const char *format, va_list args);
+static void _db_print_file(const char *format, va_list args);
 
-#if STDC_HEADERS
 void
+#if STDC_HEADERS
 _db_print(const char *format,...)
 {
-#if defined(__QNX__)
-    va_list eargs;
-#endif
-    va_list args;
 #else
-void
 _db_print(va_alist)
      va_dcl
 {
-    va_list args;
     const char *format = NULL;
 #endif
     LOCAL_ARRAY(char, f, BUFSIZ);
-#if HAVE_SYSLOG
-    LOCAL_ARRAY(char, tmpbuf, BUFSIZ);
-#endif
-
+    va_list args1;
 #if STDC_HEADERS
-    va_start(args, format);
-#if defined(__QNX__)
-    va_start(eargs, format);
-#endif
+    va_list args2;
+    va_list args3;
+    va_start(args1, format);
+    va_start(args2, format);
+    va_start(args3, format);
 #else
-    va_start(args);
-    format = va_arg(args, const char *);
+#define args2 args1
+#define args3 args1
+    format = va_arg(args1, const char *);
 #endif
+    snprintf(f, BUFSIZ, "%s| %s",
+	debugLogTime(squid_curtime),
+	format);
+    _db_print_file(f, args1);
+    _db_print_stderr(f, args2);
+#if HAVE_SYSLOG
+    _db_print_syslog(format, args3);
+#endif
+    va_end(args1);
+#if STDC_HEADERS
+    va_end(args2);
+    va_end(args3);
+#endif
+}
 
+static void
+_db_print_file(const char *format, va_list args)
+{
     if (debug_log == NULL)
 	return;
     /* give a chance to context-based debugging to print current context */
     if (!Ctx_Lock)
 	ctx_print();
-    snprintf(f, BUFSIZ, "%s| %s",
-	debugLogTime(squid_curtime),
-	format);
-#if HAVE_SYSLOG
-    /* level 0,1 go to syslog */
-    if (_db_level <= 1 && opt_syslog_enable) {
-	tmpbuf[0] = '\0';
-	vsnprintf(tmpbuf, BUFSIZ, format, args);
-	tmpbuf[BUFSIZ - 1] = '\0';
-	syslog(_db_level == 0 ? LOG_WARNING : LOG_NOTICE, "%s", tmpbuf);
-    }
-#endif /* HAVE_SYSLOG */
-    /* write to log file */
-#if defined(__QNX__)
-    vfprintf(debug_log, f, eargs);
-#else
-    vfprintf(debug_log, f, args);
-#endif
+    vfprintf(debug_log, format, args);
     if (!Config.onoff.buffered_logs)
 	fflush(debug_log);
-    if (opt_debug_stderr >= _db_level && debug_log != stderr) {
-#if defined(__QNX__)
-	vfprintf(stderr, f, eargs);
-#else
-	vfprintf(stderr, f, args);
-#endif
-    }
-#if defined(__QNX__)
-    va_end(eargs);
-#endif
-    va_end(args);
 }
+
+static void
+_db_print_stderr(const char *format, va_list args)
+{
+    if (opt_debug_stderr < _db_level)
+	return;
+    if (debug_log == stderr)
+	return;
+    vfprintf(stderr, format, args);
+}
+
+
+#if HAVE_SYSLOG
+static void
+_db_print_syslog(const char *format, va_list args)
+{
+    LOCAL_ARRAY(char, tmpbuf, BUFSIZ);
+    /* level 0,1 go to syslog */
+    if (_db_level > 1)
+	return;
+    if (0 == opt_syslog_enable)
+	return;
+    tmpbuf[0] = '\0';
+    vsnprintf(tmpbuf, BUFSIZ, format, args);
+    tmpbuf[BUFSIZ - 1] = '\0';
+    syslog(_db_level == 0 ? LOG_WARNING : LOG_NOTICE, "%s", tmpbuf);
+}
+#endif /* HAVE_SYSLOG */
 
 static void
 debugArg(const char *arg)
