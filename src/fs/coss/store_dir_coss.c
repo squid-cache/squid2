55c46e11cfd7e261e2b22decef12af2ef2ebd204
Adrian Chadd <adrian@creative.net.au>
2001-08-09 21:41:53 +0000

    COSS fixes from diskio. This code is now stable, but <2gb and sync. Next, >2gb.

--- src/fs/coss/store_dir_coss.c
+++ src/fs/coss/store_dir_coss.c
@@ -764,12 +764,15 @@ storeCossDirParse(SwapDir * sd, int index, char *path)
     cs->fd = -1;
     cs->swaplog_fd = -1;
     cs->numcollisions = 0;
-    cs->membufs = NULL;		/* set when the rebuild completes */
-    cs->current_membuf = cs->membufs;
+    cs->membufs.head = cs->membufs.tail = NULL;		/* set when the rebuild completes */
+    cs->current_membuf = NULL;
     cs->index.head = NULL;
     cs->index.tail = NULL;
 
     parse_cachedir_options(sd, NULL, 0);
+    /* Enforce maxobjsize being set to something */
+    if (sd->max_objsize == -1)
+	fatal("COSS requires max-size to be set to something other than -1!\n");
 }
 
 
@@ -791,6 +794,9 @@ storeCossDirReconfigure(SwapDir * sd, int index, char *path)
 	sd->max_size = size;
     }
     parse_cachedir_options(sd, NULL, 1);
+    /* Enforce maxobjsize being set to something */
+    if (sd->max_objsize == -1)
+	fatal("COSS requires max-size to be set to something other than -1!\n");
 }
 
 void
