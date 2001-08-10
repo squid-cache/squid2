8162cfd43ebb27a4b3f04651e368b9d884f6e285
Adrian Chadd <adrian@creative.net.au>
2001-08-10 03:20:37 +0000

    Fatal if we can't open a coss store file at init time, rather than first IO time.

--- src/fs/coss/store_dir_coss.c
+++ src/fs/coss/store_dir_coss.c
@@ -155,6 +155,10 @@ storeCossDirInit(SwapDir * sd)
     storeCossDirOpenSwapLog(sd);
     storeCossDirRebuild(sd);
     cs->fd = file_open(sd->path, O_RDWR | O_CREAT);
+    if (cs->fd < 0) {
+        debug(81, 1) ("%s: %s\n", sd->path, xstrerror());
+        fatal("storeCossDirInit: Failed to open a COSS directory.");
+    }
     n_coss_dirs++;
     (void) storeDirGetBlkSize(sd->path, &sd->fs.blksize);
 }
