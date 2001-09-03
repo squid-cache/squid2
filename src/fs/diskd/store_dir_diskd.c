ede314b2fca8be03487578c128484da5ff7fb4bb
Duane Wessels <wessels@packet-pushers.com>
2001-09-03 21:28:57 +0000

    SQUID_PREFIX is no longer needed and Makefile.am doesn't define it.

--- src/fs/diskd/store_dir_diskd.c
+++ src/fs/diskd/store_dir_diskd.c
@@ -45,10 +45,6 @@
 #define DefaultLevelTwoDirs     256
 #define STORE_META_BDISKDZ 4096
 
-#ifndef SQUID_PREFIX
-#error "SQUID_PREFIX needs defining!"
-#endif
-
 diskd_stats_t diskd_stats;
 
 typedef struct _RebuildState RebuildState;
@@ -429,7 +425,7 @@ storeDiskdDirInit(SwapDir * sd)
 	&rfd,
 	&diskdinfo->wfd);
     if (x < 0)
-	fatal("execl " SQUID_PREFIX "/bin/diskd failed");
+	fatalf("execl: %s", Config.Program.diskd);
     if (rfd != diskdinfo->wfd)
 	comm_close(rfd);
     fd_note(diskdinfo->wfd, "squid -> diskd");
