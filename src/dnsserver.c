d73bd440f71c99e5a7741602077351d4999b51fc
robertc <robertc>
2001-09-04 01:02:49 +0000

    Fix all DEFAULT_helper variables; change SQUID_VERSION to VERSION in dnsserver.c

--- src/dnsserver.c
+++ src/dnsserver.c
@@ -322,7 +322,7 @@ main(int argc, char *argv[])
 #endif /* HAVE_RES_INIT */
 	    break;
 	case 'v':
-	    printf("dnsserver version %s\n", SQUID_VERSION);
+	    printf("dnsserver version %s\n", VERSION);
 	    exit(0);
 	    break;
 	case 'h':
