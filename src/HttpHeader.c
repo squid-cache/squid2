d78c739fbdd156df4b3f7af745ae31dce39795fa
Henrik Nordstrom <hno@squid-cache.org>
2001-10-17 21:37:28 +0000

    SunPRO cc found a bug.. adrian changed the argument name to htime, but forgot to update the assertion.

--- src/HttpHeader.c
+++ src/HttpHeader.c
@@ -688,7 +688,7 @@ httpHeaderPutTime(HttpHeader * hdr, http_hdr_type id, time_t htime)
 {
     assert_eid(id);
     assert(Headers[id].type == ftDate_1123);	/* must be of an appropriate type */
-    assert(time >= 0);
+    assert(htime >= 0);
     httpHeaderAddEntry(hdr, httpHeaderEntryCreate(id, NULL, mkrfc1123(htime)));
 }
 
