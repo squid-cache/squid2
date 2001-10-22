152eac37fa750da0adf1be13ca3c20187025e4ad
Henrik Nordstrom <hno@squid-cache.org>
2001-10-22 23:55:43 +0000

    Enable GCC to check printf style arguments

--- include/snmp_debug.h
+++ include/snmp_debug.h
@@ -5,6 +5,7 @@
 #ifndef SQUID_SNMP_DEBUG_H
 #define SQUID_SNMP_DEBUG_H
 
-extern void snmplib_debug(int, const char *,...);
+extern void 
+snmplib_debug(int, const char *,...) PRINTF_FORMAT_ARG(2);
 
 #endif /* SQUID_SNMP_DEBUG_H */
