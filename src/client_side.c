a48956ec9335c8b506a407595eb72cf1a7742306
Henrik Nordstrom <hno@squid-cache.org>
2001-11-18 10:20:28 +0000

    Don't strip out anchors from URL's. Doing so violates the HTTP specification on URL transformation and semantical transparency.

--- src/client_side.c
+++ src/client_side.c
@@ -2552,8 +2552,10 @@ parseHttpRequest(ConnStateData * conn, method_t * method_p, int *status,
     dlinkAdd(http, &http->active, &ClientActiveRequests);
 
     debug(33, 5) ("parseHttpRequest: Request Header is\n%s\n", (*prefix_p) + *req_line_sz_p);
+#if THIS_VIOLATES_HTTP_SPECS_ON_URL_TRANSFORMATION
     if ((t = strchr(url, '#')))	/* remove HTML anchors */
 	*t = '\0';
+#endif
 
     /* handle internal objects */
     if (internalCheck(url)) {
