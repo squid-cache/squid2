b4b6983efa026c0c3e25d4fcd8af4b78f73175ed
Henrik Nordstrom <hno@squid-cache.org>
2002-04-16 00:35:39 +0000

    Have memBufAppend \0 terminate the data, allowing it to be used on strings (done quite frequently, the error was unnotices as mempools always zeroed the data, and the likelyhood the buffer is exacly filled is very small)

--- src/MemBuf.c
+++ src/MemBuf.c
@@ -190,11 +190,12 @@ memBufAppend(MemBuf * mb, const char *buf, mb_size_t sz)
     assert(!mb->stolen);	/* not frozen */
 
     if (sz > 0) {
-	if (mb->size + sz > mb->capacity)
-	    memBufGrow(mb, mb->size + sz);
+	if (mb->size + sz + 1 > mb->capacity)
+	    memBufGrow(mb, mb->size + sz + 1);
 	assert(mb->size + sz <= mb->capacity);	/* paranoid */
 	xmemcpy(mb->buf + mb->size, buf, sz);
 	mb->size += sz;
+	mb->buf[mb->size] = '\0';	/* \0 terminate in case we are used as a string. Not counted in the size */
     }
 }
 
