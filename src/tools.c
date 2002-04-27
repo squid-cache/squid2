1cdd7abc246642cec3049f2fa78af5b58af016c2
Henrik Nordstrom <hno@squid-cache.org>
2002-04-27 22:54:49 +0000

    Assert that there is at least one port defined when trying to figure out out port number..

--- src/tools.c
+++ src/tools.c
@@ -996,12 +996,12 @@ parseEtcHosts(void)
 int
 getMyPort(void)
 {
-#if USE_SSL
     if (Config.Sockaddr.http)
 	return ntohs(Config.Sockaddr.http->s.sin_port);
-    else
+#if USE_SSL
+    if (Config.Sockaddr.https)
 	return ntohs(Config.Sockaddr.https->s.sin_port);
-#else
-    return ntohs(Config.Sockaddr.http->s.sin_port);
 #endif
+    fatal("No port defined");
+    return 0; /* NOT REACHED */
 }
