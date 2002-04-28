2447e396d46d5acdb4002e1d41764b3a638e19e0
Henrik Nordstrom <hno@squid-cache.org>
2002-04-28 14:57:51 +0000

    MFC: Use FD_READ_METHOD rather than calling read() directly

--- src/helper.c
+++ src/helper.c
@@ -754,7 +754,7 @@ helperStatefulHandleRead(int fd, void *data)
     assert(fd == srv->rfd);
     assert(cbdataValid(data));
     statCounter.syscalls.sock.reads++;
-    len = read(fd, srv->buf + srv->offset, srv->buf_sz - srv->offset);
+    len = FD_READ_METHOD(fd, srv->buf + srv->offset, srv->buf_sz - srv->offset);
     fd_bytes(fd, len, FD_READ);
     debug(29, 5) ("helperStatefulHandleRead: %d bytes from %s #%d.\n",
 	len, hlp->id_name, srv->index + 1);
