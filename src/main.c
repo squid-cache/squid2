baf5c8c8bfa47cf543d4325e4a8b3c122890b34a
Henrik Nordstrom <hno@squid-cache.org>
2002-07-14 17:19:06 +0000

    From Guido:
    This patch avoids confusion when using -s Squid switch on platforms without
    syslog.

--- src/main.c
+++ src/main.c
@@ -205,8 +205,13 @@ mainParseOptions(int argc, char *argv[])
 #endif
 	    }
 	case 's':
+#if HAVE_SYSLOG
 	    opt_syslog_enable = 1;
 	    break;
+#else
+	    fatal("Logging to syslog not available on this platform");
+	    /* NOTREACHED */
+#endif
 	case 'u':
 	    icpPortNumOverride = atoi(optarg);
 	    if (icpPortNumOverride < 0)
