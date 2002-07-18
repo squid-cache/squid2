79a8f8c12443f9ded450bf04f13adb949a72c906
Henrik Nordstrom <hno@squid-cache.org>
2002-07-18 23:55:43 +0000

    Minor bugfix of the previous HTML cleanups

--- src/cachemgr.c
+++ src/cachemgr.c
@@ -454,10 +454,10 @@ read_reply(int s, cachemgr_request * req)
 	    break;
 	case isBodyStart:
 	    printf("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\" \"http://www.w3.org/TR/html4/loose.dtd\">\n");
-	    printf("<HTML><HEAD><TITLE>CacheMgr@%s: %s</TITLE>\n");
-	    printf("<STYLE type= \"text/css\"><!--BODY{background-color:#ffffff; font-family:verdana,sans-serif}--></STYLE>\n");
-	    printf("</HEAD><BODY>\n",
+	    printf("<HTML><HEAD><TITLE>CacheMgr@%s: %s</TITLE>\n",
 		req->hostname, action);
+	    printf("<STYLE type= \"text/css\"><!--BODY{background-color:#ffffff; font-family:verdana,sans-serif}--></STYLE>\n");
+	    printf("</HEAD><BODY>\n");
 	    if (parse_menu) {
 		printf("<H2><a href=\"%s\">Cache Manager</a> menu for %s:</H2>",
 		    menu_url(req, "authenticate"), req->hostname);
