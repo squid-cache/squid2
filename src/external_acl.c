63a16347a1ade7f67071fcc78e88234d85df8dab
Henrik Nordstrom <hno@squid-cache.org>
2002-07-18 22:02:26 +0000

    Added back a missing else.. got list in the rewrite to support authentiction.
    This fixes the parsing of headers.

--- src/external_acl.c
+++ src/external_acl.c
@@ -234,7 +234,7 @@ parse_externalAclHelper(external_acl ** list)
 		    format->type = EXT_ACL_HEADER_ID;
 	    }
 	}
-	if (strcmp(token, "%LOGIN") == 0) {
+	else if (strcmp(token, "%LOGIN") == 0) {
 	    format->type = EXT_ACL_LOGIN;
 	    a->require_auth = 1;
 	}
