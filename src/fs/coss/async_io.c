fd0ef7e9428c03e7e5bb2af1e4974d531608547e
Henrik Nordstrom <hno@squid-cache.org>
2002-07-21 00:27:31 +0000

    Oops..

--- src/fs/coss/async_io.c
+++ src/fs/coss/async_io.c
@@ -91,7 +91,7 @@ a_file_read(async_queue_t * q, int fd, void *buf, int req_len, off_t offset,
 
     /* Initiate aio */
     if (aio_read(&qe->aq_e_aiocb) < 0) {
-	fatalf(79, 1) ("Aiee! aio_read() returned error (%d)!\n", errno);
+	fatalf("Aiee! aio_read() returned error (%d)!\n", errno);
     }
 }
 
