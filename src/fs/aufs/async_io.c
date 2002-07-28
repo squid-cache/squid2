46d20f70e4a8a4993c4b74f9980da4e2961158e5
robertc <robertc>
2002-07-28 11:31:06 +0000

    Guido Serassio: add a description line to CacheMgr Async I/O counters table.

--- src/fs/aufs/async_io.c
+++ src/fs/aufs/async_io.c
@@ -316,6 +316,7 @@ void
 aioStats(StoreEntry * sentry)
 {
     storeAppendPrintf(sentry, "ASYNC IO Counters:\n");
+    storeAppendPrintf(sentry, "Operation\t# Requests\n");
     storeAppendPrintf(sentry, "open\t%d\n", squidaio_counts.open);
     storeAppendPrintf(sentry, "close\t%d\n", squidaio_counts.close);
     storeAppendPrintf(sentry, "cancel\t%d\n", squidaio_counts.cancel);
