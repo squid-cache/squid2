449c1d6832d325eed7029559f1df3e12b58ea168
Duane Wessels <wessels@packet-pushers.com>
2002-07-30 04:23:10 +0000

    getSingleParent() became orphaned somewhere along the way.  It is never called.

--- src/neighbors.c
+++ src/neighbors.c
@@ -219,6 +219,7 @@ neighborsCount(request_t * request)
     return count;
 }
 
+#if UNUSED_CODE
 peer *
 getSingleParent(request_t * request)
 {
@@ -238,6 +239,7 @@ getSingleParent(request_t * request)
     debug(15, 3) ("getSingleParent: returning %s\n", p ? p->host : "NULL");
     return p;
 }
+#endif
 
 peer *
 getFirstUpParent(request_t * request)
