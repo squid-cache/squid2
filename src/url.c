e6897e85f85f7ec3496efa3f5a325e67667eeac4
Henrik Nordstrom <hno@squid-cache.org>
2002-08-19 22:48:25 +0000

    MFC Bugzilla 403: url_convert_hex buffer overrun

--- src/url.c
+++ src/url.c
@@ -130,8 +130,8 @@ url_convert_hex(char *org_url, int allocate)
     url = allocate ? (char *) xstrdup(org_url) : org_url;
     if ((int) strlen(url) < 3 || !strchr(url, '%'))
 	return url;
-    for (s = t = url; *(s + 2); s++) {
-	if (*s == '%') {
+    for (s = t = url; *s; s++) {
+	if (*s == '%' && *(s + 1) && *(s + 2)) {
 	    code[0] = *(++s);
 	    code[1] = *(++s);
 	    *t++ = (char) strtol(code, NULL, 16);
