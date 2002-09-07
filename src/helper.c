220e36b24ebcd289e72fb2db490747e3a3dd0e30
Henrik Nordstrom <hno@squid-cache.org>
2002-09-07 23:04:50 +0000

    And another typo in the same 2 line patch..

--- src/helper.c
+++ src/helper.c
@@ -416,7 +416,7 @@ helperStats(StoreEntry * sentry, helper * hlp)
     storeAppendPrintf(sentry, "%7s\t%7s\t%7s\t%11s\t%s\t%7s\t%7s\t%7s\n",
 	"#",
 	"FD",
-	"PID"
+	"PID",
 	"# Requests",
 	"Flags",
 	"Time",
