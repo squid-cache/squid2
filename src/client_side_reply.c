53f2b84946d0d2784d5ae93caeca08990e46fd97
robertc <robertc>
2002-09-29 09:52:36 +0000

    Fix cache digests. Some over enthusiastic search and replacing a while back

--- src/client_side_reply.c
+++ src/client_side_reply.c
@@ -1166,7 +1166,7 @@ clientBuildReplyHeader(clientHttpRequest * http, HttpReply * rep)
 #if USE_CACHE_DIGESTS
     /* Append X-Cache-Lookup: -- temporary hack, to be removed @?@ @?@ */
     httpHeaderPutStrf(hdr, HDR_X_CACHE_LOOKUP, "%s from %s:%d",
-	context->lookup_type ? context->lookup_type : "NONE",
+	http->lookup_type ? http->lookup_type : "NONE",
 	getMyHostname(), getMyPort());
 #endif
     if (httpReplyBodySize(request->method, rep) < 0) {
@@ -1241,7 +1241,7 @@ clientIdentifyStoreObject(clientHttpRequest * http)
 	ipcacheInvalidate(r->host);
 #endif
 #if USE_CACHE_DIGESTS
-    context->lookup_type = e ? "HIT" : "MISS";
+    http->lookup_type = e ? "HIT" : "MISS";
 #endif
     if (NULL == e) {
 	/* this object isn't in the cache */
