ef700f832a163197e83a47750f7873275046684c
Henrik Nordstrom <hno@squid-cache.org>
2002-10-03 15:36:01 +0000

    Removed some debugging

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,4 @@
-#! /bin/sh -x
+#!/bin/sh
 # Used to setup the configure.in, autoheader and Makefile.in's if configure
 # has not been generated. This script is only needed for developers when
 # configure has not been run, or if a Makefile.am in a non-configured directory
