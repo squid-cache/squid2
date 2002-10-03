d650d844d3bfeeb827f96dad8d8ef50c86ba50c7
Henrik Nordstrom <hno@squid-cache.org>
2002-10-03 15:34:59 +0000

    More bootstrap adjustments to deal with parallell autotool installations

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,4 @@
-#! /bin/sh
+#! /bin/sh -x
 # Used to setup the configure.in, autoheader and Makefile.in's if configure
 # has not been generated. This script is only needed for developers when
 # configure has not been run, or if a Makefile.am in a non-configured directory
@@ -19,14 +19,20 @@ bootstrap() {
 # Make sure cfgaux exists
 mkdir -p cfgaux
 
-# Bootstrap the autotool subsystems
-bootstrap aclocal
-bootstrap autoheader
-bootstrap automake --foreign --add-missing
+# Adjust paths of required autool packages
+acver="213"
 if autoconf --version | grep -q 2.13; then
-  bootstrap autoconf
-else
-  bootstrap autoconf213
+  acver=""
 fi
+amver="15"
+if automake --version | grep -q 1.5; then
+  amver=""
+fi
+
+# Bootstrap the autotool subsystems
+bootstrap aclocal$amver
+bootstrap autoheader$acver
+bootstrap automake$amver --foreign --add-missing
+bootstrap autoconf$acver
 
 echo "Autotool bootstrapping complete."
