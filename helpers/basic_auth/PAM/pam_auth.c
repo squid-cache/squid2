aae34f321f984f7f0da63def518ad20f4bf7c848
Duane Wessels <wessels@packet-pushers.com>
2002-10-11 03:01:23 +0000

    print a warning on stderr if pam_auth is not started as root.

--- helpers/basic_auth/PAM/pam_auth.c
+++ helpers/basic_auth/PAM/pam_auth.c
@@ -179,6 +179,9 @@ start:
 	exit(1);
     }
 
+    if (0 != getuid())
+	fprintf(stderr, "WARNING: %s must be started as root\n", argv[0]);
+
     while (fgets(buf, BUFSIZE, stdin)) {
 	user = buf;
 	password = strchr(buf, '\n');
