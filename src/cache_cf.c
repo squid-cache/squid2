e0d63f7914fb23a5beed73fe95d65fb4a93946e4
Henrik Nordstrom <hno@squid-cache.org>
2002-10-17 02:13:47 +0000

    MFC: Fix a misunderstanding in how delay pools are allocated. Thanks to David Luyer for noticing the error and explaining what the code is meant to do.

--- src/cache_cf.c
+++ src/cache_cf.c
@@ -898,7 +898,8 @@ parse_delay_pool_class(delayConfig * cfg)
 	delayFreeDelayPool(pool);
 	safe_free(cfg->rates[pool]);
     }
-    cfg->rates[pool] = xmalloc(class * sizeof(delaySpecSet));
+    /* Allocates a "delaySpecSet" just as large as needed for the class */
+    cfg->rates[pool] = xmalloc(class * sizeof(delaySpec));
     cfg->class[pool] = class;
     cfg->rates[pool]->aggregate.restore_bps = cfg->rates[pool]->aggregate.max_bytes = -1;
     if (cfg->class[pool] >= 3)
