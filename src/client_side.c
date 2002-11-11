3c55119be7900b8854a93ea88f34c7486585453b
Henrik Nordstrom <hno@squid-cache.org>
2002-11-11 21:00:23 +0000

    Oops.. do not filter out unproxyable authentication methods from our own responses.

--- src/client_side.c
+++ src/client_side.c
@@ -1358,7 +1358,8 @@ clientBuildReplyHeader(clientHttpRequest * http, HttpReply * rep)
 		squid_curtime - http->entry->timestamp);
     }
     /* Filter unproxyable authentication types */
-    if (httpHeaderHas(hdr, HDR_WWW_AUTHENTICATE) || httpHeaderHas(hdr, HDR_PROXY_AUTHENTICATE)) {
+    if (http->log_type != LOG_TCP_DENIED &&
+	(httpHeaderHas(hdr, HDR_WWW_AUTHENTICATE) || httpHeaderHas(hdr, HDR_PROXY_AUTHENTICATE))) {
 	HttpHeaderPos pos = HttpHeaderInitPos;
 	HttpHeaderEntry *e;
 	while ((e = httpHeaderGetEntry(hdr, &pos))) {
