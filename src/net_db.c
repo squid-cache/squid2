0c81e8434b0c138b4132fa84153e3979c2cac3a3
Duane Wessels <wessels@packet-pushers.com>
2003-01-02 23:21:57 +0000

    MFC: update last_use_time in netdbHostData()

--- src/net_db.c
+++ src/net_db.c
@@ -833,6 +833,7 @@ netdbHostData(const char *host, int *samp, int *rtt, int *hops)
     *samp = n->pings_recv;
     *rtt = (int) (n->rtt + 0.5);
     *hops = (int) (n->hops + 0.5);
+    n->last_use_time = squid_curtime;
 #endif
 }
 
