ede04fd35b96d7c664cae2120a86f523b0690129
Henrik Nordstrom <hno@squid-cache.org>
2003-01-09 05:10:43 +0000

    Bugzilla 493: chroot_dir messes up all path checks.. this patch makes life easier by assuming all paths are within the chroot directory..

--- src/cache_cf.c
+++ src/cache_cf.c
@@ -2466,9 +2466,14 @@ void
 requirePathnameExists(const char *name, const char *path)
 {
     struct stat sb;
+    char pathbuf[BUFSIZ];
     assert(path != NULL);
+    if (Config.chroot_dir) {
+	snprintf(pathbuf, BUFSIZ, "%s/%s", Config.chroot_dir, path);
+	path = pathbuf;
+    }
     if (stat(path, &sb) < 0)
-	fatalf("%s: %s", path, xstrerror());
+	fatalf("%s %s: %s", name, path, xstrerror());
 }
 
 char *
