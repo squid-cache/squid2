8ce9f89bd61e8881c8e440d6c37d483d0d3fed62
Duane Wessels <wessels@packet-pushers.com>
2003-01-17 05:21:49 +0000

    failure_ratio is a ratio, not a percentage.  Removed %% from printf.

--- src/stat.c
+++ src/stat.c
@@ -456,7 +456,7 @@ info_get(StoreEntry * sentry)
 	statCounter.icp.pkts_sent);
     storeAppendPrintf(sentry, "\tNumber of queued ICP replies:\t%u\n",
 	statCounter.icp.replies_queued);
-    storeAppendPrintf(sentry, "\tRequest failure ratio:\t%5.2f%%\n",
+    storeAppendPrintf(sentry, "\tRequest failure ratio:\t%5.2f\n",
 	request_failure_ratio);
 
     storeAppendPrintf(sentry, "\tAverage HTTP requests per minute since start:\t%.1f\n",
