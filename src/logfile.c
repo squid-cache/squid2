531186f54616a883f99f4644ae5541f06793b8b7
Henrik Nordstrom <hno@squid-cache.org>
2003-01-20 18:58:48 +0000

    Bugzilla 506: log_mime_hdrs output garbage on overly long request headers

--- src/logfile.c
+++ src/logfile.c
@@ -159,6 +159,11 @@ logfilePrintf(va_alist)
     fmt = va_arg(args, char *);
 #endif
     s = vsnprintf(buf, 8192, fmt, args);
+    if (s > 8192) {
+	s = 8192;
+	if (fmt[strlen(fmt) - 1] == '\n')
+	    buf[8191] = '\n';
+    }
     logfileWrite(lf, buf, (size_t) s);
     va_end(args);
 }
