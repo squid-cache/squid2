605b7dfa119d67ca536f421f77d682adb29a7fd4
Duane Wessels <wessels@packet-pushers.com>
2003-01-27 05:00:46 +0000

    MFC: add 'select_fds_hist' object to cachemgr 'histogram' output

--- src/stat.c
+++ src/stat.c
@@ -1061,6 +1061,8 @@ statCountersHistograms(StoreEntry * sentry)
     statHistDump(&f->icp.reply_svc_time, sentry, NULL);
     storeAppendPrintf(sentry, "dns.svc_time histogram:\n");
     statHistDump(&f->dns.svc_time, sentry, NULL);
+    storeAppendPrintf(sentry, "select_fds_hist histogram:\n");
+    statHistDump(&f->select_fds_hist, sentry, NULL);
 }
 
 static void
