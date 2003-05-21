0b3808d3e546fec111ab4b47fbba7daab82a4c2f
Henrik Nordstrom <hno@squid-cache.org>
2003-05-21 00:02:57 +0000

    Update ChangeLog when making snapshots

--- mksnapshot.sh
+++ mksnapshot.sh
@@ -49,6 +49,8 @@ if [ -f $relnotes ]; then
 	cp -p $relnotes ${PACKAGE}-${VERSION}-${date}-RELEASENOTES.html
 	echo ${PACKAGE}-${VERSION}-${date}-RELEASENOTES.html >>${tag}.out
 fi
+cp -p ChangeLog
+echo ChangeLog >>${tag}.out
 
 if (echo $VERSION | grep PRE) || (echo $VERSION | grep STABLE); then
   echo "Differences from ${PACKAGE}-${VERSION} to ${PACKAGE}-${VERSION}-${date}" >${PACKAGE}-${VERSION}-${date}.diff
