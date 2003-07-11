122444f23989729ae7e1856e596a18c4eb28edef
Henrik Nordstrom <hno@squid-cache.org>
2003-07-11 22:40:34 +0000

    Bug #699: Forward Host headers in place
    Some firewalls or servers get confused if the Host header is too far
    into the headers. To make sure Squid is not blaimed for this forward
    the Host header exacly where it was in the request.

--- src/http.c
+++ src/http.c
@@ -795,15 +795,22 @@ httpBuildRequestHeader(request_t * request,
 	    break;
 	case HDR_HOST:
 	    /*
-	     * Normally Squid does not copy the Host: header from
-	     * a client request into the forwarded request headers.
-	     * However, there is one case when we do: If the URL
+	     * Normally Squid rewrites the Host: header.
+	     * However, there is one case when we don't: If the URL
 	     * went through our redirector and the admin configured
 	     * 'redir_rewrites_host' to be off.
 	     */
-	    if (request->flags.redirected)
-		if (!Config.onoff.redir_rewrites_host)
-		    httpHeaderAddEntry(hdr_out, httpHeaderEntryClone(e));
+	    if (request->flags.redirected && !Config.onoff.redir_rewrites_host)
+		httpHeaderAddEntry(hdr_out, httpHeaderEntryClone(e));
+	    else {
+		/* use port# only if not default */
+		if (orig_request->port == urlDefaultPort(orig_request->protocol)) {
+		    httpHeaderPutStr(hdr_out, HDR_HOST, orig_request->host);
+		} else {
+		    httpHeaderPutStrf(hdr_out, HDR_HOST, "%s:%d",
+			orig_request->host, (int) orig_request->port);
+		}
+	    }
 	    break;
 	case HDR_IF_MODIFIED_SINCE:
 	    /* append unless we added our own;
