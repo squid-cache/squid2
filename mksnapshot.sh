9e7cb1ad782f7eb1b9cd47a055155a9cbb9cfa69
Henrik Nordstrom <hno@squid-cache.org>
2003-11-07 16:46:49 +0000

    Synch with HEAD
    Corrected ChangeLog handling

--- mksnapshot.sh
+++ mksnapshot.sh
@@ -3,12 +3,12 @@ if [ $# -gt 1 ]; then
 	echo "Usage: $0 [branch]"
 	exit 1
 fi
-package=squid
+module=squid
 tag=${1:-HEAD}
 startdir=$PWD
 date=`env TZ=GMT date +%Y%m%d`
 
-tmpdir=${TMPDIR:-${PWD}}/${package}-${tag}-mksnapshot
+tmpdir=${TMPDIR:-${PWD}}/${module}-${tag}-mksnapshot
 
 CVSROOT=${CVSROOT:-/server/cvs-server/squid}
 export CVSROOT
@@ -17,21 +17,20 @@ rm -rf $tmpdir
 trap "rm -rf $tmpdir" 0
 
 rm -f ${tag}.out
-cvs -Q export -d $tmpdir -r $tag $package
+cvs -Q export -d $tmpdir -r $tag $module
 if [ ! -f $tmpdir/configure ]; then
-	echo "ERROR! Tag $tag not found in $package"
+	echo "ERROR! Tag $tag not found in $module"
 fi
 
 cd $tmpdir
-eval CVS`grep ^VERSION= configure`
-VERSION=`echo $CVSVERSION | sed -e 's/-CVS//'`
-eval `grep ^PACKAGE= configure`
+eval `grep "^ *VERSION=" configure | sed -e 's/-CVS//'`
+eval `grep "^ *PACKAGE=" configure`
 ed -s configure.in <<EOS
-g/${CVSVERSION}/ s//${VERSION}-${date}/
+g/VERSION=${VERSION}-CVS/ s//VERSION=${VERSION}-${date}/
 w
 EOS
 ed -s configure <<EOS
-g/${CVSVERSION}/ s//${VERSION}-${date}/
+g/VERSION=${VERSION}-CVS/ s//VERSION=${VERSION}-${date}/
 w
 EOS
 
@@ -49,11 +48,11 @@ if [ -f $relnotes ]; then
 	cp -p $relnotes ${PACKAGE}-${VERSION}-${date}-RELEASENOTES.html
 	echo ${PACKAGE}-${VERSION}-${date}-RELEASENOTES.html >>${tag}.out
 fi
-cp -p ChangeLog
-echo ChangeLog >>${tag}.out
+cp -p $tmpdir/ChangeLog ${PACKAGE}-${VERSION}-${date}-ChangeLog.txt
+echo ${PACKAGE}-${VERSION}-${date}-ChangeLog.txt >>${tag}.out
 
 if (echo $VERSION | grep PRE) || (echo $VERSION | grep STABLE); then
   echo "Differences from ${PACKAGE}-${VERSION} to ${PACKAGE}-${VERSION}-${date}" >${PACKAGE}-${VERSION}-${date}.diff
-  cvs -q rdiff -u -r SQUID_`echo $VERSION | tr .- __` -r $tag $package >>${PACKAGE}-${VERSION}-${date}.diff
+  cvs -q rdiff -u -r SQUID_`echo $VERSION | tr .- __` -r $tag $module >>${PACKAGE}-${VERSION}-${date}.diff
   echo ${PACKAGE}-${VERSION}-${date}.diff >>${tag}.out
 fi
