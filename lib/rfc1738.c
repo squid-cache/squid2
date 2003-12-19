5eabe4be19393daa97f03b1b26661922510a9e9a
Duane Wessels <wessels@packet-pushers.com>
2003-12-19 06:11:20 +0000

    bugfix: handle %00 as a special case

--- lib/rfc1738.c
+++ lib/rfc1738.c
@@ -195,6 +195,10 @@ rfc1738_unescape(char *s)
 	    continue;
 	}
 	if (s[j + 1] && s[j + 2]) {
+	    if (s[j + 1] == '0' && s[j + 2] == '0') {	/* %00 case */
+		j += 2;
+		continue;
+	    }
 	    hexnum[0] = s[j + 1];
 	    hexnum[1] = s[j + 2];
 	    hexnum[2] = '\0';
