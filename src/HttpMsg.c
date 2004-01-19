82bfa57a4417d6096285fd0d1c8d78daf613834c
Henrik Nordstrom <hno@squid-cache.org>
2004-01-19 17:46:40 +0000

    Spelling correction

--- src/HttpMsg.c
+++ src/HttpMsg.c
@@ -91,7 +91,7 @@ httpMsgIsolateHeaders(const char **parse_start, const char **blk_start, const ch
 int
 httpMsgIsPersistent(http_version_t http_ver, const HttpHeader * hdr)
 {
-#if WHEN_SQUID_IS_NOT_HTTP1_1
+#if WHEN_SQUID_IS_HTTP1_1
     if ((http_ver.major >= 1) && (http_ver.minor >= 1)) {
 	/*
 	 * for modern versions of HTTP: persistent unless there is
