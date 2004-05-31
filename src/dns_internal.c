725a4257a11f023bc02208e5244fe6310a228efa
Henrik Nordstrom <hno@squid-cache.org>
2004-05-31 23:33:52 +0000

    Bug #991: dns should fallback to localhost if no resolv.conf

--- src/dns_internal.c
+++ src/dns_internal.c
@@ -656,14 +656,16 @@ idnsInit(void)
     if (0 == nns)
 	idnsParseWIN32Registry();
 #endif
-    if (0 == nns)
-	fatal("Could not find any nameservers.\n"
+    if (0 == nns) {
+	debug(78, 1) ("Warning: Could not find any nameservers. Trying to use localhost\n");
 #if defined(_SQUID_MSWIN_) || defined(_SQUID_CYGWIN_)
-	    "       Please check your TCP-IP settings or /etc/resolv.conf file\n"
+	debug(78, 1) ("Please check your TCP-IP settings or /etc/resolv.conf file\n");
 #else
-	    "       Please check your /etc/resolv.conf file\n"
+	debug(78, 1) ("Please check your /etc/resolv.conf file\n");
 #endif
-	    "       or use the 'dns_nameservers' option in squid.conf.");
+	debug(78, 1) ("or use the 'dns_nameservers' option in squid.conf.");
+	idnsAddNameserver("127.0.0.1");
+    }
     if (!init) {
 	memDataInit(MEM_IDNS_QUERY, "idns_query", sizeof(idns_query), 0);
 	cachemgrRegister("idns",
