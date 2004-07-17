3cb55a96223e6266116bf9553a095766225774c3
Henrik Nordstrom <hno@squid-cache.org>
2004-07-17 20:09:23 +0000

    Bug #882: Add delay pools information to active_requests
    This patch adds information about the active delay pool in cachemgr
    active_requests entry.

--- src/stat.c
+++ src/stat.c
@@ -1446,6 +1446,9 @@ statClientRequests(StoreEntry * s)
 	    (long int) http->start.tv_sec,
 	    (int) http->start.tv_usec,
 	    tvSubDsec(http->start, current_time));
+#if DELAY_POOLS
+	storeAppendPrintf(s, "delay_pool %d\n", delayClient(http) >> 16);
+#endif
 	storeAppendPrintf(s, "\n");
     }
 }
