ee986d47cf839c73ee937b557decaf48151f74b9
Henrik Nordstrom <hno@squid-cache.org>
2004-09-30 09:23:30 +0000

    Bug #1033: CARP does ignore cache_peer_domain

--- src/carp.c
+++ src/carp.c
@@ -119,7 +119,7 @@ carpSelectParent(request_t * request)
     for (tp = Config.peers; tp; tp = tp->next) {
 	if (0.0 == tp->carp.load_factor)
 	    continue;
-	if (tp->tcp_up != PEER_TCP_MAGIC_COUNT)
+	if (!peerHTTPOkay(tp, request))
 	    continue;
 	assert(tp->type == PEER_PARENT);
 	combined_hash = (url_hash ^ tp->carp.hash);
