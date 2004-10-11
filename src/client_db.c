c6dd3f18b4b48c0dc81979e73fb921bb8e8ae532
Henrik Nordstrom <hno@squid-cache.org>
2004-10-11 20:25:52 +0000

    Fix client db scheduling logics

--- src/client_db.c
+++ src/client_db.c
@@ -57,7 +57,7 @@ clientdbAdd(struct in_addr addr)
     c->addr = addr;
     hash_join(client_table, &c->hash);
     statCounter.client_http.clients++;
-    if ((statCounter.client_http.clients > max_clients) && !cleanup_running && !cleanup_scheduled < 2) {
+    if ((statCounter.client_http.clients > max_clients) && !cleanup_running && cleanup_scheduled < 2) {
 	cleanup_scheduled++;
 	eventAdd("client_db garbage collector", clientdbScheduledGC, NULL, 90, 0);
     }
