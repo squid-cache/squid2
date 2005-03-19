88c35de5a8597cfacf5d3301781f1004ec6172c0
Henrik Nordstrom <hno@squid-cache.org>
2005-03-19 00:53:34 +0000

    Bug #1258: LDAP helpers fails to compile with SUN LDAP SDK

--- helpers/basic_auth/LDAP/squid_ldap_auth.c
+++ helpers/basic_auth/LDAP/squid_ldap_auth.c
@@ -252,15 +252,24 @@ open_ldap_connection(const char *ldapServer, int port)
     if (version == -1) {
 	version = LDAP_VERSION2;
     }
-    if (ldap_set_option(ld, LDAP_OPT_PROTOCOL_VERSION, &version)
-	!= LDAP_OPT_SUCCESS) {
+    if (ldap_set_option(ld, LDAP_OPT_PROTOCOL_VERSION, &version) != LDAP_SUCCESS) {
 	fprintf(stderr, "Could not set LDAP_OPT_PROTOCOL_VERSION %d\n",
 	    version);
 	exit(1);
     }
-    if (use_tls && (version == LDAP_VERSION3) && (ldap_start_tls_s(ld, NULL, NULL) != LDAP_SUCCESS)) {
-	fprintf(stderr, "Could not Activate TLS connection\n");
+    if (use_tls) {
+#ifdef LDAP_OPT_X_TLS
+        if (version == LDAP_VERSION3 && ldap_start_tls_s(ld, NULL, NULL) != LDAP_SUCCESS) {
+	    fprintf(stderr, "Could not Activate TLS connection\n");
+	    exit(1);
+	} else {
+	    fprintf(stderr, "TLS requires LDAP version 3\n");
+	    exit(1);
+	}
+#else
+	fprintf(stderr, "TLS not supported with your LDAP library\n");
 	exit(1);
+#endif
     }
 #endif
     squid_ldap_set_timelimit(ld, timelimit);
