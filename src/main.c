b30f93304de5ff97bb880467ff074ef2544735bc
Henrik Nordstrom <hno@squid-cache.org>
2005-06-27 21:24:28 +0000

    To match the daemon process privileges we should only leave_suid() not no_suid() before sending -k signal if not running chroot:ed.

--- src/main.c
+++ src/main.c
@@ -683,10 +683,13 @@ main(int argc, char **argv)
     /* send signal to running copy and exit */
     if (opt_send_signal != -1) {
 	/* chroot if configured to run inside chroot */
-	if (Config.chroot_dir && chroot(Config.chroot_dir)) {
-	    fatal("failed to chroot");
+	if (Config.chroot_dir) {
+	    if (chroot(Config.chroot_dir))
+		fatal("failed to chroot");
+	    no_suid();
+	} else {
+	    leave_suid();
 	}
-	no_suid();
 	sendSignal();
 	/* NOTREACHED */
     }
