91eb03ef2d571faa28414dd1c3b86cc76af5b32c
Henrik Nordstrom <hno@squid-cache.org>
2006-05-12 15:57:23 +0000

    Bug #1532: Mime icons are not displayed when viewing ftp sites when visible_hostname is a short hostname (without domain).

--- src/internal.c
+++ src/internal.c
@@ -135,6 +135,9 @@ internalHostname(void)
 {
     LOCAL_ARRAY(char, host, SQUIDHOSTNAMELEN + 1);
     xstrncpy(host, getMyHostname(), SQUIDHOSTNAMELEN);
+    if (Config.appendDomain && !strchr(host, '.'))
+	strncat(host, Config.appendDomain, SQUIDHOSTNAMELEN -
+	    strlen(host) - 1);
     Tolower(host);
     return host;
 }
