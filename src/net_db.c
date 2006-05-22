f4e416cb4d2701f9b58cbfd6382835f490f9b391
serassio <serassio>
2006-05-22 21:17:09 +0000

    Changed netdb file format to binary like in 3.0

--- src/net_db.c
+++ src/net_db.c
@@ -435,7 +435,7 @@ netdbReloadState(void)
      * Solaris bugs, its a bad idea.  fopen can fail if more than
      * 256 FDs are open.
      */
-    fd = file_open(path, O_RDONLY | O_TEXT);
+    fd = file_open(path, O_RDONLY | O_BINARY);
     if (fd < 0)
 	return;
     if (fstat(fd, &sb) < 0) {
