b5fefff7ebf6e8c282afcb53b40c2b94670ff2ef
Henrik Nordstrom <hno@squid-cache.org>
2006-05-30 13:05:57 +0000

    Remove USE_EPOLL code which got redundant by commSetEvents()

--- src/fd.c
+++ src/fd.c
@@ -84,11 +84,6 @@ fd_close(int fd)
 	assert(F->write_handler == NULL);
     }
     debug(51, 3) ("fd_close FD %d %s\n", fd, F->desc);
-#if USE_EPOLL
-    /* the epoll code needs to update the descriptor before flags.ope is 0 */
-    commSetSelect(fd, COMM_SELECT_READ, NULL, NULL, 0);
-    commSetSelect(fd, COMM_SELECT_WRITE, NULL, NULL, 0);
-#endif
     commSetEvents(fd, 0, 0, 0);
     F->flags.open = 0;
     fdUpdateBiggest(fd, 0);
