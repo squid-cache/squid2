ae7b81d680688dbbeacd5f02852c6f2850053f39
serassio <serassio>
2006-06-04 08:24:33 +0000

    Added PRIx64 support for native Windows and HP Tru64

--- src/squid.h
+++ src/squid.h
@@ -378,12 +378,29 @@ struct rusage {
 #define S_ISDIR(mode) (((mode) & (_S_IFMT)) == (_S_IFDIR))
 #endif
 
+/* 
+ * ISO C99 Standard printf() macros for 64 bit integers
+ * On some 64 bit platform, HP Tru64 is one, for printf must be used
+ * "%lx" instead of "%llx" 
+ */
 #ifndef PRId64
-#define PRId64	"lld"
+#ifdef _SQUID_MSWIN_		/* Windows native port using MSVCRT */
+#define PRId64 "I64d"
+#elif SIZEOF_INT64_T > SIZEOF_LONG
+#define PRId64 "lld"
+#else
+#define PRId64 "ld"
+#endif
 #endif
 
 #ifndef PRIu64
-#define PRIu64	"llu"
+#ifdef _SQUID_MSWIN_		/* Windows native port using MSVCRT */
+#define PRIu64 "I64u"
+#elif SIZEOF_INT64_T > SIZEOF_LONG
+#define PRIu64 "llu"
+#else
+#define PRIu64 "lu"
+#endif
 #endif
 
 #ifdef USE_GNUREGEX
