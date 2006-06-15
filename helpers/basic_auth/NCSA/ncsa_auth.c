695a7e8e831dd8a6948b5dbef880d58431939200
Henrik Nordstrom <hno@squid-cache.org>
2006-06-15 19:30:37 +0000

    MD5 hash support for NCSA auth, supporting both shadow and Apache htpasswd MD5 hases (same algorithms, different permutation strings)
    Merge from Squid-3.

--- helpers/basic_auth/NCSA/ncsa_auth.c
+++ helpers/basic_auth/NCSA/ncsa_auth.c
@@ -42,6 +42,7 @@
 
 #include "util.h"
 #include "hash.h"
+#include "crypt_md5.h"
 
 static hash_table *hash = NULL;
 static HASHFREE my_free;
@@ -126,22 +127,25 @@ main(int argc, char **argv)
 		change_time = sb.st_mtime;
 	    }
 	}
-	user = buf;
-	passwd = strchr(buf, ' ');
-	if (!passwd) {
+	if ((user = strtok(buf, " ")) == NULL) {
 	    printf("ERR\n");
 	    continue;
 	}
-	*passwd++ = '\0';
+	if ((passwd = strtok(NULL, "")) == NULL) {
+	    printf("ERR\n");
+	    continue;
+	}
 	rfc1738_unescape(user);
 	rfc1738_unescape(passwd);
-	u = hash_lookup(hash, user);
+	u = (user_data *)hash_lookup(hash, user);
 	if (u == NULL) {
 	    printf("ERR No such user\n");
-	} else if (strcmp(u->passwd, (char *) crypt(passwd, u->passwd))) {
-	    printf("ERR Wrong password\n");
-	} else {
+	} else if (strcmp(u->passwd, (char *) crypt(passwd, u->passwd)) == 0) {
 	    printf("OK\n");
+	} else if (strcmp(u->passwd, (char *) crypt_md5(passwd, u->passwd)) == 0) {
+	    printf("OK\n");
+	} else {
+	    printf("ERR Wrong password\n");
 	}
     }
     exit(0);
