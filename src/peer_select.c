0334d57bf3ca1425b8ba1271435e13f343061d17
Henrik Nordstrom <hno@squid-cache.org>
2006-06-22 23:26:46 +0000

    Fix a connection pinning releated segfault on CONNECT requests (entry == NULL)

--- src/peer_select.c
+++ src/peer_select.c
@@ -276,7 +276,7 @@ peerSelectFoo(ps_state * ps)
 	debug(44, 3) ("peerSelectFoo: direct = %s\n",
 	    DirectStr[ps->direct]);
     }
-    if (entry->ping_status == PING_NONE)
+    if (!entry || entry->ping_status == PING_NONE)
 	peerGetPinned(ps);
     if (entry == NULL) {
 	(void) 0;
@@ -323,10 +323,12 @@ peerGetPinned(ps_state * ps)
     if (clientGetPinnedInfo(request->pinned_connection, request, &peer) != -1) {
 	if (peer && peerAllowedToUse(peer, request)) {
 	    peerAddFwdServer(&ps->servers, peer, PINNED);
-	    ps->entry->ping_status = PING_DONE;		/* Skip ICP */
+	    if (ps->entry)
+		ps->entry->ping_status = PING_DONE;	/* Skip ICP */
 	} else if (!peer && ps->direct != DIRECT_NO) {
 	    peerAddFwdServer(&ps->servers, NULL, PINNED);
-	    ps->entry->ping_status = PING_DONE;		/* Skip ICP */
+	    if (ps->entry)
+		ps->entry->ping_status = PING_DONE;	/* Skip ICP */
 	}
     }
 }
