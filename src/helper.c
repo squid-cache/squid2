0a225f680148ccbc2d3bf9cfd6f822e9e466e477
serassio <serassio>
2006-06-24 09:46:38 +0000

    Fixed wrong debug section

--- src/helper.c
+++ src/helper.c
@@ -598,10 +598,10 @@ helperServerFree(int fd, void *data)
 	debug(84, 0) ("WARNING: %s #%d (FD %d) exited\n",
 	    hlp->id_name, srv->index + 1, fd);
 	if (hlp->n_active <= hlp->n_to_start / 2) {
-	    debug(80, 0) ("Too few %s processes are running\n", hlp->id_name);
+	    debug(84, 0) ("Too few %s processes are running\n", hlp->id_name);
 	    if (hlp->last_restart > squid_curtime - 30)
 		fatalf("The %s helpers are crashing too rapidly, need help!\n", hlp->id_name);
-	    debug(80, 0) ("Starting new helpers\n");
+	    debug(84, 0) ("Starting new helpers\n");
 	    helperOpenServers(hlp);
 	}
     }
@@ -638,10 +638,10 @@ helperStatefulServerFree(int fd, void *data)
 	debug(84, 0) ("WARNING: %s #%d (FD %d) exited\n",
 	    hlp->id_name, srv->index + 1, fd);
 	if (hlp->n_active <= hlp->n_to_start / 2) {
-	    debug(80, 0) ("Too few %s processes are running\n", hlp->id_name);
+	    debug(84, 0) ("Too few %s processes are running\n", hlp->id_name);
 	    if (hlp->last_restart > squid_curtime - 30)
 		fatalf("The %s helpers are crashing too rapidly, need help!\n", hlp->id_name);
-	    debug(80, 0) ("Starting new helpers\n");
+	    debug(84, 0) ("Starting new helpers\n");
 	    helperStatefulOpenServers(hlp);
 	}
     }
