9b1618db4ee991e8a5938e1f05b74012a4212277
Henrik Nordstrom <hno@squid-cache.org>
2006-07-26 19:55:10 +0000

    Bug #1694: Assertion failure in mgr:config if using access_log_format %<h

--- src/access_log.c
+++ src/access_log.c
@@ -398,7 +398,9 @@ struct logformat_token_table_entry logformat_token_table[] =
     {"tr", LFT_TIME_TO_HANDLE_REQUEST},
 
     {">h", LFT_REQUEST_HEADER},
+    {">h", LFT_REQUEST_ALL_HEADERS},
     {"<h", LFT_REPLY_HEADER},
+    {"<h", LFT_REPLY_ALL_HEADERS},
 
     {"un", LFT_USER_NAME},
     {"ul", LFT_USER_LOGIN},
@@ -983,8 +985,8 @@ accessLogDumpLogFormat(StoreEntry * entry, const char *name, logformat * definit
 		assert(te->config != NULL);
 	    }
 	}
+	storeAppend(entry, "\n", 1);
     }
-    storeAppend(entry, "\n", 1);
 }
 
 void
