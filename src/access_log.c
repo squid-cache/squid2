e3177e293adf96bcc54ac34fb5f87036dc35ed36
Henrik Nordstrom <hno@squid-cache.org>
2006-08-01 17:42:29 +0000

    Bug #1665: %ue and %ui log_format tags was missing
    the implementation of the %ue and %ui log tags was only partially complete,
    missing the tag definition in the config parser.. (meaning they was
    implemented, but not understood..)

--- src/access_log.c
+++ src/access_log.c
@@ -407,6 +407,8 @@ struct logformat_token_table_entry logformat_token_table[] =
 /*{ "ur", LFT_USER_REALM }, */
 /*{ "us", LFT_USER_SCHEME }, */
     {"ui", LFT_USER_IDENT},
+    {"us", LFT_USER_SSL},
+    {"ue", LFT_USER_EXT},
 
     {"Hs", LFT_HTTP_CODE},
 /*{ "Ht", LFT_HTTP_STATUS }, */
@@ -429,13 +431,13 @@ struct logformat_token_table_entry logformat_token_table[] =
 /*{ ">sb", LFT_REQUEST_SIZE_BODY }, */
 /*{ ">sB", LFT_REQUEST_SIZE_BODY_NO_TE }, */
 
+    {"ea", LFT_EXT_LOG},
+
     {"<st", LFT_REPLY_SIZE_TOTAL},
 /*{ "<sl", LFT_REPLY_SIZE_LINE }, * /   / * the reply line (protocol, code, text) */
 /*{ "<sh", LFT_REPLY_SIZE_HEADERS }, */
 /*{ "<sb", LFT_REPLY_SIZE_BODY }, */
 /*{ "<sB", LFT_REPLY_SIZE_BODY_NO_TE }, */
-
-    {"ea", LFT_EXT_LOG},
 
     {"%", LFT_PERCENT},
 
