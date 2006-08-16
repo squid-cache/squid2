7719ca47e018a674f19d654702471fd48295c5c9
serassio <serassio>
2006-08-16 18:37:42 +0000

    Fix an harmless compiler warning on Irix.

--- helpers/ntlm_auth/fakeauth/ntlm.h
+++ helpers/ntlm_auth/fakeauth/ntlm.h
@@ -121,7 +121,7 @@ extern int fail_debug_enabled;
 #include <unistd.h>
 static char *__foo;
 #define debug(X...) if (debug_enabled) { \
-                    fprintf(stderr,"ntlm-auth[%d](%s:%d): ", getpid(), \
+                    fprintf(stderr,"ntlm-auth[%d](%s:%d): ", (int)getpid(), \
                     ((__foo=strrchr(__FILE__,'/'))==NULL?__FILE__:__foo+1),\
                     __LINE__);\
                     fprintf(stderr,X); }
