4cd4e2c39e96880328e4a8e8e9b9372fbdaa88b0
serassio <serassio>
2006-08-28 09:19:29 +0000

    Fix another harmless fake_auth compiler warning on gcc 4.1.1 x86

--- helpers/ntlm_auth/fakeauth/ntlm.h
+++ helpers/ntlm_auth/fakeauth/ntlm.h
@@ -119,9 +119,9 @@ extern int fail_debug_enabled;
 #ifdef DEBUG
 #include <stdio.h>
 #include <unistd.h>
-static char *__foo;
+static const char *__foo;
 #define debug(X...) if (debug_enabled) { \
-                    fprintf(stderr,"ntlm-auth[%d](%s:%d): ", (int)getpid(), \
+                    fprintf(stderr,"ntlm-auth[%ld](%s:%d): ", (long)getpid(), \
                     ((__foo=strrchr(__FILE__,'/'))==NULL?__FILE__:__foo+1),\
                     __LINE__);\
                     fprintf(stderr,X); }
@@ -142,7 +142,7 @@ debug(char *format,...)
 	va_list args;
 
 	va_start(args, format);
-	fprintf(stderr, "ntlm-auth[%d]: ", getpid());
+	fprintf(stderr, "ntlm-auth[%ld]: ", (long)getpid());
 	vfprintf(stderr, format, args);
 	va_end(args);
 #if FAIL_DEBUG
