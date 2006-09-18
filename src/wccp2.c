19a7988fedc6a863ac8bc863abbd660f55f49fb3
Henrik Nordstrom <hno@squid-cache.org>
2006-09-18 23:09:39 +0000

    Bug #1770: WCCP2 weighted assignment
    This patch adds the wccp2_weight directive defining this cache servers
    proportional weight in the WCCP2 cache cluster.

--- src/wccp2.c
+++ src/wccp2.c
@@ -267,6 +267,7 @@ struct router_view_t {
 /* Lists used to keep track of caches, routers and services */
 struct wccp2_cache_list_t {
     struct in_addr cache_ip;
+    int weight;
     struct wccp2_cache_list_t *next;
 };
 
@@ -544,7 +545,7 @@ wccp2Init(void)
 	memset(&wccp2_identity_info.cache_identity.hash_revision, '\0', sizeof(wccp2_identity_info.cache_identity.hash_revision));
 	memset(&wccp2_identity_info.cache_identity.bits, '\0', sizeof(wccp2_identity_info.cache_identity.bits));
 	memset(&wccp2_identity_info.cache_identity.buckets, '\0', sizeof(wccp2_identity_info.cache_identity.buckets));
-	wccp2_identity_info.cache_identity.weight = htons(10000);
+	wccp2_identity_info.cache_identity.weight = htons(Config.Wccp2.weight);
 	memset(&wccp2_identity_info.cache_identity.status, '\0', sizeof(wccp2_identity_info.cache_identity.status));
 	xmemcpy(ptr, &wccp2_identity_info, sizeof(struct wccp2_identity_info_t));
 	service_list_ptr->wccp2_identity_info_ptr = (struct wccp2_identity_info_t *) ptr;
@@ -1005,6 +1006,7 @@ wccp2HandleUdp(int sock, void *not_used)
 
 	    /* Update the cache list */
 	    cache_list_ptr->cache_ip = cache_address;
+	    cache_list_ptr->weight = ntohs(cache_identity->weight);
 	    cache_list_ptr->next = xcalloc(1, sizeof(struct wccp2_cache_list_t));
 	    cache_list_ptr = cache_list_ptr->next;
 	    cache_list_ptr->next = NULL;
@@ -1200,29 +1202,63 @@ wccp2AssignBuckets(void *voidnotused)
 
 	saved_offset = offset;
 	for (router_list_ptr = &service_list_ptr->router_list_head; router_list_ptr->next != NULL; router_list_ptr = router_list_ptr->next) {
+	    unsigned long *weight = xcalloc(sizeof(*weight), ntohl(router_list_ptr->num_caches));
+	    unsigned long total_weight = 0;
+	    int num_caches = ntohl(router_list_ptr->num_caches);
 	    offset = saved_offset;
 
 	    /* Number of caches */
 	    xmemcpy(&wccp_packet[offset], &router_list_ptr->num_caches, sizeof(router_list_ptr->num_caches));
 	    offset += sizeof(router_list_ptr->num_caches);
 
-	    if (ntohl(router_list_ptr->num_caches)) {
-		for (cache_list_ptr = &router_list_ptr->cache_list_head; cache_list_ptr->next; cache_list_ptr = cache_list_ptr->next) {
+	    if (num_caches) {
+		int cache;
+		for (cache = 0, cache_list_ptr = &router_list_ptr->cache_list_head; cache_list_ptr->next; cache_list_ptr = cache_list_ptr->next, cache++) {
 		    /* add caches */
 		    cache_address = (struct in_addr *) &wccp_packet[offset];
 		    xmemcpy(cache_address, &cache_list_ptr->cache_ip, sizeof(struct in_addr));
+		    total_weight += cache_list_ptr->weight << 12;
+		    weight[cache] = cache_list_ptr->weight << 12;
 		    offset += sizeof(struct in_addr);
 		}
 	    }
 	    /* Add buckets */
 	    buckets = (char *) &wccp_packet[offset];
 	    memset(buckets, '\0', WCCP_BUCKETS);
-	    if (ntohl(router_list_ptr->num_caches) != 0) {
-		for (bucket_counter = 0; bucket_counter < WCCP_BUCKETS; bucket_counter++) {
-		    buckets[bucket_counter] = (char) (bucket_counter % ntohl(router_list_ptr->num_caches));
+	    if (num_caches != 0) {
+		if (total_weight == 0) {
+		    for (bucket_counter = 0; bucket_counter < WCCP_BUCKETS; bucket_counter++) {
+			buckets[bucket_counter] = (char) (bucket_counter % num_caches);
+		    }
+		} else {
+		    unsigned long *assigned = xcalloc(sizeof(*assigned), num_caches);
+		    unsigned long done = 0;
+		    int cache = -1;
+		    unsigned long per_bucket = total_weight / WCCP_BUCKETS;
+		    for (bucket_counter = 0; bucket_counter < WCCP_BUCKETS; bucket_counter++) {
+			int n;
+			unsigned long step;
+			for (n = num_caches; n; n--) {
+			    cache++;
+			    if (cache >= num_caches)
+				cache = 0;
+			    if (!weight[cache]) {
+				n++;
+				continue;
+			    }
+			    if (assigned[cache] <= done)
+				break;
+			}
+			buckets[bucket_counter] = (char) cache;
+			step = per_bucket * total_weight / weight[cache];
+			assigned[cache] += step;
+			done += per_bucket;
+		    }
+		    safe_free(assigned);
 		}
 	    }
 	    offset += (WCCP_BUCKETS * sizeof(char));
+	    safe_free(weight);
 
 	    /* Fill in length */
 	    assignment_header->length = htons(sizeof(struct assignment_key_t) + sizeof(service_list_ptr->num_routers) +
