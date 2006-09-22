59795a1bceda26cf2cd7fb86e4056485ce1ccdb7
serassio <serassio>
2006-09-22 11:30:17 +0000

    Windows port: add S_ISREG() macro

--- include/squid_mswin.h
+++ include/squid_mswin.h
@@ -138,6 +138,7 @@ typedef unsigned long ino_t;
 #define S_IRWXO 007
 #if defined(_MSC_VER) /* Microsoft C Compiler ONLY */
 #define	S_ISDIR(m) (((m) & _S_IFDIR) == _S_IFDIR)
+#define	S_ISREG(m) (((m) & _S_IFREG) == _S_IFREG)
 #endif
 
 #ifndef SIGHUP
