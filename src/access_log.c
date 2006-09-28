76013b17bb112db77edc6d16ddfc8f05d0d200c9
Henrik Nordstrom <hno@squid-cache.org>
2006-09-28 21:55:37 +0000

    Bug #1784: access_log syslog results in blanks syslog lines between every access log entry.
    workaround: define a custom log format.

--- src/access_log.c
+++ src/access_log.c
@@ -1031,31 +1031,46 @@ accessLogSquid(AccessLogEntry * al, Logfile * logfile)
 #endif
     if (user && !*user)
 	safe_free(user);
-    logfilePrintf(logfile, "%9ld.%03d %6d %s %s/%03d %" PRINTF_OFF_T " %s %s %s %s%s/%s %s",
-	(long int) current_time.tv_sec,
-	(int) current_time.tv_usec / 1000,
-	al->cache.msec,
-	client,
-	log_tags[al->cache.code],
-	al->http.code,
-	al->cache.size,
-	al->private.method_str,
-	al->url,
-	user ? user : dash_str,
-	al->hier.ping.timedout ? "TIMEOUT_" : "",
-	hier_strings[al->hier.code],
-	al->hier.host,
-	al->http.content_type);
-    safe_free(user);
-    if (Config.onoff.log_mime_hdrs) {
+    if (!Config.onoff.log_mime_hdrs) {
+	logfilePrintf(logfile, "%9ld.%03d %6d %s %s/%03d %" PRINTF_OFF_T " %s %s %s %s%s/%s %s\n",
+	    (long int) current_time.tv_sec,
+	    (int) current_time.tv_usec / 1000,
+	    al->cache.msec,
+	    client,
+	    log_tags[al->cache.code],
+	    al->http.code,
+	    al->cache.size,
+	    al->private.method_str,
+	    al->url,
+	    user ? user : dash_str,
+	    al->hier.ping.timedout ? "TIMEOUT_" : "",
+	    hier_strings[al->hier.code],
+	    al->hier.host,
+	    al->http.content_type);
+    } else {
 	char *ereq = log_quote(al->headers.request);
 	char *erep = log_quote(al->headers.reply);
-	logfilePrintf(logfile, " [%s] [%s]\n", ereq, erep);
+	logfilePrintf(logfile, "%9ld.%03d %6d %s %s/%03d %" PRINTF_OFF_T " %s %s %s %s%s/%s %s [%s] [%s]\n",
+	    (long int) current_time.tv_sec,
+	    (int) current_time.tv_usec / 1000,
+	    al->cache.msec,
+	    client,
+	    log_tags[al->cache.code],
+	    al->http.code,
+	    al->cache.size,
+	    al->private.method_str,
+	    al->url,
+	    user ? user : dash_str,
+	    al->hier.ping.timedout ? "TIMEOUT_" : "",
+	    hier_strings[al->hier.code],
+	    al->hier.host,
+	    al->http.content_type,
+	    ereq,
+	    erep);
 	safe_free(ereq);
 	safe_free(erep);
-    } else {
-	logfilePrintf(logfile, "\n");
     }
+    safe_free(user);
 }
 
 static void
