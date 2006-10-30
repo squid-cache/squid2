41589e9f1fe9df887f094c78a9081ffa6d815227
Henrik Nordstrom <hno@squid-cache.org>
2006-10-30 00:04:52 +0000

    comm_select cleanups, adding sys/time.h dependency and checking error indications

--- src/comm_select.c
+++ src/comm_select.c
@@ -35,6 +35,10 @@
 #include "squid.h"
 #include "comm_generic.c"
 
+#if HAVE_SYS_TIME_H
+#include <sys/time.h>
+#endif
+
 #if HAVE_SYS_SELECT_H
 #include <sys/select.h>
 #endif
@@ -52,6 +56,7 @@ static fd_set *global_readfds;
 static fd_set *global_writefds;
 static fd_set *current_readfds;
 static fd_set *current_writefds;
+static fd_set *current_errfds;
 static int nreadfds;
 static int nwritefds;
 
@@ -62,6 +67,7 @@ do_select_init()
     global_writefds = xcalloc(FD_MASK_BYTES, howmany(Squid_MaxFD, FD_MASK_BITS));
     current_readfds = xcalloc(FD_MASK_BYTES, howmany(Squid_MaxFD, FD_MASK_BITS));
     current_writefds = xcalloc(FD_MASK_BYTES, howmany(Squid_MaxFD, FD_MASK_BITS));
+    current_errfds = xcalloc(FD_MASK_BYTES, howmany(Squid_MaxFD, FD_MASK_BITS));
     nreadfds = nwritefds = 0;
 }
 
@@ -78,6 +84,7 @@ do_select_shutdown()
     safe_free(global_writefds);
     safe_free(current_readfds);
     safe_free(current_writefds);
+    safe_free(current_errfds);
 }
 
 void
@@ -112,6 +119,7 @@ do_comm_select(int msec)
     struct timeval tv;
     fd_mask *rfdsp = (fd_mask *) current_readfds;
     fd_mask *wfdsp = (fd_mask *) current_writefds;
+    fd_mask *efdsp = (fd_mask *) current_errfds;
     int maxindex = howmany(Biggest_FD + 1, FD_MASK_BITS);
     int fd_set_size = maxindex * FD_MASK_BYTES;
     int j;
@@ -122,10 +130,11 @@ do_comm_select(int msec)
     }
     memcpy(current_readfds, global_readfds, fd_set_size);
     memcpy(current_writefds, global_writefds, fd_set_size);
+    memcpy(current_errfds, global_writefds, fd_set_size);
     tv.tv_sec = msec / 1000;
     tv.tv_usec = (msec % 1000) * 1000;
     statCounter.syscalls.selects++;
-    num = select(Biggest_FD + 1, current_readfds, current_writefds, NULL, &tv);
+    num = select(Biggest_FD + 1, current_readfds, current_writefds, current_errfds, &tv);
     statCounter.select_loops++;
 
     if (num < 0) {
@@ -143,15 +152,18 @@ do_comm_select(int msec)
 
     for (j = 0; j < maxindex; j++) {
 	int k;
-	fd_mask tmask = rfdsp[j] | wfdsp[j];
+	fd_mask tmask = rfdsp[j] | wfdsp[j] | efdsp[j];
 	for (k = 0; tmask && k < FD_MASK_BITS; k++) {
 	    int fd;
+	    int read_event, write_event;
 	    if (!EBIT_TEST(tmask, k))
 		continue;
 	    /* Found a set bit */
 	    fd = (j * FD_MASK_BITS) + k;
+	    read_event = FD_ISSET(fd, current_readfds);
+	    write_event = FD_ISSET(fd, current_writefds) | FD_ISSET(fd, current_errfds);
 	    EBIT_CLR(tmask, k);	/* this will be done */
-	    comm_call_handlers(fd, FD_ISSET(fd, current_readfds), FD_ISSET(fd, current_writefds));
+	    comm_call_handlers(fd, read_event, write_event);
 	}
     }
     return COMM_OK;
