7ffcec218d5e6c68bbf4bb627b352096efae237a
Henrik Nordstrom <hno@squid-cache.org>
2006-12-02 21:51:14 +0000

    Bug #1584: Unable to register with multiple WCCP2 routers
    Disable PMTU-discovery on the WCCP2 UDP socket, making Linux and possibly
    others send unique IP ID fields in the UDP traffic. Hopefully this will
    convince Cisco IOS that the WCCP exchanges is not duplicate traffic.

--- src/wccp2.c
+++ src/wccp2.c
@@ -772,6 +772,12 @@ wccp2ConnectionOpen(void)
 	"WCCPv2 Socket");
     if (theWccp2Connection < 0)
 	fatal("Cannot open WCCP Port");
+#if defined(IP_MTU_DISCOVER) && defined(IP_PMTUDISC_DONT)
+    {
+	int i = IP_PMTUDISC_DONT;
+	setsockopt(theWccp2Connection, SOL_IP, IP_MTU_DISCOVER, &i, sizeof i);
+    }
+#endif
     commSetSelect(theWccp2Connection,
 	COMM_SELECT_READ,
 	wccp2HandleUdp,
