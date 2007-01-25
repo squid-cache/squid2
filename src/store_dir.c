881361b42598e8b9cf0c2fd90547a08c34872891
Duane Wessels <wessels@packet-pushers.com>
2007-01-25 20:39:38 +0000

    CLEAN_BUF_SZ was defined, but never used anywhere

--- src/store_dir.c
+++ src/store_dir.c
@@ -403,7 +403,6 @@ storeDirCloseSwapLogs(void)
  *  the run. Thanks goes to Eric Stern, since this solution
  *  came out of his COSS code.
  */
-#define CLEAN_BUF_SZ 16384
 int
 storeDirWriteCleanLogs(int reopen)
 {
@@ -472,7 +471,6 @@ storeDirWriteCleanLogs(int reopen)
 	dt, (double) n / (dt > 0.0 ? dt : 1.0));
     return n;
 }
-#undef CLEAN_BUF_SZ
 
 /*
  * sync all avaliable fs'es ..
