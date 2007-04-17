19621a95b3109b84e3edd0c0379adb4548ec1418
Henrik Nordstrom <hno@squid-cache.org>
2007-04-17 09:39:56 +0000

    MFC: Don't log ECONNREFUSED/ECONNABORTED accept failures in cache.log
    These errors isn't very interesting any may occur during normal
    operation on some operating systems due to client behaviour.
    
    Merged changes:
    2007/04/02 14:06:08 hno +2 -2 Don't log ECONNREFUSED accept errors in cache.log. Not very interesting.
    2007/04/02 14:09:15 hno +2 -2 Don't log ECONNABORTED accept failures in cache.log, these isn't very interesting

--- src/comm.c
+++ src/comm.c
@@ -550,7 +550,7 @@ comm_accept(int fd, struct sockaddr_in *pn, struct sockaddr_in *me)
     Slen = sizeof(P);
     statCounter.syscalls.sock.accepts++;
     if ((sock = accept(fd, (struct sockaddr *) &P, &Slen)) < 0) {
-	if (ignoreErrno(errno)) {
+	if (ignoreErrno(errno) || errno == ECONNREFUSED || errno == ECONNABORTED) {
 	    debug(5, 5) ("comm_accept: FD %d: %s\n", fd, xstrerror());
 	    return COMM_NOMESSAGE;
 	} else if (ENFILE == errno || EMFILE == errno) {
