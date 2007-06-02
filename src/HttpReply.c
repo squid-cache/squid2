99410daaa1853a459081115978682e7d73607727
Henrik Nordstrom <hno@squid-cache.org>
2007-06-02 18:52:14 +0000

    Fix header cache, affecting content-length in access.log
    The header cache was only partially set on cloned replies, causing
    the content-type to not be logged in access.log.
    
    This change makes the header cache to be fully initialized when the
    reply is cloned, not even attempting to copy the cached results.
    
    Could be optimized to copy the cached results instead.. probably
    only content_length missing (audit needed).

--- src/HttpReply.c
+++ src/HttpReply.c
@@ -358,10 +358,6 @@ httpReplyClone(HttpReply * src)
 
     /* basic variables */
     dst->hdr_sz = src->hdr_sz;
-    dst->content_length = src->content_length;
-    dst->date = src->date;
-    dst->last_modified = src->last_modified;
-    dst->expires = src->expires;
 
     /* parser state */
     dst->pstate = src->pstate;
@@ -378,10 +374,15 @@ httpReplyClone(HttpReply * src)
      * when we've already -done- that, but I'll worry about doing it
      * faster later. Besides, there's too much other code to fix up.
      */
-    /* cache control */
+    httpReplyHdrCacheInit(dst);
+#if 0
+    dst->content_length = src->content_length;
+    dst->date = src->date;
+    dst->last_modified = src->last_modified;
+    dst->expires = src->expires;
     dst->cache_control = httpHeaderGetCc(&dst->header);
-    /* content range */
     dst->content_range = httpHeaderGetContRange(&dst->header);
+#endif
 
     return dst;
 }
