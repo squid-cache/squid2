586c85908fdf29bbe69419f836214591582f60b2
Henrik Nordstrom <hno@squid-cache.org>
2007-06-25 23:39:04 +0000

    Author: Gonzalo Arana <gonzalo.arana@gmail.com> MFC: Bug #1085: Add no-wrap to cache manager HTML tables
    Adding "white-space:nowrap" to cache manager styles makes 'Memory Utilization'
    more readable I think.
    
    Merged changes:
    2007/06/25 12:25:42	Gonzalo Arana	+2 -2	Bug #1085: Add no-wrap to cache manager HTML tables

--- tools/cachemgr.c
+++ tools/cachemgr.c
@@ -592,7 +592,7 @@ read_reply(int s, cachemgr_request * req)
 	    printf("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\" \"http://www.w3.org/TR/html4/loose.dtd\">\n");
 	    printf("<HTML><HEAD><TITLE>CacheMgr@%s: %s</TITLE>\n",
 		req->hostname, action);
-	    printf("<STYLE type=\"text/css\"><!--BODY{background-color:#ffffff;font-family:verdana,sans-serif}TABLE{background-color:#333333;border:0pt;padding:0pt}TH,TD{background-color:#ffffff}--></STYLE>\n");
+	    printf("<STYLE type=\"text/css\"><!--BODY{background-color:#ffffff;font-family:verdana,sans-serif}TABLE{background-color:#333333;border:0pt;padding:0pt}TH,TD{background-color:#ffffff;white-space:nowrap}--></STYLE>\n");
 	    printf("</HEAD><BODY>\n");
 	    if (parse_menu) {
 		printf("<H2><a href=\"%s\">Cache Manager</a> menu for %s:</H2>",
