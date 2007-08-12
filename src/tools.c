33804b4d4443d89f3fc915218347d8ddcc34010e
serassio <serassio>
2007-08-12 10:30:58 +0000

    Fixed typo in previous commit (forgotten to remove a no more needed line)

--- src/tools.c
+++ src/tools.c
@@ -798,7 +798,6 @@ setSystemLimits(void)
 /* RLIMIT_NOFILE doesn't work on Cygwin */
 #if HAVE_SETRLIMIT && defined(RLIMIT_NOFILE) && !defined(_SQUID_CYGWIN_)
     /* limit system filedescriptors to our own limit */
-    struct rlimit rl;
     if (getrlimit(RLIMIT_NOFILE, &rl) < 0) {
 	debug(50, 0) ("setrlimit: RLIMIT_NOFILE: %s\n", xstrerror());
     } else {
