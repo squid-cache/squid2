590b6cee543689f2edd2bf91c832b2db3a58ec34
Henrik Nordstrom <hno@squid-cache.org>
2007-08-30 21:52:47 +0000

    MFC: Bug #2066: chdir after chroot

--- src/main.c
+++ src/main.c
@@ -526,7 +526,7 @@ static void
 mainInitialize(void)
 {
     /* chroot if configured to run inside chroot */
-    if (Config.chroot_dir && chroot(Config.chroot_dir)) {
+    if (Config.chroot_dir && (chroot(Config.chroot_dir) != 0 || chdir("/") != 0)) {
 	fatal("failed to chroot");
     }
     if (opt_catch_signals) {
