e4b8440e0b32b0a655293d2835214c6b57e0b788
Henrik Nordstrom <hno@squid-cache.org>
2008-01-09 13:58:12 +0000

    MFC: Bug #2175: Update valgrind support for valgrind-3.3.0
    Valgrind memcheck rearranged the names of it's support macros in
    version 3.2.0 to better represent the actual function, and now the
    old names has been removed starting with version 3.3.0 causing
    build a failure if --with-valgrind-debug is used.
    
    this patch updates Squid to use the new valgrind macro names, and
    adds a little glue to be compatible with older valgrind versions

--- src/MemPool.c
+++ src/MemPool.c
@@ -266,16 +266,16 @@ memPoolAlloc(MemPool * pool)
 	gb_inc(&TheMeter.saved, pool->obj_size);
 	obj = stackPop(&pool->pstack);
 #if DEBUG_MEMPOOL
-	(void) VALGRIND_MAKE_READABLE(obj, pool->real_obj_size + sizeof(struct mempool_cookie));
+	(void) VALGRIND_MAKE_MEM_DEFINED(obj, pool->real_obj_size + sizeof(struct mempool_cookie));
 #else
-	(void) VALGRIND_MAKE_READABLE(obj, pool->obj_size);
+	(void) VALGRIND_MAKE_MEM_DEFINED(obj, pool->obj_size);
 #endif
 #if DEBUG_MEMPOOL
 	{
 	    struct mempool_cookie *cookie = (void *) (((unsigned char *) obj) + pool->real_obj_size);
 	    assert(cookie->cookie == MEMPOOL_COOKIE(obj));
 	    assert(cookie->pool == pool);
-	    (void) VALGRIND_MAKE_NOACCESS(cookie, sizeof(cookie));
+	    (void) VALGRIND_MAKE_MEM_NOACCESS(cookie, sizeof(cookie));
 	}
 #endif
     } else {
@@ -289,7 +289,7 @@ memPoolAlloc(MemPool * pool)
 	    cookie = (struct mempool_cookie *) (((unsigned char *) obj) + pool->real_obj_size);
 	    cookie->cookie = MEMPOOL_COOKIE(obj);
 	    cookie->pool = pool;
-	    (void) VALGRIND_MAKE_NOACCESS(cookie, sizeof(cookie));
+	    (void) VALGRIND_MAKE_MEM_NOACCESS(cookie, sizeof(cookie));
 	}
 #else
 	obj = xcalloc(1, pool->obj_size);
@@ -305,11 +305,11 @@ memPoolFree(MemPool * pool, void *obj)
     memMeterDec(pool->meter.inuse);
     memMeterDel(TheMeter.inuse, pool->obj_size);
     mem_pool_free_calls++;
-    (void) VALGRIND_CHECK_WRITABLE(obj, pool->obj_size);
+    (void) VALGRIND_CHECK_MEM_IS_ADDRESSABLE(obj, pool->obj_size);
 #if DEBUG_MEMPOOL
     {
 	struct mempool_cookie *cookie = (void *) (((unsigned char *) obj) + pool->real_obj_size);
-	(void) VALGRIND_MAKE_READABLE(cookie, sizeof(cookie));
+	(void) VALGRIND_MAKE_MEM_DEFINED(cookie, sizeof(cookie));
 	assert(cookie->cookie == MEMPOOL_COOKIE(obj));
 	assert(cookie->pool == pool);
     }
@@ -319,9 +319,9 @@ memPoolFree(MemPool * pool, void *obj)
 	memMeterAdd(TheMeter.idle, pool->obj_size);
 	memset(obj, 0, pool->obj_size);
 #if DEBUG_MEMPOOL
-	(void) VALGRIND_MAKE_NOACCESS(obj, pool->real_obj_size + sizeof(struct mempool_cookie));
+	(void) VALGRIND_MAKE_MEM_NOACCESS(obj, pool->real_obj_size + sizeof(struct mempool_cookie));
 #else
-	(void) VALGRIND_MAKE_NOACCESS(obj, pool->obj_size);
+	(void) VALGRIND_MAKE_MEM_NOACCESS(obj, pool->obj_size);
 #endif
 	stackPush(&pool->pstack, obj);
     } else {
