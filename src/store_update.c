b86bd59fb110c064acbd462011107701298167c6
Henrik Nordstrom <hno@squid-cache.org>
2008-01-22 14:35:10 +0000

    MFC: Bug #2173: Memory leak on header update of Vary:ing objects

--- src/store_update.c
+++ src/store_update.c
@@ -164,7 +164,7 @@ storeUpdate(StoreEntry * entry, request_t * request)
 	if (vary) {
 	    state->newentry->mem_obj->vary_headers = xstrdup(vary);
 	    if (strBuf(request->vary_encoding))
-		entry->mem_obj->vary_encoding = xstrdup(strBuf(request->vary_encoding));
+		state->newentry->mem_obj->vary_encoding = xstrdup(strBuf(request->vary_encoding));
 	}
     } else {
 	if (entry->mem_obj->vary_headers)
