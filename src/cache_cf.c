497776dc407273086bd6d6f62aaca8a41fbc666c
Henrik Nordstrom <hno@squid-cache.org>
2008-02-08 02:01:15 +0000

    Make include support wildcard patterns

--- src/cache_cf.c
+++ src/cache_cf.c
@@ -34,6 +34,7 @@
  */
 
 #include "squid.h"
+#include <glob.h>
 
 #if SQUID_SNMP
 #include "snmp.h"
@@ -331,11 +332,20 @@ parseManyConfigFiles(char *files, int depth)
 {
     int error_count = 0;
     char *saveptr = NULL;
-    char *file = strwordtok(files, &saveptr);
-    while (file != NULL) {
-	error_count += parseOneConfigFile(file, depth);
-	file = strwordtok(NULL, &saveptr);
+    char *path;
+    glob_t globbuf;
+    int i;
+    memset(&globbuf, 0, sizeof(globbuf));
+    for (path = strwordtok(files, &saveptr); path; path = strwordtok(NULL, &saveptr)) {
+	if (glob(path, globbuf.gl_pathc ? GLOB_APPEND : 0, NULL, &globbuf) != 0) {
+	    fatalf("Unable to find configuration file: %s: %s",
+		path, xstrerror());
+	}
     }
+    for (i = 0; i < globbuf.gl_pathc; i++) {
+	error_count += parseOneConfigFile(globbuf.gl_pathv[i], depth);
+    }
+    globfree(&globbuf);
     return error_count;
 }
 
