2dc1ce3672cb47d77ff444e6412272466127686f
Henrik Nordstrom <hno@squid-cache.org>
2008-03-21 23:00:57 +0000

    Bug #2278: Cache-Control: max-stale=0 forwarded wrongly as max-stale (without delta)
    not exacly what was indended by the requesting client.. max-stale=0 and
    max-stale is the exact opposite of each other.

--- src/HttpHdrCc.c
+++ src/HttpHdrCc.c
@@ -208,7 +208,7 @@ httpHdrCcPackInto(const HttpHdrCc * cc, Packer * p)
 	    if (flag == CC_S_MAXAGE)
 		packerPrintf(p, "=%d", (int) cc->s_maxage);
 
-	    if (flag == CC_MAX_STALE && cc->max_stale > 0)
+	    if (flag == CC_MAX_STALE && cc->max_stale >= 0)
 		packerPrintf(p, "=%d", (int) cc->max_stale);
 
 	    pcount++;
