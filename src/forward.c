4190cf91eb8720856a32fe542e5980f1e0bf01e0
Henrik Nordstrom <hno@squid-cache.org>
2008-04-02 01:16:29 +0000

    Author: Nerijus Baliunas Bug #1886: tcp_outgoing_address acl doesn't work with indirect source address (follow-x-forwarded-for)
    Using squid with --enable-follow-x-forwarded-for works as expected with source
    address in logs and in acls, but not when using tcp_outgoing_address acl.
    
    also applies to tcp_outgoing_tos
    
    Fixed differently in Squid-2.7

--- src/forward.c
+++ src/forward.c
@@ -424,7 +424,12 @@ getOutgoingAddr(request_t * request)
     aclCheck_t ch;
     memset(&ch, '\0', sizeof(aclCheck_t));
     if (request) {
-	ch.src_addr = request->client_addr;
+#if FOLLOW_X_FORWARDED_FOR
+	if (Config.onoff.acl_uses_indirect_client) {
+	    ch.src_addr = request->indirect_client_addr;
+	} else
+#endif /* FOLLOW_X_FORWARDED_FOR */
+	    ch.src_addr = request->client_addr;
 	ch.my_addr = request->my_addr;
 	ch.my_port = request->my_port;
 	ch.request = request;
@@ -438,7 +443,12 @@ getOutgoingTOS(request_t * request)
     aclCheck_t ch;
     memset(&ch, '\0', sizeof(aclCheck_t));
     if (request) {
-	ch.src_addr = request->client_addr;
+#if FOLLOW_X_FORWARDED_FOR
+	if (Config.onoff.acl_uses_indirect_client) {
+	    ch.src_addr = request->indirect_client_addr;
+	} else
+#endif /* FOLLOW_X_FORWARDED_FOR */
+	    ch.src_addr = request->client_addr;
 	ch.my_addr = request->my_addr;
 	ch.my_port = request->my_port;
 	ch.request = request;
