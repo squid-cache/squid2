a9e583925b7c06c690bda3c7e431b4e9fa50901e
Henrik Nordstrom <hno@squid-cache.org>
2008-04-25 19:45:19 +0000

    Author: Alexander Lukyanov <lav@yar.ru> Bug 2329: Range header ignored on HIT

--- src/HttpHdrRange.c
+++ src/HttpHdrRange.c
@@ -242,7 +242,7 @@ httpHdrRangeParseInit(HttpHdrRange * range, const String * str)
     if (strNCaseCmp(*str, "bytes=", 6))
 	return 0;
     /* skip "bytes="; hack! */
-    pos = strBuf(*str) + 5;
+    pos = strBuf(*str) + 6;
     /* iterate through comma separated list */
     while (strListGetItem(str, ',', &item, &ilen, &pos)) {
 	HttpHdrRangeSpec *spec = httpHdrRangeSpecParseCreate(item, ilen);
