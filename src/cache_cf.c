e46e35d13b37e13b87c1cabbad423c842ec02f75
Henrik Nordstrom <hno@squid-cache.org>
2008-06-11 14:49:23 +0000

    Bug #2192: http_port ... vport broken by recent changes in how accelerator mode deals with port numbers

--- src/cache_cf.c
+++ src/cache_cf.c
@@ -2994,7 +2994,7 @@ parse_http_port_option(http_port_list * s, char *token)
 	s->vhost = 1;
 	s->accel = 1;
     } else if (strcmp(token, "vport") == 0) {
-	s->vport = ntohs(s->s.sin_port);
+	s->vport = -1;
 	s->accel = 1;
     } else if (strncmp(token, "vport=", 6) == 0) {
 	s->vport = xatos(token + 6);
