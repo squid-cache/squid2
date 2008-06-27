c25725c8bea6d37bc5b4b8335ae7ee3e99caa722
Henrik Nordstrom <hno@squid-cache.org>
2008-06-27 21:53:17 +0000

    Plug a small "squid -k reconfigure" race in the new round-robin counter management
    The round-robin clear loop only got started on startup, not if the first
    round-robin peer was added by a "squid -k reconfigure".

--- src/cache_cf.c
+++ src/cache_cf.c
@@ -1618,7 +1618,6 @@ static void
 parse_peer(peer ** head)
 {
     char *token = NULL;
-    void *arg = NULL;		/* throwaway arg to make eventAdd happy */
     peer *p;
     p = cbdataAlloc(peer);
     p->http_port = CACHE_HTTP_PORT;
@@ -1810,9 +1809,7 @@ parse_peer(peer ** head)
 	head = &(*head)->next;
     *head = p;
     Config.npeers++;
-    if (!reconfiguring && Config.npeers == 1) {
-	peerClearRRLoop(arg);
-    }
+    peerClearRRStart();
 }
 
 static void
