9f0223362003056fdb2fbea3d8c83093cfd9ac5f
serassio <serassio>
2008-06-30 16:20:06 +0000

    Windows port: fix typo in handling of notification of IP address changes

--- src/win32.c
+++ src/win32.c
@@ -368,7 +368,7 @@ WIN32_IpAddrChangeMonitorExit()
 {
     DWORD status = ERROR_SUCCESS;
 
-    if (NotifyAddrChange_thread = !INVALID_HANDLE_VALUE) {
+    if (NotifyAddrChange_thread != INVALID_HANDLE_VALUE) {
 	TerminateThread(NotifyAddrChange_thread, status);
 	CloseHandle(NotifyAddrChange_thread);
     }
