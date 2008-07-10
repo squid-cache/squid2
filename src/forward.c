5c94a1e30c881d7396560af9c9b9505bfb16f3da
Henrik Nordstrom <hno@squid-cache.org>
2008-07-10 09:48:41 +0000

    Bug #2408: assertion failed: forward.c:529: "fs"
    pinned connections is a bit special and we need to add the alternate
    paths even if it looks the same as a pinned one..
    
    also make the forwarding logic a little more robust in case
    there is no server left to try.

--- src/forward.c
+++ src/forward.c
@@ -53,6 +53,7 @@ static PF fwdPeerClosed;
 static CNCB fwdConnectDone;
 static int fwdCheckRetry(FwdState * fwdState);
 static int fwdReforward(FwdState *);
+static void fwdRestart(void *);
 static void fwdStartFail(FwdState *);
 static void fwdLogReplyStatus(int tries, http_status status);
 static OBJH fwdStats;
@@ -207,7 +208,7 @@ fwdServerClosed(int fd, void *data)
 	    }
 	}
 	/* use eventAdd to break potential call sequence loops and to slow things down a little */
-	eventAdd("fwdConnectStart", fwdConnectStart, fwdState, originserver ? 0.05 : 0.005, 0);
+	eventAdd("fwdRestart", fwdRestart, fwdState, originserver ? 0.05 : 0.005, 0);
 	return;
     }
     if (!fwdState->err && shutting_down)
@@ -571,7 +572,7 @@ fwdConnectStart(void *data)
 	fwdState->request->pinned_connection = NULL;
 	fwdState->servers = fs->next;
 	fwdServerFree(fs);
-	fwdConnectStart(fwdState);
+	fwdRestart(fwdState);
 	return;
     }
 #if LINUX_TPROXY
@@ -694,6 +695,16 @@ fwdConnectStart(void *data)
 	hierarchyNote(&fwdState->request->hier, fs->code, fwdState->request->host);
     }
     commConnectStart(fd, host, port, fwdConnectDone, fwdState);
+}
+
+static void
+fwdRestart(void *data)
+{
+    FwdState *fwdState = data;
+    if (fwdState->servers)
+	fwdConnectStart(fwdState);
+    else
+	fwdStartFail(fwdState);
 }
 
 static void
