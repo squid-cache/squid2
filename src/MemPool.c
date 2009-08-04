52d0327e420193eb0962eb4b5293882d1e0388d3
Henrik Nordstrom <hno@squid-cache.org>
2009-08-04 01:28:55 +0000

    Correct Valgrind mempool protection

--- src/MemPool.c
+++ src/MemPool.c
@@ -284,7 +284,7 @@ memPoolAlloc(MemPool * pool)
 	    assert(cookie->cookie == MEMPOOL_COOKIE2(obj));
 	    assert(cookie->pool == pool);
 	    cookie->cookie = MEMPOOL_COOKIE(obj);
-	    (void) VALGRIND_MAKE_MEM_NOACCESS(cookie, sizeof(cookie));
+	    (void) VALGRIND_MAKE_MEM_NOACCESS(cookie, sizeof(*cookie));
 	}
 	if (Config.onoff.zero_buffers || pool->flags.dozero)
 	    memset(obj, 0, pool->obj_size);
@@ -300,7 +300,7 @@ memPoolAlloc(MemPool * pool)
 	    cookie = (struct mempool_cookie *) (((unsigned char *) obj) + pool->real_obj_size);
 	    cookie->cookie = MEMPOOL_COOKIE(obj);
 	    cookie->pool = pool;
-	    (void) VALGRIND_MAKE_MEM_NOACCESS(cookie, sizeof(cookie));
+	    (void) VALGRIND_MAKE_MEM_NOACCESS(cookie, sizeof(*cookie));
 	}
 #else
 	if (Config.onoff.zero_buffers || pool->flags.dozero)
@@ -323,7 +323,7 @@ memPoolFree(MemPool * pool, void *obj)
 #if DEBUG_MEMPOOL
     {
 	struct mempool_cookie *cookie = (void *) (((unsigned char *) obj) + pool->real_obj_size);
-	(void) VALGRIND_MAKE_MEM_DEFINED(cookie, sizeof(cookie));
+	(void) VALGRIND_MAKE_MEM_DEFINED(cookie, sizeof(*cookie));
 	assert(cookie->cookie == MEMPOOL_COOKIE(obj));
 	assert(cookie->pool == pool);
 	cookie->cookie = MEMPOOL_COOKIE2(obj);
