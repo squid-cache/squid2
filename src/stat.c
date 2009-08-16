16cbdd35a679d39b01dfebda831b77dcd74423a5
Henrik Nordstrom <hno@squid-cache.org>
2009-08-16 21:45:14 +0000

    Author: Hirling Endre <endre@dawn.royalcomp.hu> Bug 2369: squid traffic counter 32-bit overflow
    The client counters have been tested. On Debian 64-bit.
    
    Also changes identical server and non-HTTP protocol casts.
    
    TODO: Some other casts in the cache-digests section still need to be
          checked and removed.

--- src/stat.c
+++ src/stat.c
@@ -1146,48 +1146,48 @@ statCountersDump(StoreEntry * sentry)
 	f->client_http.hits);
     storeAppendPrintf(sentry, "client_http.errors = %d\n",
 	f->client_http.errors);
-    storeAppendPrintf(sentry, "client_http.kbytes_in = %d\n",
-	(int) f->client_http.kbytes_in.kb);
-    storeAppendPrintf(sentry, "client_http.kbytes_out = %d\n",
-	(int) f->client_http.kbytes_out.kb);
-    storeAppendPrintf(sentry, "client_http.hit_kbytes_out = %d\n",
-	(int) f->client_http.hit_kbytes_out.kb);
+    storeAppendPrintf(sentry, "client_http.kbytes_in = %" PRINTF_OFF_T "\n",
+	f->client_http.kbytes_in.kb);
+    storeAppendPrintf(sentry, "client_http.kbytes_out = %" PRINTF_OFF_T "\n",
+	f->client_http.kbytes_out.kb);
+    storeAppendPrintf(sentry, "client_http.hit_kbytes_out = %" PRINTF_OFF_T "\n",
+	f->client_http.hit_kbytes_out.kb);
 
     storeAppendPrintf(sentry, "server.all.requests = %d\n",
-	(int) f->server.all.requests);
+	f->server.all.requests);
     storeAppendPrintf(sentry, "server.all.errors = %d\n",
-	(int) f->server.all.errors);
-    storeAppendPrintf(sentry, "server.all.kbytes_in = %d\n",
-	(int) f->server.all.kbytes_in.kb);
-    storeAppendPrintf(sentry, "server.all.kbytes_out = %d\n",
-	(int) f->server.all.kbytes_out.kb);
+	f->server.all.errors);
+    storeAppendPrintf(sentry, "server.all.kbytes_in = %" PRINTF_OFF_T "\n",
+	f->server.all.kbytes_in.kb);
+    storeAppendPrintf(sentry, "server.all.kbytes_out = %" PRINTF_OFF_T "\n",
+	f->server.all.kbytes_out.kb);
 
     storeAppendPrintf(sentry, "server.http.requests = %d\n",
-	(int) f->server.http.requests);
+	f->server.http.requests);
     storeAppendPrintf(sentry, "server.http.errors = %d\n",
-	(int) f->server.http.errors);
-    storeAppendPrintf(sentry, "server.http.kbytes_in = %d\n",
-	(int) f->server.http.kbytes_in.kb);
-    storeAppendPrintf(sentry, "server.http.kbytes_out = %d\n",
-	(int) f->server.http.kbytes_out.kb);
+	f->server.http.errors);
+    storeAppendPrintf(sentry, "server.http.kbytes_in = %" PRINTF_OFF_T "\n",
+	f->server.http.kbytes_in.kb);
+    storeAppendPrintf(sentry, "server.http.kbytes_out = %" PRINTF_OFF_T "\n",
+	f->server.http.kbytes_out.kb);
 
     storeAppendPrintf(sentry, "server.ftp.requests = %d\n",
-	(int) f->server.ftp.requests);
+	f->server.ftp.requests);
     storeAppendPrintf(sentry, "server.ftp.errors = %d\n",
-	(int) f->server.ftp.errors);
-    storeAppendPrintf(sentry, "server.ftp.kbytes_in = %d\n",
-	(int) f->server.ftp.kbytes_in.kb);
-    storeAppendPrintf(sentry, "server.ftp.kbytes_out = %d\n",
-	(int) f->server.ftp.kbytes_out.kb);
+	f->server.ftp.errors);
+    storeAppendPrintf(sentry, "server.ftp.kbytes_in = %" PRINTF_OFF_T "\n",
+	f->server.ftp.kbytes_in.kb);
+    storeAppendPrintf(sentry, "server.ftp.kbytes_out = %" PRINTF_OFF_T "\n",
+	f->server.ftp.kbytes_out.kb);
 
     storeAppendPrintf(sentry, "server.other.requests = %d\n",
-	(int) f->server.other.requests);
+	f->server.other.requests);
     storeAppendPrintf(sentry, "server.other.errors = %d\n",
-	(int) f->server.other.errors);
-    storeAppendPrintf(sentry, "server.other.kbytes_in = %d\n",
-	(int) f->server.other.kbytes_in.kb);
-    storeAppendPrintf(sentry, "server.other.kbytes_out = %d\n",
-	(int) f->server.other.kbytes_out.kb);
+	f->server.other.errors);
+    storeAppendPrintf(sentry, "server.other.kbytes_in = %" PRINTF_OFF_T "\n",
+	f->server.other.kbytes_in.kb);
+    storeAppendPrintf(sentry, "server.other.kbytes_out = %" PRINTF_OFF_T "\n",
+	f->server.other.kbytes_out.kb);
 
     storeAppendPrintf(sentry, "icp.pkts_sent = %d\n",
 	f->icp.pkts_sent);
