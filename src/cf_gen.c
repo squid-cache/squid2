26dddb78abee6a7a2a531a07b0084620c3511d7e
Henrik Nordstrom <hno@squid-cache.org>
2009-08-19 01:36:39 +0000

    Install cleaned up squid.conf(.default) and documentation in squid.conf.documented

--- src/cf_gen.c
+++ src/cf_gen.c
@@ -53,8 +53,8 @@
 
 #define MAX_LINE	1024	/* longest configuration line */
 #define _PATH_PARSER		"cf_parser.h"
-#define _PATH_SQUID_CONF	"squid.conf.default"
-#define _PATH_SQUID_CONF_SHORT	"squid.conf.clean"
+#define _PATH_SQUID_CONF	"squid.conf.documented"
+#define _PATH_SQUID_CONF_SHORT	"squid.conf.default"
 #define _PATH_CF_DEPEND		"cf.data.depend"
 
 enum State {
