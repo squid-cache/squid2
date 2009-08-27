d23ba8c8cd7f157f8cf40535f3671414d042566d
Henrik Nordstrom <hno@squid-cache.org>
2009-08-27 09:36:01 +0000

    Fix PROXYPASS case when using external acl creds

--- src/http.c
+++ src/http.c
@@ -1145,7 +1145,7 @@ httpFixupAuthentication(request_t * request, request_t * orig_request, const Htt
     }
     /* external_acl provided credentials */
     if (orig_request->extacl_user && orig_request->extacl_passwd &&
-	(strcmp(orig_request->peer_login, "PASS") == 0 || strcmp(orig_request->peer_login, "PROXYPASS"))) {
+	(strcmp(orig_request->peer_login, "PASS") == 0 || strcmp(orig_request->peer_login, "PROXYPASS") == 0)) {
 	char loginbuf[256];
 	snprintf(loginbuf, sizeof(loginbuf), "%s:%s", orig_request->extacl_user, orig_request->extacl_passwd);
 	httpHeaderPutStrf(hdr_out, header, "Basic %s",
