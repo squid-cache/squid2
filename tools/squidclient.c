812e0a24c536adae1385b51b4e9e90ec506d8d1a
Henrik Nordstrom <hno@squid-cache.org>
2009-10-23 01:14:40 +0000

    64-bit filesize issue in squidclient if trying to post a file > 2GB

--- tools/squidclient.c
+++ tools/squidclient.c
@@ -351,7 +351,7 @@ main(int argc, char *argv[])
 	strcat(msg, buf);
     }
     if (put_fd > 0) {
-	snprintf(buf, BUFSIZ, "Content-length: %d\r\n", (int) sb.st_size);
+	snprintf(buf, BUFSIZ, "Content-length: %" PRINTF_OFF_T "\r\n", (squid_off_t) sb.st_size);
 	strcat(msg, buf);
     }
     if (opt_noaccept == 0) {
