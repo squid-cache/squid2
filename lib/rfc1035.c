a2ea389098754d85b22064a5490f23567b2f31bd
Henrik Nordstrom <hno@squid-cache.org>
2010-02-12 20:58:25 +0000

    Author: amosjeffries Handle DNS header-only packets as invalid. (CVE-2010-0308)

--- lib/rfc1035.c
+++ lib/rfc1035.c
@@ -286,7 +286,9 @@ rfc1035NameUnpack(const char *buf, size_t sz, int *off, unsigned short *rdlength
     size_t len;
     assert(ns > 0);
     do {
-	assert((*off) < sz);
+	if ((*off) >= sz) {
+	    return 1;
+	}
 	c = *(buf + (*off));
 	if (c > 191) {
 	    /* blasted compression */
